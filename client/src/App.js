6cfece8fdb76919962222ce65c2834651961bd8e
Jan Guillermo <ehteibwen@gmail.com>
2019-05-29 14:14:26 -0400

    Fix subdirectory issue

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
 
 	render() {
 		return (
-			<Router className="app">
+			<Router basepath={process.env.PUBLIC_URL} className="app">
 				<Main path="/" />
 				<SupplementaryMaterials path="/supplementary" />
 			</Router>
