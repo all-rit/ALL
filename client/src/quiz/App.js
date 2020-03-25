21f85e6d168bd4254d9a0abee1627d7c6ec6022a
Saad Khan <sk6786@g.rit.edu>
2020-03-24 21:44:10 -0400

    Fixed Quiz format header

--- client/src/quiz/App.js
+++ client/src/quiz/App.js
@@ -189,7 +189,7 @@ class App extends Component {
         return (
             <div className="quiz">
                 <div className="quizHeader">
-                    <h2 class="section-heading text-uppercase">Accessibility Lab1 Quiz</h2>
+                    <h2 class="section-heading text-uppercase">Treasure Hunter: Quiz</h2>
                 </div>
                 {this.state.result ? this.renderResult() : this.renderQuiz()}
             </div>
