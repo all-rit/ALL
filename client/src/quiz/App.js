3350a291eda17b8e9321b8145ea8dfc4a5a9564d
Komal Sorte <ks7229@g.rit.edu>
2020-04-02 20:33:47 -0400

    Quiz Result Display

--- client/src/quiz/App.js
+++ client/src/quiz/App.js
@@ -18,13 +18,7 @@ class App extends Component {
             result: '',
             myCount: {},
             disableNextQuestion: true,
-            selectedAnswers: {},
-            students: [
-                {id: 1, name: 'Wasif', age: 21, email: 'wasif@email.com'},
-                {id: 2, name: 'Ali', age: 19, email: 'ali@email.com'},
-                {id: 3, name: 'Saad', age: 16, email: 'saad@email.com'},
-                {id: 4, name: 'Asad', age: 25, email: 'asad@email.com'}
-            ]
+            selectedAnswers: {}
         };
 
         this.handleAnswerSelected = this.handleAnswerSelected.bind(this);
