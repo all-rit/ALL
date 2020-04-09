1f1758bd35cf29abb6d8e5eb4ebbd6698df7f92a
Komal Atul Sorte <47836659+komalsorte@users.noreply.github.com>
2020-04-09 17:26:16 -0400

    Update quiz.js
    Quiz - Title

--- client/src/components/body/quiz.js
+++ client/src/components/body/quiz.js
@@ -1,23 +1,34 @@
 import React from "react";
-import QuizComp from "./../../quiz/App"
+import Quiz from "./../../quiz/App"
 import "./../../vendor/bootstrap/css/bootstrap.min.css";
 import "./../../css/agency.min.css";
 import "./../../css/style.css";
+import CaseStudy from "../../pages/CaseStudy";
 
-const Quiz = ({ title, description, links }) => {
-  if (links === undefined) {
-    links = [null, null, null];
-  }
+const Reading = ({title, description, links}) => {
+    if (links === undefined) {
+        links = [null, null, null];
+    }
 
-  return (
-    <div class="container">
-        //TODO add the title
-      <div class="row quiz">
-        <QuizComp />
-      </div>
+    return (
+        <div className="container">
+            <section className="page-section" style={{paddingBottom: "0px"}}>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-12 text-center">
+                            <br/>
+                            <br/>
+                            <h2 className="section-heading text-uppercase">
+                                Treasure Hunter: Quiz
+                            </h2>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <Quiz/>
+        </div>
 
-    </div>
-  );
+    );
 };
 
-export default Quiz;
+export default Reading;
