e17f27b78dfd689d31824a509f49899df29cacf6
sk2628 <sk2628@g.rit.edu>
2020-04-14 15:19:15 -0400

    Button changes for Game

--- client/src/App.js
+++ client/src/App.js
@@ -128,7 +128,7 @@ class App extends Component {
                 <Change />
               </div>
               <div class="btn-information">
-                These buttons are disabled so as to not interfere with the
+                The navigation and accessibility buttons are disabled so as to not interfere with the
                 accessibility-related portions of the lab.
               </div>
             </footer>
