1f1f485d3b0893379738832bf7c8f7c6a39825ae
sk2628 <sk2628@g.rit.edu>
2020-04-15 11:58:29 -0400

    Footer changes for Game

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Reading from "./components/body/reading";
 import Video from "./components/body/video";
 import Quiz from "./components/body/quiz";
 import Change from "./components/footer/change";
+import gamefooter from "./components/footer/gameFooter"
 import Header from "./components/header/header"
 import { Google } from "./components/header/buttons/google";
 import "./vendor/bootstrap/css/bootstrap.min.css";
@@ -101,36 +102,44 @@ class App extends Component {
   };
 
   render() {
+    const isState = this.state.count;
     return (
 
          <div>
            <Header count={this.state.count} handleQuiz={this.handleQuiz} handleAbout={this.handleAbout} handleHome={this.handleHome} handleReading={this.handleReading} handleGame={this.handleGame} handleVideo={this.handleVideo}  />
-
+           <Change handleDecrement={this.handleDecrement} disappearBack={this.disappearBack} handleIncrement={this.handleIncrement} disappearNext = {this.disappearNext} />
          <div>{section[this.state.count]}</div>
 
           <div class="container">
-            <button
-                className="btn btn-second btn-xl text-uppercase js-scroll-trigger back "
-                onClick={this.handleDecrement}
-                disabled={this.disappearBack() ? "disabled" : false}
-            >
-              back
-            </button>
-            <button
-                className="btn btn-primary btn-xl text-uppercase js-scroll-trigger next"
-                onClick={this.handleIncrement}
-                disabled={this.disappearNext() ? "disabled" : false}
-            >
-              next
-            </button>
-            <footer >
+            {/*<button*/}
+            {/*    className="btn btn-second btn-xl text-uppercase js-scroll-trigger back "*/}
+            {/*    onClick={this.handleDecrement}*/}
+            {/*    disabled={this.disappearBack() ? "disabled" : false}*/}
+            {/*>*/}
+            {/*  back*/}
+            {/*</button>*/}
+            {/*<button*/}
+            {/*    className="btn btn-primary btn-xl text-uppercase js-scroll-trigger next"*/}
+            {/*    onClick={this.handleIncrement}*/}
+            {/*    disabled={this.disappearNext() ? "disabled" : false}*/}
+            {/*>*/}
+            {/*  next*/}
+            {/*</button>*/}
+
               <div class="btn-change">
-                <Change />
+                {isState !== 2
+
+                    ? <Change />
+                    : <gamefooter />
+
+                }
               </div>
-              <div class="btn-information">
-                These buttons are disabled so as to not interfere with the
-                accessibility-related portions of the lab.
-              </div>
+              <footer >
+
+              {/*<div class="btn-information">*/}
+              {/*  These buttons are disabled so as to not interfere with the*/}
+              {/*  accessibility-related portions of the lab.*/}
+              {/*</div>*/}
             </footer>
           </div>
            </div>
--- client/src/components/footer/change.js
+++ client/src/components/footer/change.js
@@ -3,33 +3,61 @@ import "./../../vendor/bootstrap/css/bootstrap.min.css";
 import "./../../css/agency.min.css";
 import "./../../css/style.css";
 
-const Change = ({ title, description, links }) => {
-  if (links === undefined) {
-    links = [null, null, null];
-  }
+const Change = (props) => {
+    // if (links === undefined) {
+    //     links = [null, null, null];
+    // }
 
-  return (
-    <div class="btn-change">
-      <button
-        class="btn-text btn btn-disabled text-uppercase"
-        alt="Increase text size"
-        title="Larger text"
-        disabled="true"
-      >
-        Text+
-      </button>
-      <button
-        class="btn-text btn btn-disabled text-uppercase"
-        alt="Decrease text size"
-        title="Smaller text"
-        disabled="true"
-      >
-        Text-
-      </button>
-      <button class="btn btn-disabled text-uppercase" disabled="true">Change Color</button>
-      <button class="btn btn-disabled text-uppercase" disabled="true">Change Color</button>
+    return (
+<div class ="container">
+    <button
+        className="btn btn-second btn-xl text-uppercase js-scroll-trigger back "
+        onClick={props.handleDecrement}
+        disabled={props.disappearBack ? "disabled" : false}
+    >
+        back
+    </button>
+    <button
+        className="btn btn-primary btn-xl text-uppercase js-scroll-trigger next"
+        onClick={props.handleIncrement}
+        disabled={props.disappearNext ? "disabled" : false}
+    >
+        next
+    </button>
+        <div class="btn-change">
+            <button
+                class="btn-text btn btn-disabled text-uppercase"
+                alt="Increase text size"
+                title="Larger text"
+                disabled="true"
+            >
+                Text+
+            </button>
+            <button
+                class="btn-text btn btn-disabled text-uppercase"
+                alt="Decrease text size"
+                title="Smaller text"
+                disabled="true"
+            >
+                Text-
+            </button>
+            <button class="btn btn-disabled text-uppercase" disabled="true">Change Color</button>
+            <button class="btn btn-disabled text-uppercase" disabled="true">Change Color</button>
+
+        </div>
+
+        <footer>
+            <div className="btn-information">
+                These buttons are disabled so as to not interfere with the
+                accessibility-related portions of the lab.
+            </div>
+        </footer>
     </div>
-  );
+
+
+
+
+    );
 };
 
 export default Change;
