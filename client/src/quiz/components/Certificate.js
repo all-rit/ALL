c756fb4aaf709c3353903105f842de01c99436c2
Saad Khan <sk6786@g.rit.edu>
2020-04-24 12:32:01 -0400

    removed underline and fixed certificate

--- client/src/quiz/components/Certificate.js
+++ client/src/quiz/components/Certificate.js
@@ -30,13 +30,24 @@ class Certificate extends Component {
 
 
     }
+    getColor=()=>{
+        let score = this.props.quizResult;
+        score = parseFloat(score);
+        switch (true) {
+            case score<=40:
+                return "red";
+            case score<=70:
+                return "orange";
+            default:
+                return "green";
+        }
+    };
 
     render() {
         console.log(this.props);
         const {state, actions} = this.props;
         var today = new Date();
         var date = (today.getMonth()+1)+'/'+today.getDate() +'/'+ today.getFullYear();
-
         return (
             <div style={{width:"100%", height:"auto", padding:"20px", border: "10px solid #787878"}}>
                 <div style={{width:"100%", height:"auto", border: "5px solid #787878"}}>
@@ -48,8 +59,8 @@ class Certificate extends Component {
                         <br /><br />
                             {/*<span style={{fontSize:"30px"}}><b>Sakshi</b></span><br/><br/>*/}
                             {/*<span style={{fontSize:"25px"}}><i>have completed the course</i></span> <br/><br/>*/}
-                            <span style={{fontSize:"30px",textAlign:"center",padding:"20px"}}>Accessibility Learning Lab 1: Deaf and Hard of Hearing</span> <br/><br/>
-                            <span style={{fontSize:"25px",textAlign:"center",padding:"20px"}}>with score of <b>{this.props.quizResult}</b></span> <br/><br/>
+                            <span style={{fontSize:"30px",textAlign:"center",padding:"20px"}}>Accessibility Learning Lab 1: Accessibility to Sound and Speech</span> <br/><br/>
+                            <span style={{fontSize:"25px",textAlign:"center",padding:"20px"}}>with score of <b style={{color:this.getColor()}}>{this.props.quizResult}</b></span> <br/><br/>
                             <span style={{fontSize:"25px",textAlign:"center",padding:"20px"}}><i>dated</i></span><br />
                             <span style={{fontSize:"30px",textAlign:"center",padding:"20px"}}>{date}</span><br/><br/>
                             {/*<img src={logo} height="100" width="100">*/}
