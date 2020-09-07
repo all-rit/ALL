1206b2750548f321fa53171429c6f81bc687658d
Saad Khan <sk6786@g.rit.edu>
2020-09-07 00:31:37 -0400

    Added test file

---
+++ client/src/App.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
+
+import App from './App';
+
+import reducers from './reducers';
+import sagas from './sagas';
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(reducers, applyMiddleware(sagaMiddleware));
+
+sagaMiddleware.run(sagas);
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Provider store={store}>
+    <App />
+  </Provider>, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
