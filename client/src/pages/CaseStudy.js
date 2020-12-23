df20ae8120ae753663c98bda123587f3be2065ff
Mark Sternfeld <smarky02@gmail.com>
2020-12-23 09:55:57 -0500

    cited info

--- client/src/pages/CaseStudy.js
+++ client/src/pages/CaseStudy.js
@@ -172,6 +172,7 @@ class CaseStudy extends Component {
 					</a>{' '}
 					is vital to the continued success of web applications on the internet.
 				</p>
+				<p>Some information for this page was gathered from <a href="http://www.nchearingloss.org" target="_blank" rel="noopener noreferrer">www.nchearingloss.org</a></p>
 
 				<div className="study__actions">
 					<Link to="/supplementary">&laquo; Back to Supplementary Materials</Link>
