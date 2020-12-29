d7e9d6330ea9cbe0e6a50dd7dcd31e6e92b2856b
cxs7700 <christophersavan@gmail.com>
2020-12-29 15:12:06 -0500

    eliminate and replace conditional.js

--- client/src/services/PageService.js
+++ client/src/services/PageService.js
@@ -3,14 +3,15 @@ import {API} from './API';
 const endpoints = {
     CREATE_PAGE: '/page/complete'
 };
-const labname = "Lab3";
+
+const labid;
 
 export const PageService = {
     createPage: (pagename, completiontime) => {
         return API.postWithBody(process.env.REACT_APP_SERVER_URL + endpoints.CREATE_PAGE, {
             pagename,
             completiontime,
-            labname
+            labid
         });
     },
 }
