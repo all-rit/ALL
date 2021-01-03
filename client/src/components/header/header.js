2d91cb779e1e38be5603d1c4b9736585ac3c642e
st5626 <st5626@g.rit.edu>
2021-01-02 23:18:20 -0800

    replaced inline styling in header.js with scss

--- client/src/components/header/header.js
+++ client/src/components/header/header.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import logo from "../../assets/images/accessCycle.png";
 import "../../assets/stylesheets/components/css/agency.min.css";
 import "../../assets/stylesheets/components/css/style.css";
+import "../../assets/stylesheets/components/Header.scss";
 import WelcomeMessage from './helpers/WelcomeMessage';
 import {connect} from "react-redux";
 import {actions as appActions} from '../../reducers/lab1/AppReducer';
@@ -69,24 +70,18 @@ const alert_check = (state)=> {
 
 const Header = (props) => {
     const [isOpen, setIsOpen] = useState(false);
-    const activeStyle = {color: "#fed136"};
+    // const activeStyle = {color: "#fed136"};
     const toggle = () => setIsOpen(!isOpen);
     const {state, actions} = props;
     let count = state.app.body;
     const loginEnabled = !(state.game.plays > 0 || (state.game.plays === 0 && state.game.state !== GAME_IDLE));
     return (
-        <Navbar dark expand="lg" className="navbar labnav" style={{backgroundColor: "rgb(60,61,60)", paddingTop: "1rem"}}>
+        <Navbar dark expand="lg" className="navbar labnav">
             <div className="container">
                 <NavbarBrand href="http://all.rit.edu">
                     <div className="logo-container justify-content-center">
                         <img className="logo img-fluid"
                              src={logo}
-                             style={{
-                                 paddingRight: "20px",
-                                 paddingBottom: "10px",
-                                 paddingTop: "10px",
-                                 marginTop: "-10px"
-                             }}
                              alt="Computing Accessibility"
                         />
                     </div>
@@ -96,31 +91,31 @@ const Header = (props) => {
                 <NavbarToggler onClick={toggle}/>
                 <Collapse isOpen={isOpen} navbar>
                     <Nav className="ml-auto" navbar>
-                        <NavItem class="collapse navbar-collapse"
+                        <NavItem className="collapse navbar-collapse"
                                  id="navbarResponsive"
                         > </NavItem>
-                        <NavItem class="collapse navbar-collapse"
+                        <NavItem className="collapse navbar-collapse"
                                  id="navbarResponsive"
                         >
                             <NavLink
-                                class="nav-link js-scroll-trigger"
+                                className="nav-link js-scroll-trigger text-white"
                                 href="http://all.rit.edu"
-                                style={{color: "#fff"}}>
+                            >
                                 <ul className="navbar-nav text-uppercase ml-auto">
-                                    <li className="nav-item" style={{align: "right"}}>
+                                    <li className="nav-item text-right">
                                         Home
                                     </li>
                                 </ul>
                             </NavLink>
                         </NavItem>
                         <NavItem
-                            class="collapse navbar-collapse"
+                            className="collapse navbar-collapse"
                             id="navbarResponsive">
                             <NavLink
-                                class="nav-link js-scroll-trigger"
+                                className={"nav-link js-scroll-trigger " + (count === 0 ? "active" : "inactive")}
                                 onClick={() => handleAbout(actions, state)}
                                 href="#"
-                                style={count === 0 ? activeStyle : {color: "#fff"}}>
+                            >
                                 <ul className="navbar-nav text-uppercase ml-auto">
                                     <li className="nav-item">
                                         About
@@ -129,13 +124,13 @@ const Header = (props) => {
                             </NavLink>
                         </NavItem>
                         <NavItem
-                            class="collapse navbar-collapse"
+                            className="collapse navbar-collapse"
                             id="navbarResponsive">
                             <NavLink
-                                class="nav-link js-scroll-trigger"
+                                className={"nav-link js-scroll-trigger " + (count === 1 ? "active" : "inactive")}
                                 onClick={() => handleReading(actions, state)}
                                 href="#"
-                                style={count === 1 ? activeStyle : {color: "#fff"}}>
+                            >
                                 <ul className="navbar-nav text-uppercase ml-auto">
                                     <li className="nav-item">
                                         Reading
@@ -145,13 +140,13 @@ const Header = (props) => {
 
                         </NavItem>
                         <NavItem
-                            class="collapse navbar-collapse"
+                            className="collapse navbar-collapse"
                             id="navbarResponsive">
                             <NavLink
-                                class="nav-link js-scroll-trigger"
+                                className={"nav-link js-scroll-trigger " + (count === 2 ? "active" : "inactive")}
                                 onClick={() => handleGame(actions, state)}
                                 href="#"
-                                style={count === 2 ? activeStyle : {color: "#fff"}}>
+                            >
                                 <ul className="navbar-nav text-uppercase ml-auto">
                                     <li className="nav-item">
                                         Game
@@ -161,13 +156,13 @@ const Header = (props) => {
 
                         </NavItem>
                         <NavItem
-                            class="collapse navbar-collapse"
+                            className="collapse navbar-collapse"
                             id="navbarResponsive">
                             <NavLink
-                                class="nav-link js-scroll-trigger"
+                                className={"nav-link js-scroll-trigger " + (count === 3 ? "active" : "inactive")}
                                 onClick={() => handleVideo(actions, state)}
                                 href="#"
-                                style={count === 3 ? activeStyle : {color: "#fff"}}>
+                            >
                                 <ul className="navbar-nav text-uppercase ml-auto">
                                     <li className="nav-item">
                                         Video
@@ -177,14 +172,14 @@ const Header = (props) => {
 
                         </NavItem>
                         <NavItem
-                            class="collapse navbar-collapse"
+                            className="collapse navbar-collapse"
                             id="navbarResponsive"
                             style={{paddingRight: ".5rem"}}>
                             <NavLink
-                                class="nav-link js-scroll-trigger"
+                                className={"nav-link js-scroll-trigger " + (count === 4 ? "active" : "inactive")}
                                 onClick={() => handleQuiz(actions, state)}
                                 href="#"
-                                style={count === 4 ? activeStyle : {color: "#fff"}}>
+                            >
                                 <ul className="navbar-nav text-uppercase ml-auto">
                                     <li className="nav-item">
                                         Quiz
@@ -193,7 +188,6 @@ const Header = (props) => {
                             </NavLink>
                         </NavItem>
                         <WelcomeMessage user={state.main.user} loginEnabled={loginEnabled} />
-
                     </Nav>
                 </Collapse>
             </div>
