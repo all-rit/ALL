655d13d256f89044453fb1e18b9ae73da2556be5
st5626 <st5626@g.rit.edu>
2021-01-02 20:19:29 -0800

    replaced inline styling for Certificate.js with an scss file

--- client/src/components/quiz/components/Certificate.js
+++ client/src/components/quiz/components/Certificate.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import "../../../assets/stylesheets/components/css/agency.min.css";
 import "../../../assets/stylesheets/components/css/style.css";
+import "../../../assets/stylesheets/components/Certificate.scss";
 import {connect} from "react-redux";
 import {actions as appActions} from '../../../reducers/lab1/AppReducer';
 import {bindActionCreators} from 'redux';
@@ -19,13 +20,13 @@ const mapDispatchToProps = (dispatch) => ({
 
 
 class Certificate extends Component {
-    getColor=()=>{
+    getColor = () => {
         let score = this.props.quizResult;
         score = parseFloat(score);
         switch (true) {
-            case score<=40:
+            case score <= 40:
                 return "crimson";
-            case score<=70:
+            case score <= 70:
                 return "orange";
             default:
                 return "chartreuse";
@@ -33,42 +34,38 @@ class Certificate extends Component {
     };
 
     render() {
-        // console.log(this.props);
         const {state} = this.props;
-        var today = new Date();
-        // console.log(state.game.results);
-        var date = (today.getMonth()+1)+'/'+today.getDate() +'/'+ today.getFullYear();
+        const today = new Date();
+        const date = (today.getMonth() + 1) + '/' + today.getDate() + '/' + today.getFullYear();
         return (
-            <div style={{width:"100%", height:"auto", padding:"20px", border: "10px solid #787878"}}>
-                <div style={{width:"100%", height:"auto", border: "5px solid #787878"}}>
-                    <div style={{width:"50%", margin: "auto"}}>
-                    <span style={{fontSize:"50px", fontWeight:"bold",textAlign:"center"}}>
+            <div className="outer-border">
+                <div className="inner-border">
+                    <div className="title-div">
+                    <span className="title">
                             Certificate of Completion
                     </span>
                     </div>
-                    <br /><br />
-                        <span style={{fontSize:"25px",textAlign:"center",padding:"20px"}}>
+                    <br/><br/>
+                    <span className="normal-text">
                             {state.main.user !== null
-                                ? <i>This is to certify that <b>{state.main.user.firstname}</b> has completed the course:</i>
+                                ?
+                                <i>This is to certify that <b>{state.main.user.firstname}</b> has completed the course:</i>
                                 : <i>This is to certify that you have completed the course:</i>
                             }
                             </span>
-                        <br /><br />
-                            <span style={{fontSize:"30px",textAlign:"center",padding:"20px"}}>Accessibility Learning Lab 1: Accessibility to Sound and Speech</span> <br/><br/>
-                            <span style={{fontSize:"25px",textAlign:"center",padding:"20px"}}>with score of <b style={{color:this.getColor()}}>{this.props.quizResult}</b></span> <br/><br/>
-                            <span style={{fontSize:"25px",textAlign:"center",padding:"20px"}}><i>dated</i></span><br />
-                            <span style={{fontSize:"30px",textAlign:"center",padding:"20px"}}>{date}</span><br/><br/>
-                            <div style={{backgroundColor: "rgb(61, 61, 61)"}} >
-                            <img src={logo}
-                                alt="logo"
-                                 style={{height:"120px",
-                                     width:"500px"}}></img>
-                            </div>
+                    <br/><br/>
+                    <span className="bigger-text">Accessibility Learning Lab 1: Accessibility to Sound and Speech</span>
+                    <br/><br/>
+                    <span className="normal-text">with a score of <b
+                        style={{color: this.getColor()}}>{this.props.quizResult}</b></span> <br/><br/>
+                    <span className="normal-text"><i>dated</i></span><br/>
+                    <span className="bigger-text">{date}</span><br/><br/>
+                    <div className="logo-div">
+                        <img src={logo} alt="logo" className="logo"/>
+                    </div>
                 </div>
             </div>
-
         );
-
     };
 }
 
