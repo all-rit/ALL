194c9429f24382420b57bb91cf8dc6240290d4c5
Mark Sternfeld <smarky02@gmail.com>
2021-02-16 09:37:28 -0500

    added quiz questions

---
+++ client/src/components/quiz/api/Lab2/quizQuestions.js
@@ -0,0 +1,107 @@
+var quizQuestions = [
+    {
+        question: "What is an audio cue?",
+        answers: [
+            {
+                val: 0,
+                content: "A vibration that conveys key information"
+            },
+            {
+                val: 1,
+                content: "A playing sound that conveys key information"
+            },
+            {
+                val: 0,
+                content: "A visual indicator that conveys key information"
+            },
+            {
+                val: 0,
+                content: "None of the above"
+            }
+
+        ],
+        multiChoice: false
+    },
+    {
+        question: "How many people (aged 18 and over) with hearing loss are there in the United States?",
+        answers: [
+            {
+                val: 0,
+                content: "20-30 million people"
+            },
+            {
+                val: 1,
+                content: "30-40 million people"
+            },
+            {
+                val: 0,
+                content: "40-50 million people"
+            },
+            {
+                val: 0,
+                content: "50-60 million people"
+            }
+        ],
+        multiChoice: false
+    },
+    {
+        question: "When should accessibility be considered in the development process?",
+        answers: [
+            {
+                val: 0,
+                content: "At the end"
+            },
+            {
+                val: 1,
+                content: "From the beginning"
+            },
+            {
+                val: 0,
+                content: "Whenever the developer feels it's necessary"
+            },
+            {
+                val: 0,
+                content: "Only if it's needed for the project"
+            }
+        ],
+        multiChoice: false
+    },
+    {
+        question: "What does the 'Perceivable' accessibility principle mean?",
+        answers: [
+            {
+                val: 0,
+                content: "User interface components and navigation must be operable."
+            },
+            {
+                val: 1,
+                content: "Information and user interface components must be presentable to users in ways they can perceive."
+            },
+            {
+                val: 0,
+                content: "Information and the operation of user interface must be understandable."
+            },
+            {
+                val: 0,
+                content: "None of the above"
+            }
+        ],
+        multiChoice: false
+    },
+    {
+        question: "Do users with hearing loss have a disadvantage when they use applications that utilize audio cues?",
+        answers: [
+            {
+                val: 1,
+                content: "Yes"
+            },
+            {
+                val: 0,
+                content: "No"
+            }
+        ],
+        multiChoice: false
+    }
+];
+
+export default quizQuestions;
