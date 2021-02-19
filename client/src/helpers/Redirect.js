0407c71921019687d0ea5192e0ad007ae6218a36
Saad Khan <sk6786@g.rit.edu>
2021-02-19 16:58:14 -0500

    fixed redirect

--- client/src/helpers/Redirect.js
+++ client/src/helpers/Redirect.js
@@ -14,31 +14,42 @@ export const handleRedirect = (actions, lab, body = 0) => {
 
 }
 export const stateChange = (actions, pathname) =>{
-    console.log(pathname);
     let parsed = pathname.split('/');
-    const index = parsed.indexOf("");
-    if (index > -1) {
-        parsed.splice(index, 1);
-    }
-    const [labs, bodies] = getLabsBodies();
+    parsed =parsed.filter(x => x !== "");
     let redirect_lab = null;
     let redirect_body = null;
     let noMatch = false;
-    parsed.filter(string => {
-        bodies.forEach(word => {
-            if (string === word && !redirect_body){
-                redirect_body = word;
-            }
+    if (parsed.length=== 0){ //if url is all.rit.edu
+        actions.setLab(0);
+        actions.setBody(0);
+        return;
+    }
+    let [labs, bodies] = getLabsBodies();
+    if(parsed.length ===1){ //if url is all.rit.edu/{Lab} or all.rit.edu/{homebody}
+        let bodies = getLabsBodies(true);//check for home body like sitemap first
+        redirect_body= bodies.includes(parsed[0]) ? parsed[0] : null;
+        redirect_lab = "";
+        if(!redirect_body){ //means it wasn't a valid home page body, so check lab
+            redirect_lab= labs.includes(parsed[0]) ? parsed[0] : null;
+            redirect_body = "";
+        }
+    }
+    else{
+        parsed.filter(string => { //checks for all.rit.edu/{Lab}/{Something}
+            bodies.forEach(word => {
+                if (string === word && !redirect_body){
+                    redirect_body = word;
+                }
 
-        })
-        labs.forEach(word => {
-            if (string === word && !redirect_lab){
-                redirect_lab = word;
-            }
-
-        })
-        return "";
-    });
+            })
+            labs.forEach(word => {
+                if (string === word && !redirect_lab){
+                    redirect_lab = word;
+                }
+            })
+            return "";
+        });
+    }
     switch (redirect_lab) {
         case "Lab1":
             actions.setLab(1);
@@ -76,7 +87,7 @@ export const stateChange = (actions, pathname) =>{
         case "Quiz":
             actions.setBody(4);
             break;
-        case "Sitemap":
+        case "SiteMap":
             actions.setBody(1);
             break;
         default:
@@ -86,78 +97,10 @@ export const stateChange = (actions, pathname) =>{
             }
             break;
     }
-    // if (noMatch){
-    //     alert("Page does not exist")
-    //     navigate(process.env.PUBLIC_URL + "/").then(()=>{
-    //         actions.setBody(0);
-    //         actions.setLab(0);
-    //     }
-    //     )
-    // }
-}
-                })
-                labs.forEach(word => {
-                    if (string === word && !redirect_lab){
-                        redirect_lab = word;
-                    }
-                })
-                return "";
-            });
-        }
-        switch (redirect_lab) {
-            case "Lab1":
-                actions.setLab(1);
-                break;
-            // case "Lab2":
-            //     actions.setLab(2);
-            //     break;
-            case "Lab3":
-                actions.setLab(3);
-                break;
-            // case "Lab4":
-            //     actions.setLab(4);
-            //     break;
-            case "":
-                actions.setLab(0);
-                break;
-            default:
-                noMatch = true;
-                actions.setLab(0);
-                break;
-        }
-        switch (redirect_body) {
-            case "About":
-                actions.setBody(0);
-                break;
-            case "Reading":
-                actions.setBody(1);
-                break;
-            case "Game":
-                actions.setBody(2);
-                break;
-            case "Video":
-                actions.setBody(3);
-                break;
-            case "Quiz":
-                actions.setBody(4);
-                break;
-            case "SiteMap":
-                actions.setBody(1);
-                break;
-            default:
-                actions.setBody(0);
-                if(redirect_body !== ""){
-                    noMatch = true;
-                }
-                break;
-        }
-        if (noMatch){
-            actions.setBody(0);
-            actions.setLab(0);
-        }
-
+    if (noMatch){
+        actions.setBody(0);
+        actions.setLab(0);
     }
-
 }
 const getLabsBodies =(home = false)=>{
     let labs=[];
