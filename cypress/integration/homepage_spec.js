2180f4cbda26cf182344ae587d0447a0c72ed4a0
cxs7700 <cxs7700@g.rit.edu>
2021-02-23 12:22:30 -0500

    homepage lab links test

--- cypress/integration/homepage_spec.js
+++ cypress/integration/homepage_spec.js
@@ -3,15 +3,24 @@ describe("Testing homepage", () => {
     cy.visit('http://localhost:3000/');
   });
   
-  it("Test lab links", () => {
-    cy.get('h4').each(($el) => {
-        if ($el !== null) {
-          expect($el).to.exist;
-          cy.get($el).next().click();
-          cy.location('pathname').should('include', '/Lab', { message: ''});
-        }
-        cy.visit('http://localhost:3000/');
-      })
-    }
-  )
+  it("tests Lab 1 link", () => {
+    cy.get('h4').eq(0).should('be.visible');
+    cy.get('h4').eq(0).children().should('have.attr', 'href', '# ')
+  })
+  
+  it("tests Lab 2 link", () => {
+    cy.get('h4').eq(1).should('be.visible');
+    cy.get('h4').eq(1).children().should('have.attr', 'href', '# ')
+  })
+  
+  it("tests Lab 3 link", () => {
+    cy.get('h4').eq(2).should('be.visible');
+    cy.get('h4').eq(2).children().should('have.attr', 'href', '# ')
+  })
+  
+  it("tests Lab 4 link", () => {
+    cy.get('h4').eq(3).should('be.visible');
+    cy.get('h4').eq(3).children().should('have.attr', 'href', '# ')
+  })
+  
 })
