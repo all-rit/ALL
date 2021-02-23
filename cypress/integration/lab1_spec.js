95ed17131500d1432cd00b7dd7d066562dcf57da
cxs7700 <cxs7700@g.rit.edu>
2021-02-23 14:52:57 -0500

    commands, footer navigation buttons

--- cypress/integration/lab1_spec.js
+++ cypress/integration/lab1_spec.js
@@ -1,10 +1,17 @@
 describe('Testing Lab 1', () => {
   beforeEach(() => {
+    cy.fixture('data.json').as('data');
     cy.visit('http://localhost:3000/Lab1/');
   });
   
-  it('tests navigation', () => {
+  it('tests navigation bar', () => {
     cy.testNavigationBar();
+  });
+  
+  it('tests footer navigation', () => {
+    cy.testNextPage();
+    cy.visit('http://localhost:3000/Lab1/Quiz/');
+    cy.testPreviousPage();
   });
 
   it('tests login', () => {
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -31,28 +31,84 @@ Cypress.Commands.add('testLogin', () => {
   cy.get('div[class*="google__button').click();
 });
 
-// -- Tests the navigation bar visibility -- 
+// -- Tests the navigation bar visibility and functionality -- 
 Cypress.Commands.add('testNavigationBar', (labId) => {
   // Desktop
   cy.viewport('macbook-15')
   cy.wait(200)
   cy.get('nav').contains('About').click();
+  cy.location('pathname').should('include', '/About');
   cy.get('nav').contains('Reading').click();
+  cy.location('pathname').should('include', '/Reading');
   cy.get('nav').contains('Game').click();
+  cy.location('pathname').should('include', '/Game');
   cy.get('nav').contains('Video').click();
+  cy.location('pathname').should('include', '/Video');
   cy.get('nav').contains('Quiz').click();
+  cy.location('pathname').should('include', '/Quiz');
   
   // Tablet
   cy.viewport('ipad-2');
   cy.wait(200);
   cy.get('button[class*="navbar-toggler').click();
   cy.get('nav').contains('About').click();
+  cy.location('pathname').should('include', '/About');
   cy.get('nav').contains('Reading').click();
+  cy.location('pathname').should('include', '/Reading');
   cy.get('nav').contains('Game').click();
+  cy.location('pathname').should('include', '/Game');
   cy.get('nav').contains('Video').click();
+  cy.location('pathname').should('include', '/Video');
   cy.get('nav').contains('Quiz').click();
+  cy.location('pathname').should('include', '/Quiz');
+  
 });
 
-Cypress.Commands.add('testFooterNavigation', () => {
+Cypress.Commands.add('testNextPage', () => {
+  cy.get('button').contains('Next').should('have.class', 'next').then(() => {
+    // cy.location('pathname').should('include', '/About'); // going to lab does not change URL
+    cy.get('button').contains('Next').click();
+    
+    cy.location('pathname').should('include', '/Reading');
+    cy.get('button').contains('Next').click();
+    
+    cy.location('pathname').should('include', '/Game');
+    cy.get('button').contains('Next').click();
+    
+    cy.location('pathname').should('include', '/Video');
+    cy.get('button').contains('Next').click();
+    
+    cy.location('pathname').should('include', '/Quiz');
+  })
+});
+
+Cypress.Commands.add('testPreviousPage', () => {
+  cy.get('button').contains('Previous').should('have.class', 'back').then(() => {
+    cy.location('pathname').should('include', '/Quiz');
+    cy.get('button').contains('Previous').click();
+    
+    cy.location('pathname').should('include', '/Video');
+    cy.get('button').contains('Previous').click();
+    
+    cy.location('pathname').should('include', '/Game');
+    cy.get('button').contains('Previous').click();
+    
+    cy.location('pathname').should('include', '/Reading');
+    cy.get('button').contains('Previous').click();
+    
+    cy.location('pathname').should('include', '/About');
+  })
+})
+
+// For each color and font size test, check consistency into another page
+Cypress.Commands.add('testChangeBgColor', () => {
+  cy.get();
+});
+
+Cypress.Commands.add('testChangeTextColor', () => {
+  cy.get();
+});
+
+Cypress.Commands.add('testFontSizeDecrease', () => {
   cy.get();
 });
