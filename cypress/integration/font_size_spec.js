09d41661fa0391391d8e07b17e5a394437cd6fe2
cxs7700 <cxs7700@g.rit.edu>
2021-03-13 12:52:03 -0500

    starter files

---
+++ cypress/integration/font_size_spec.js
@@ -0,0 +1,21 @@
+describe('Testing font size', () => {
+  
+  it('tests font size increase', () => {
+    // view changeSize(), changeTSize(), editPage.js
+  });
+  
+  it('tests font size increase persistency', () => {
+    
+  });
+  
+  it('tests font size decrease', () => {
+    
+  });
+  
+  it('tests font size decrease persistency', () => {
+    
+  });
+  
+  // into homepage?
+  
+})
