a4e58ee4130eca352f5cac6ebdd0322a3797d65d
Mark Sternfeld <smarky02@gmail.com>
2021-04-12 22:20:31 -0400

    minor style changes

--- client/src/components/body/landingpage/citation/Profile.js
+++ client/src/components/body/landingpage/citation/Profile.js
@@ -4,7 +4,7 @@ import { SocialIcon } from 'react-social-icons';
 class Profile extends Component{
     render(){
         const { profile_image, name, title, bio ,socials} = this.props;
-        
+
         return(
             
             <ul id={name} class="citation citation__profile_collumn portfolio-item ">
@@ -19,7 +19,7 @@ class Profile extends Component{
                 <li id={title} class="citation__title">{title}</li>
                 <li id="bio" class="citation__bio">{bio}</li>
                 <li id="social_media" >
-                    <ul class="row">
+                    <ul class="landingpage__row citation__social_row">
                         {socials.map((social,index)=>{
                             return <li class="citation__social_media" key={index}><SocialIcon url={social} target="__blank"/></li>
                         })}
--- client/src/components/body/landingpage/citation/profileInfomation.js
+++ client/src/components/body/landingpage/citation/profileInfomation.js
@@ -5,8 +5,34 @@ var profileInformation = [
         name: "Mark Sternefeld",
         title: "Developer",
         bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        linkedin:"mark-ferenc-sternefeld/",
-        github: "Mster115",
+        socials: ["https://google.com","https://www.linkedin.com/in/mark-ferenc-sternefeld/","https://github.com/Mster115"]
+    },
+    {
+        profile_image: mark_sternefeld_image,
+        name: "Mark Sternefeld",
+        title: "Developer",
+        bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        socials: ["https://www.linkedin.com/in/mark-ferenc-sternefeld/"]
+    },
+    {
+        profile_image: mark_sternefeld_image,
+        name: "Mark Sternefeld",
+        title: "Developer",
+        bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        socials: ["https://www.linkedin.com/in/mark-ferenc-sternefeld/","https://trello.com/b/JfaewHEi/accessibility-labs"]
+    },
+    {
+        profile_image: mark_sternefeld_image,
+        name: "Mark Sternefeld",
+        title: "Developer",
+        bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        socials: ["https://www.linkedin.com/in/mark-ferenc-sternefeld/","https://google.com","https://trello.com/b/JfaewHEi/accessibility-labs","https://github.com/Mster115"]
+    },
+    {
+        profile_image: mark_sternefeld_image,
+        name: "Mark Sternefeld",
+        title: "Developer",
+        bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
         socials: ["https://www.linkedin.com/in/mark-ferenc-sternefeld/","https://github.com/Mster115"]
     },
     {
@@ -14,45 +40,7 @@ var profileInformation = [
         name: "Mark Sternefeld",
         title: "Developer",
         bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        linkedin:"mark-ferenc-sternefeld/", 
-        github: "Mster115",
-        socials: ["https://www.linkedin.com/in/mark-ferenc-sternefeld/","https://github.com/Mster115"]
-    },
-    {
-        profile_image: mark_sternefeld_image,
-        name: "Mark Sternefeld",
-        title: "Developer",
-        bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        linkedin:"mark-ferenc-sternefeld/", 
-        github: "Mster115",
-        socials: ["https://www.linkedin.com/in/mark-ferenc-sternefeld/","https://github.com/Mster115"]
-    },
-    {
-        profile_image: mark_sternefeld_image,
-        name: "Mark Sternefeld",
-        title: "Developer",
-        bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        linkedin:"mark-ferenc-sternefeld/", 
-        github: "Mster115",
-        socials: ["https://www.linkedin.com/in/mark-ferenc-sternefeld/","https://github.com/Mster115"]
-    },
-    {
-        profile_image: mark_sternefeld_image,
-        name: "Mark Sternefeld",
-        title: "Developer",
-        bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        linkedin:"mark-ferenc-sternefeld/", 
-        github: "Mster115",
-        socials: ["https://www.linkedin.com/in/mark-ferenc-sternefeld/","https://github.com/Mster115"]
-    },
-    {
-        profile_image: mark_sternefeld_image,
-        name: "Mark Sternefeld",
-        title: "Developer",
-        bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        linkedin:"mark-ferenc-sternefeld/", 
-        github: "Mster115",
-        socials: ["https://www.linkedin.com/in/mark-ferenc-sternefeld/","https://github.com/Mster115"]
+        socials: ["https://www.linkedin.com/in/mark-ferenc-sternefeld/","https://github.com/Mster115","https://www.linkedin.com/in/mark-ferenc-sternefeld/","https://google.com","https://trello.com/b/JfaewHEi/accessibility-labs","https://github.com/Mster115"]
     }
     
 ]
