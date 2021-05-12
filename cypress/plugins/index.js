39b35a3c578411eddb923aa21ca06ea0633e5473
Saad Khan <sk6786@g.rit.edu>
2021-05-12 16:24:10 -0400

    skip e2e

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -19,112 +19,114 @@
  * @type {Cypress.PluginConfig}
  */
 module.exports = async(on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
-  const fs = require('fs');
-  const path = require('path');
-  const pathname = path.join(__dirname, '/../../server/database/models');
-  const Sequelize = require('../../server/node_modules/sequelize');
-  const withPassword = config.env.DB_PASS ? `:${config.env.DB_PASS}` : '';
-  const URI = `postgres://${config.env.DB_USER}${withPassword}@${config.env.DB_HOST}:${config.env.ENVIRONMENT === "dev" ? 5433:5432}/${config.env.DB_SCHEMA}`;
-  const database = async() => {
-    try {
-      const sequelize = new Sequelize(URI, {
-        dialect: 'postgres',
-        pool: {
-          max: 5,
-          min: 0,
-          idle: 10000
-        },
-        define: {
-          timestamps: false
-        }
-      })
-      const db = {};
-      await sequelize
-      .authenticate()
-      .then(() => {
-        console.log('Connection has been established successfully.');
-      })
-      .catch((err) => {
-        console.error('Unable to connect to the database:', err);
-      });
-      
-      const files = [];
-      const sortDir = (maniDir) => {
-        const folders = [];
-        const CheckFile = filePath => (fs.statSync(filePath).isFile());
-        const sortPath = (dir) => {
-          fs
-            .readdirSync(dir)
-            .filter(file => (file.indexOf(".") !== 0) && (file !== "index.js"))
-            .forEach((res) => {
-              const filePath = path.join(dir, res);
-              if (CheckFile(filePath)) {
-                files.push(filePath);
-              } else {
-                folders.push(filePath);
-              }
+  if (!Cypress.env("SKIP_E2E")) {
+    // `on` is used to hook into various events Cypress emits
+    // `config` is the resolved Cypress config
+    const fs = require('fs');
+    const path = require('path');
+    const pathname = path.join(__dirname, '/../../server/database/models');
+    const Sequelize = require('../../server/node_modules/sequelize');
+    const withPassword = config.env.DB_PASS ? `:${config.env.DB_PASS}` : '';
+    const URI = `postgres://${config.env.DB_USER}${withPassword}@${config.env.DB_HOST}:${config.env.ENVIRONMENT === "dev" ? 5433 : 5432}/${config.env.DB_SCHEMA}`;
+    const database = async () => {
+      try {
+        const sequelize = new Sequelize(URI, {
+          dialect: 'postgres',
+          pool: {
+            max: 5,
+            min: 0,
+            idle: 10000
+          },
+          define: {
+            timestamps: false
+          }
+        })
+        const db = {};
+        await sequelize
+            .authenticate()
+            .then(() => {
+              console.log('Connection has been established successfully.');
+            })
+            .catch((err) => {
+              console.error('Unable to connect to the database:', err);
             });
-        };
-        folders.push(maniDir);
-        let i = 0;
-        do {
-          sortPath(folders[i]);
-          i += 1;
-        } while (i < folders.length);
-      };
-      sortDir(pathname);
-      files
-        .forEach((file) => {
-          const model = require(file)(sequelize, Sequelize.DataTypes);
-          db[model.name] = model;
-        });
-        
-      Object.keys(db).forEach((modelName) => {
-        if (db[modelName].associate) {
-          db[modelName].associate(db);
-        }
-      });
 
-      db.sequelize = sequelize;
-      db.Sequelize = Sequelize;
-      
-      return db;
-     
-    } catch (err) {
-      console.log('error: ', err)
-    }
-  };
-  const db = await database();
-  on('task', {
-    
-    async userLabComplete({labid, userid}) {
-      return new Promise((resolve, reject) => {
-        db.Session.findOne({
-          where: {userid: userid}
-        }).then(res => {
-          db.UserLab.findOne({
-            where: {
-              usersessionid: res.usersessionid,
-              labid: labid
-            }
-          }).then(userlab => {
-            return resolve(userlab)
+        const files = [];
+        const sortDir = (maniDir) => {
+          const folders = [];
+          const CheckFile = filePath => (fs.statSync(filePath).isFile());
+          const sortPath = (dir) => {
+            fs
+                .readdirSync(dir)
+                .filter(file => (file.indexOf(".") !== 0) && (file !== "index.js"))
+                .forEach((res) => {
+                  const filePath = path.join(dir, res);
+                  if (CheckFile(filePath)) {
+                    files.push(filePath);
+                  } else {
+                    folders.push(filePath);
+                  }
+                });
+          };
+          folders.push(maniDir);
+          let i = 0;
+          do {
+            sortPath(folders[i]);
+            i += 1;
+          } while (i < folders.length);
+        };
+        sortDir(pathname);
+        files
+            .forEach((file) => {
+              const model = require(file)(sequelize, Sequelize.DataTypes);
+              db[model.name] = model;
+            });
+
+        Object.keys(db).forEach((modelName) => {
+          if (db[modelName].associate) {
+            db[modelName].associate(db);
+          }
+        });
+
+        db.sequelize = sequelize;
+        db.Sequelize = Sequelize;
+
+        return db;
+
+      } catch (err) {
+        console.log('error: ', err)
+      }
+    };
+    const db = await database();
+    on('task', {
+
+      async userLabComplete({labid, userid}) {
+        return new Promise((resolve, reject) => {
+          db.Session.findOne({
+            where: {userid: userid}
+          }).then(res => {
+            db.UserLab.findOne({
+              where: {
+                usersessionid: res.usersessionid,
+                labid: labid
+              }
+            }).then(userlab => {
+              return resolve(userlab)
+            })
           })
         })
-      })
-    },
-    
-    async userToSession(userid) {
-      return new Promise((resolve, reject) => {
-        db.Session.findOne({
-          where: {userid: userid}
-        }).then(session => {
-          return resolve(session.usersessionid)
-        })
-      });
-    }
-    
-  })
+      },
+
+      async userToSession(userid) {
+        return new Promise((resolve, reject) => {
+          db.Session.findOne({
+            where: {userid: userid}
+          }).then(session => {
+            return resolve(session.usersessionid)
+          })
+        });
+      }
+
+    })
+  }
 }
