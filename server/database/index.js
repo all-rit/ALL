37a7145777f3c5f82cb0ec5e7349c10eb325a5b5
Shantanav Saurav <ss9415@rit.edu>
2021-06-09 15:30:18 -0400

    Removing unnecessary package files

--- server/database/index.js
+++ server/database/index.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 const pathname = path.join(__dirname, 'models');
-const withPassword = process.env.DB_PASS ? `:${process.env.DB_PASS}` : '';
-const URI = `postgres://${process.env.DB_USER}${withPassword}@${process.env.DB_HOST}:${process.env.ENVIRONMENT === "dev"?5433:5432}/${process.env.DB_SCHEMA}`;
+const withPassword = process.env.POSTGRES_PASSWORD ? `:${process.env.POSTGRES_PASSWORD}` : '';
+const URI = `postgres://${process.env.POSTGRES_USER}${withPassword}@${process.env.DB_HOST}`;
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(URI, {
 	dialect: 'postgres',
