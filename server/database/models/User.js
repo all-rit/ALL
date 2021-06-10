0a3f6de8ac2baea8c225ccd139b4a3eec04ea6a0
Su Thit Thazin <st5626@g.rit.edu>
2021-06-10 11:45:03 -0400

    removed isInstructor from User table

--- server/database/models/User.js
+++ server/database/models/User.js
@@ -10,7 +10,6 @@ module.exports = (sequelize, DataTypes) => {
 			},
 			firstname: { type: DataTypes.TEXT },
 			lastinitial: { type: DataTypes.CHAR(1) },
-			isInstructor: { type: DataTypes.BOOLEAN },
 			email1: { type: DataTypes.TEXT },
 			email2: { type: DataTypes.TEXT },
 			image: { type: DataTypes.TEXT },  //todo get rid of this potentially
@@ -24,13 +23,11 @@ module.exports = (sequelize, DataTypes) => {
 	// 	User.create({
 	// 		firstname: 'Samuel',
 	// 		lastinitial: 'M',
-	// 		isInstructor: true,
 	// 		email1: 'sam@yahoo.com',
 	// 	});
 	// 	User.create({
 	// 		firstname: 'Su Thit',
 	// 		lastinitial: 'T',
-	// 		isInstructor: false,
 	// 		email1: 'stthazi@yahoo.com'
 	// 	})
 	// })
