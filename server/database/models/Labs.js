57b134ebac0a1d5af69c9698d5aef5ffea9856b7
Mark Sternfeld <smarky02@gmail.com>
2021-06-17 11:20:04 -0400

    lab and team info

--- server/database/models/Labs.js
+++ server/database/models/Labs.js
@@ -30,31 +30,46 @@ module.exports = (sequelize, DataTypes) => {
     //     Labs.create({
     //         labName: "Accessibility to Sound and Speech",
     //         category: "Accessibility",
+    //         learningObjectives: "Learn how organizations like the National Association of the Deaf (NAD) fought for easier access for hearing impaired individuals, increase your understanding through an interactive module about hearing impairments, watch related videos, and take a quiz to test your knowledge",
     //         shortDescription: "Learn about designing the web for the Deaf and Hard-of-Hearing community.",
+    //         fullDescription: "In this lab, you will learn why it is important to create software that is accessible to users with hearing impairments. You will learn how organizations like the National Association of the Deaf (NAD) fought for easier access for hearing impaired individuals, increase your understanding through an interactive module about hearing impairments, watch related videos, and take a quiz to test your knowledge.",
+    //         thumbnailImageURL: "https://all.rit.edu/static/media/ear.70ea9584.jpg",
     //         labURL: "https://all.rit.edu/Lab1/"
     //     });
     //     Labs.create({
     //         labName: "Accessibility to Color Blindness",
     //         category: "Accessibility",
+    //         learningObjectives:"Learn about different color vision deficiencies, increase your understanding through an interactive module about visual impairments, watch related videos, and take a quiz to test your knowledge",
     //         shortDescription: "Learn more about designing the web for color blind individuals.",
+    //         fullDescription:"In this lab, you will learn about why it is important to create software that is accessible to users with visual impairments. You will learn about different color vision deficiencies, increase your understanding through an interactive module about visual impairments, watch related videos, and take a quiz to test your knowledge.",
+    //         thumbnailImageURL:"https://all.rit.edu/static/media/eye.96e74818.jpg",
     //         labURL: "https://all.rit.edu/Lab2/"
     //     });
     //     Labs.create({
     //         labName: "Accessibility with Screen Readers",
     //         category: "Accessibility",
+    //         learningObjectives:"Learn about using alt tags, increase your understanding through an interactive module about screenreaders and alt tags, watch related videos, and take a quiz to test your knowledge.",
     //         shortDescription: "Learn more about screen readers.",
+    //         fullDescription:"In this lab, you will learn about why it is important to create software that is accessible to users who utilize screenreaders. You will learn about using alt tags, increase your understanding through an interactive module about screenreaders and alt tags, watch related videos, and take a quiz to test your knowledge.",
+    //         thumbnailImageURL:"https://all.rit.edu/static/media/braille.45ad9103.jpg",
     //         labURL: "https://all.rit.edu/Lab3/"
     //     });
     //     Labs.create({
     //         labName: "Accessibility to Dexterity",
     //         category: "Accessibility",
+    //         learningObjectives:"Learn about issues related to dexterity, increase your understanding through an interactive module about dexterity impairments, watch related videos, and take a quiz to test your knowledge",
     //         shortDescription: "Learn more about designing the web for individuals with motor and dexterity impairments.",
+    //         fullDescription:"In this lab, you will learn about why it is important to create software that is accessible to users with dexterity impairments. You will learn about issues related to dexterity, increase your understanding through an interactive module about dexterity impairments, watch related videos, and take a quiz to test your knowledge.",
+    //         thumbnailImageURL:"https://all.rit.edu/static/media/hand.f673a19e.jpg",
     //         labURL: "https://all.rit.edu/Lab4/"
     //     });
     //     Labs.create({
     //         labName: "Accessibility to Cognitive Impairments",
     //         category: "Accessibility",
+    //         learningObjectives:"Learn about using clear descriptive headings, handling time driven notifications, and creating informative form responses. Afterwards, you will watch related videos and take a quiz to test your knowledge.",
     //         shortDescription: "Learn more about designing the web for users with cognitive impairments.",
+    //         fullDescription:"In this lab, you will learn about why it is important to create software that is accessible to users who face cognitive impairments. You will learn about using clear descriptive headings, handling time driven notifications, and creating informative form responses. Afterwards, you will watch related videos and take a quiz to test your knowledge.",
+    //         thumbnailImageURL:"https://all.rit.edu/static/media/cognitiveimpairment.0805e6e7.jpg",
     //         labURL: "https://all.rit.edu/Lab5/"
     //     });
     // })
--- server/database/models/TeamMembers.js
+++ server/database/models/TeamMembers.js
@@ -19,5 +19,71 @@ module.exports = (sequelize, DataTypes) => {
         { tableName: 'team_members' }
     );
     TeamMembers.sync();
+    // TeamMembers.sync({
+    //     force: true
+    // }).then(function(){
+    //     TeamMembers.create({
+    //         firstName: "Daniel",
+    //         lastName: "Krutz",
+    //         title: "PI",
+    //         imageURL: "https://all.rit.edu/static/media/Professor_Krutz.1ac30542.jpg",
+    //         websiteURL: "https://danielkrutz.github.io/",
+    //     });
+    //     TeamMembers.create({
+    //         firstName: "Samuel",
+    //         lastName: "Malachowsky",
+    //         title: "Co-PI",
+    //         imageURL: "https://all.rit.edu/static/media/Professor_Malachowsky.cf390355.jpg",
+    //         websiteURL: "http://www.se.rit.edu/~samvse/",
+    //     });
+    //     TeamMembers.create({
+    //         firstName: "Saad",
+    //         lastName: "Khan",
+    //         title: "Project Manager",
+    //         imageURL: "https://all.rit.edu/static/media/Saad_Kahn.0588d381.jpg",
+    //         websiteURL: "https://www.linkedin.com/in/saad-khan23/",
+    //         datesActive: "2020-2021"
+    //     });
+    //     TeamMembers.create({
+    //         firstName: "Heather",
+    //         lastName: "Moses",
+    //         title: "Software Engineer",
+    //         imageURL: "https://all.rit.edu/static/media/Heather_Moses.16193a57.jpg",
+    //         websiteURL: "https://www.linkedin.com/in/heather-moses/",
+    //         datesActive: "2020-Present"
+    //     });
+    //     TeamMembers.create({
+    //         firstName: "Chistopher",
+    //         lastName: "Savan",
+    //         title: "Project Manager",
+    //         imageURL: "https://all.rit.edu/static/media/Christopher_Savan.7d22b015.jpg",
+    //         websiteURL: "https://www.linkedin.com/in/christophersavan/",
+    //         datesActive: "2020-2021"
+    //     });
+    //     TeamMembers.create({
+    //         firstName: "Su",
+    //         lastName: "Thit Thazin",
+    //         title: "Software Engineer",
+    //         imageURL: "https://all.rit.edu/static/media/Su_Thit_Thazin.1837a06b.jpg",
+    //         websiteURL: "https://www.linkedin.com/in/suthitthazin/",
+    //         datesActive: "2020-Present"
+    //     });
+    //     TeamMembers.create({
+    //         firstName: "Mark",
+    //         lastName: "Sternefeld",
+    //         title: "Software Engineer",
+    //         imageURL: "https://all.rit.edu/static/media/Mark_Sternefeld.403d4119.jpg",
+    //         websiteURL: "https://www.linkedin.com/in/mark-ferenc-sternefeld/",
+    //         datesActive: "2020-Present"
+    //     });
+    //     TeamMembers.create({
+    //         firstName: "Shantanav",
+    //         lastName: "Saurav",
+    //         title: "Software Engineer",
+    //         imageURL: "https://all.rit.edu/static/media/default_profile_image.d214dfdb.jpg",
+    //         websiteURL: "https://www.linkedin.com/in/shantanav/",
+    //         datesActive: "2021-Present"
+    //     });
+    // })
     return TeamMembers;
 };
