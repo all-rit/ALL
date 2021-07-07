63fa1711c26f822fb1be6ee955388bc3fc19edb3
Shantanav Saurav <ss9415@rit.edu>
2021-07-08 00:08:13 +0200

    Try and have server run over HTTPS

--- server/app.js
+++ server/app.js
@@ -7,11 +7,21 @@ const cookieParser = require('cookie-parser');
 const session = require('express-session')
 const passport = require('passport');
 const auth = require('./auth');
+const fs = require('fs');
+const https = require('https')
 
 const app = express();
 const port = process.env.PORT || 5005;
 
 const allowedOrigins = [process.env.CLIENT_URL, 'https://localhost:5005','https://localhost:3000', 'https://all.rit.edu'];
+
+const key = fs.readFileSync('/etc/letsencrypt/live/all.rit.edu/privkey.pem')
+const cert = fs.readFileSync('/etc/letsencrypt/live/all.rit.edu/fullchain.pem')
+
+const options = {
+  key: key,
+  cert: cert
+}
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -41,4 +51,10 @@ app.use(cors({
 
 app.use(require('./routes'));
 
-app.listen(port, () => console.log(`Listening on port ${port}!`));
+app.get('/', (req, res) => {
+  res.send('Test endpoint over https')
+})
+
+let server = https.createServer(options, app)
+
+server.listen(port, () => console.log(`Listening on port ${port}!`));
