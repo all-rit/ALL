4510c7180cebc0347d0ec886ff47e1413071279b
Su Thit Thazin <st5626@g.rit.edu>
2021-07-22 13:48:51 -0400

    minor formatting changes

--- client/src/components/body/profilepage/profile.js
+++ client/src/components/body/profilepage/profile.js
@@ -7,31 +7,29 @@ import InstructingGroups from "./instructingGroups";
 
 const Profile = (props) => {
     return (
-        <div>
+        <React.Fragment>
         {props.user?.firstname === null
             ?
             <h3>You are currently not logged in.</h3>
             :
             <div className="profile container">
                 <ProfileHeader user={props}/>
+                <br/>
 
                 <h4>My Enrolled Groups</h4>
                 <EnrolledCourses user={props.user}/>
-
-                <h4>In Progress Labs</h4>
                 <br/>
+                <h4>In Progress Labs</h4>
                 <div className="landingpage__row">
                     <LabGeneration actions={actions} progressState="IN_PROGRESS"/>
                 </div>
 
                 <h4>To-do Labs</h4>
-                <br/>
                 <div className="landingpage__row">
                     <LabGeneration actions={actions} progressState="NOT_STARTED"/>
                 </div>
 
                 <h4>Completed Labs</h4>
-                <br/>
                 <div className="landingpage__row">
                     <LabGeneration actions={actions} progressState="COMPLETED"/>
                 </div>
@@ -42,8 +40,8 @@ const Profile = (props) => {
 
             </div>
         }
-        </div>
+        </React.Fragment>
     );
-}
+};
 
 export default Profile;
