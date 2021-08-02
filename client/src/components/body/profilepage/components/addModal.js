fdc20969c2b65527a80fb26dc3f3ad0d751b6a37
Su Thit Thazin <st5626@g.rit.edu>
2021-08-02 14:29:14 -0400

    stubbed out 'create instructing group' modal

--- client/src/components/body/profilepage/components/addModal.js
+++ client/src/components/body/profilepage/components/addModal.js
@@ -1,5 +1,15 @@
 import React, { useState } from 'react';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import {
+    Button,
+    Modal,
+    ModalHeader,
+    ModalBody,
+    ModalFooter,
+    Form,
+    FormGroup,
+    Label,
+    Input,
+} from 'reactstrap';
 import AddIcon from "@material-ui/icons/Add";
 import Fab from "@material-ui/core/Fab";
 
@@ -18,10 +28,33 @@ const AddModal = (props) => {
                         <AddIcon />
                     </Fab>
                     <Modal isOpen={modal} toggle={toggle} className="add_instr_grp_modal">
-                        <ModalHeader>Update assigned labs</ModalHeader>
+                        <ModalHeader>Create an instructing group</ModalHeader>
                         <ModalBody>
-                            Group name field<br/>
-                            Choose labs to assign
+                            <Form>
+                                <FormGroup>
+                                    <Label for="groupName">Group name</Label>
+                                    <Input
+                                        type="text" name="groupName" id="groupName"
+                                        placeholder={"SWEN 344 Web Engineering Fall 2021"}
+                                    />
+                                </FormGroup>
+                                <Label for="assign-lab">Choose labs to assign</Label>
+                                <FormGroup check>
+                                    <Input type="checkbox" name="lab1" id="lab1"/>
+                                    <Label for="lab1" check>Sound and Speech</Label><br/>
+                                    <Input type="checkbox" name="lab2" id="lab2"/>
+                                    <Label for="lab2" check>Color Blindness</Label><br/>
+                                    <Input type="checkbox" name="lab3" id="lab3"/>
+                                    <Label for="lab3" check>Screen Readers</Label><br/>
+                                    <Input type="checkbox" name="lab4" id="lab4"/>
+                                    <Label for="lab4" check>Dexterity</Label><br/>
+                                    <Input type="checkbox" name="lab5" id="lab5"/>
+                                    <Label for="lab5" check>Cognitive Impairments</Label>
+                                </FormGroup>
+                            </Form>
+                            <Form>
+
+                            </Form>
                         </ModalBody>
                         <ModalFooter>
                             <Button color="primary" onClick={toggle}>Create Group</Button>
