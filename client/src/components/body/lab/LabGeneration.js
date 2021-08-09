4e0797d07cdc9010aec658cd33f3ba409cec5f1e
Su Thit Thazin <st5626@g.rit.edu>
2021-08-09 10:51:24 -0400

    put labinformation call back into labgeneration

--- client/src/components/body/lab/LabGeneration.js
+++ client/src/components/body/lab/LabGeneration.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Lab from "./lab";
+import LabService from "../../../services/LabService";
 
 function renderLabData(actions,labInfo,progressState, index, labRecord) {
     const {id,labName,shortDescription,thumbnailImageURL}= labInfo //destructuring
@@ -18,7 +19,20 @@ function renderLabData(actions,labInfo,progressState, index, labRecord) {
     );
 }
 const LabGeneration = (props)=>{
-  const { actions,progressState, labInformation, labids, labRecords }=props;
+  const { actions,progressState, labids, labRecords }=props;
+  const [ labInformation, setLabInformation] = useState([]);
+
+    useEffect(() => {
+        if(labInformation.length===0){
+            async function fetchGroups() {
+                return LabService.getAllLabs();
+            }
+            fetchGroups().then((data) => {
+                setLabInformation(data);
+            });
+
+        }
+    });
 
   if (progressState){
       if (progressState === "NOT_STARTED"){
--- client/src/components/body/profilepage/labs.js
+++ client/src/components/body/profilepage/labs.js
@@ -4,13 +4,11 @@ import LabGeneration from "../lab/LabGeneration";
 import {actions} from "../../../reducers/MainReducer";
 import UserService from "../../../services/UserService";
 import UserLabService from "../../../services/UserLabService";
-import LabService from "../../../services/LabService";
 
 const Labs = (props) => {
 
     const [ toDoLabs, setToDoLabs ] = useState(null);
     const [ labRecords, setLabRecords ] = useState(null);
-    const [ labInformation, setLabInformation] = useState([]);
 
     let inProgressLabs = [];
     let completedLabs = [];
@@ -29,12 +27,6 @@ const Labs = (props) => {
             fetchLabRecords().then((data) => {
                 setLabRecords(data);
             })
-            async function fetchLabs() {
-                return LabService.getAllLabs();
-            }
-            fetchLabs().then((data) => {
-                setLabInformation(data);
-            });
         }
     }, [props.user])
 
@@ -58,17 +50,17 @@ const Labs = (props) => {
                         <br/>
                         <h4>In Progress Labs</h4>
                         <div className="landingpage__row">
-                            <LabGeneration actions={actions} progressState="IN_PROGRESS" labInformation={labInformation} labRecords={inProgressLabs}/>
+                            <LabGeneration actions={actions} progressState="IN_PROGRESS" labRecords={inProgressLabs}/>
                         </div>
 
                         <h4>To-do Labs</h4>
                         <div className="landingpage__row">
-                            <LabGeneration actions={actions} progressState="NOT_STARTED" labInformation={labInformation} labids={toDoLabs}/>
+                            <LabGeneration actions={actions} progressState="NOT_STARTED" labids={toDoLabs}/>
                         </div>
 
                         <h4>Completed Labs</h4>
                         <div className="landingpage__row">
-                            <LabGeneration actions={actions} progressState="COMPLETED" labInformation={labInformation} labRecords={completedLabs}/>
+                            <LabGeneration actions={actions} progressState="COMPLETED" labRecords={completedLabs}/>
                         </div>
                     </>
             }
