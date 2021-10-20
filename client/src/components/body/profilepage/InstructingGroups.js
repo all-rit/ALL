1f649ba437dd3d00b008544ddd484daafb1efe8e
Su Thit Thazin <st5626@g.rit.edu>
2021-10-20 13:34:54 -0400

    fixing indentations

--- client/src/components/body/profilepage/InstructingGroups.js
+++ client/src/components/body/profilepage/InstructingGroups.js
@@ -18,61 +18,61 @@ const InstructingGroups = (props) => {
     }, [user]);
 
     return (
-        <div>
-        <div className="header_with_button">
-                    <h4>My Instructing Groups</h4>
-                    <AddModal addMode={"add_instr_grp"} user={props.user} setInstructingGroups={setInstructingGroups}/>
-        </div>
-        <div className="instructing-groups">
-            {
-                instructingGroups.length === 0 ?
-                    <p> You currently do not have any groups you are instructing.</p> :
-                    <>
-                        <Table>
-                            <thead>
-                            <tr>
-                                <th>Groups</th>
-                                <th>Assigned Labs</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            {instructingGroups.map((group, index) => (
-                                <tr key={index}>
-                                    <td className="bold">
-                                        <p className="bold">{group.groupName}</p>
-                                        <p className="bold">Invite Code: {group.code}</p>
-                                    </td>
-                                    <GroupDetails group={group} instructing={true} user={user}/>
-                                    <></>
+        <>
+            <div className="header_with_button">
+                <h4>My Instructing Groups</h4>
+                <AddModal addMode={"add_instr_grp"} user={props.user} setInstructingGroups={setInstructingGroups}/>
+            </div>
+            <div className="instructing-groups">
+                {
+                    instructingGroups.length === 0 ?
+                        <p> You currently do not have any groups you are instructing.</p> :
+                        <>
+                            <Table>
+                                <thead>
+                                <tr>
+                                    <th>Groups</th>
+                                    <th>Assigned Labs</th>
                                 </tr>
-                            ))}
-                            </tbody>
-                        </Table>
-                        {/* Mock of converting from using table to unordered list */}
-                        {/* <ul class="table">
-                            <ul class="table_header">
-                                <ul class="table_row">
-                                    <li>Groups</li>
-                                    <li>Assigned Labs</li>
-                                </ul>
-                            </ul>
-                            <ul class="table_body ">
+                                </thead>
+                                <tbody>
                                 {instructingGroups.map((group, index) => (
-                                    <ul class="table_row" key={index}>
-                                        <ul className="bold">
-                                            <li className="bold">{group.groupName}</li>
-                                            <li className="bold">Invite Code: {group.code}</li>
-                                        </ul>
-                                        <GroupDetails group={group} instructing={true}/>
+                                    <tr key={index}>
+                                        <td className="bold">
+                                            <p className="bold">{group.groupName}</p>
+                                            <p className="bold">Invite Code: {group.code}</p>
+                                        </td>
+                                        <GroupDetails group={group} instructing={true} user={user}/>
                                         <></>
-                                    </ul>
+                                    </tr>
                                 ))}
-                            </ul>
-                        </ul> */}
-                    </>
-            }
-        </div>
-        </div>
+                                </tbody>
+                            </Table>
+                            {/* Mock of converting from using table to unordered list */}
+                            {/* <ul class="table">
+                                <ul class="table_header">
+                                    <ul class="table_row">
+                                        <li>Groups</li>
+                                        <li>Assigned Labs</li>
+                                    </ul>
+                                </ul>
+                                <ul class="table_body ">
+                                    {instructingGroups.map((group, index) => (
+                                        <ul class="table_row" key={index}>
+                                            <ul className="bold">
+                                                <li className="bold">{group.groupName}</li>
+                                                <li className="bold">Invite Code: {group.code}</li>
+                                            </ul>
+                                            <GroupDetails group={group} instructing={true}/>
+                                            <></>
+                                        </ul>
+                                    ))}
+                                </ul>
+                            </ul> */}
+                        </>
+                }
+            </div>
+        </>
     )
 };
 
