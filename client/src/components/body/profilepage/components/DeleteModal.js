4e24d49c42cf79d02acea6294c7d4701970e9fde
Mark Sternfeld <smarky02@gmail.com>
2022-02-16 14:41:27 -0500

    delete modal fix

--- client/src/components/body/profilepage/components/DeleteModal.js
+++ client/src/components/body/profilepage/components/DeleteModal.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import GroupService from '../../../../services/GroupService';
 
 import { Button,Modal,ModalHeader,ModalBody,ModalFooter } from 'reactstrap';
