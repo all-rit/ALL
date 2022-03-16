bda42817e70f614723e1a312427e46171c78a15c
Mark Sternfeld <smarky02@gmail.com>
2022-03-16 15:57:02 -0400

    applicant component half done

--- client/src/components/exercise/lab6/pages/Employer/HiringCandidate.js
+++ client/src/components/exercise/lab6/pages/Employer/HiringCandidate.js
@@ -1,6 +1,8 @@
 import React, {useEffect } from "react";
 import { navigate } from "@reach/router";
 import {EXERCISE_PLAYING} from "../../../../../constants/lab6";
+import Applicant from "../../components/Applicant";
+import { Form } from "reactstrap";
 
 
 
@@ -16,19 +18,29 @@ const HiringCandidate = (props) =>{
     }
 
     return(
-        <div className="center-div">
-            <h2 class="playthrough__title">Choose A Candidate</h2>
-            <h2 class="cognitive_instructions">Click on a candidates picture to select</h2>
-            <h2 class="cognitive_instructions">Hiring for the job of “EMPLOYEE” at “MegaCorp Inc.”</h2>
+        <Form>
+            <div className="center-div">
+                <h2 className="playthrough__title" >Choose A Candidate</h2>
+                <h2 className="cognitive_instructions">Click on a candidates picture to select</h2>
+                <h2 className="cognitive_instructions">Hiring for the job of “EMPLOYEE” at “MegaCorp Inc.”</h2>
+                <div className="candidate__row">
+                    <Applicant type={"key"} gender={"Gender:"} age={"Age:"} years={"Years of Experience:"} availability={"Availability:"} pay={"Expected Pay:"} ai={"AI Recommendation:"}/>
+                    <Applicant type={"applicant"} gender={"gender"} age={"age"} years={"years of experience"} availability={"availability"} pay={"expected pay"} ai={"AI Recommendation"}/>
+                    <Applicant type={"applicant"} gender={"gender"} age={"age"} years={"years of experience"} availability={"availability"} pay={"expected pay"} ai={"AI Recommendation"}/>
+                    <Applicant type={"applicant"} gender={"gender"} age={"age"} years={"years of experience"} availability={"availability"} pay={"expected pay"} ai={"AI Recommendation"}/>
+                    <Applicant type={"applicant"} gender={"gender"} age={"age"} years={"years of experience"} availability={"availability"} pay={"expected pay"} ai={"AI Recommendation"}/>
+                </div>
+                
 
-            <button
-                className="btn btn-primary text-black btn-xl text-uppercase "
-                onClick = {handleContinue}
-                key="confirm"
-            >
-                Continue
-            </button>
-      </div>
+                <button
+                    className="btn btn-primary text-black btn-xl text-uppercase "
+                    onClick = {handleContinue}
+                    key="confirm"
+                >
+                    Continue
+                </button>
+            </div>
+        </Form>
     );
 }
 
