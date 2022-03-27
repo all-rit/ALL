ce4a551630af40745006a86641e920da109f1d15
Stormkiller-01 <jwedner0726@gmail.com>
2022-03-27 01:00:59 -0400

    previous messages are now showing, just not in textbox

--- client/src/components/imagine/pages/MatchLobby.js
+++ client/src/components/imagine/pages/MatchLobby.js
@@ -1,11 +1,50 @@
-import React from "react";
+import React, {Component} from "react";
 import { navigate } from "@reach/router";
 import { createText } from "@reach/router";
+import { Form,  Input, ListGroup} from "reactstrap";
+import { useState } from "react";
+
+import {InputGroup} from "reactstrap";
+import {Button} from "reactstrap";
+// import React, { useState } from 'react';
 
 const MatchLobby = (props) => {
 
     const handleNext = () => {
         navigate("/Imagine/TicTacToe");
+    }
+
+    const [chatText, setChatText] = useState("");
+    const [messages, setMessages] = useState([]);
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(chatText);
+        const date= {chatText}
+        if(chatText){
+            setMessages((ls)=>[...ls, date])
+            setChatText("");
+        }
+        // messages.push(chatText);
+        // setMessages([...messages, chatText]);
+    }
+
+    // const setChatText = () => {
+    //     navigate("");
+    // }
+
+
+    //class doesn't do anything yet, may not use later
+    class TextAppearanceInBox extends Component {
+        render() {
+            const { visible, inputRevealed } = this.props;
+            const message = inputRevealed ? chatText:chatText;
+    
+            if (!visible) return null;
+    
+            return message;
+        }
     }
 
     // ReactDOM.render(element, document.getElementById('root'));
@@ -16,6 +55,15 @@ const MatchLobby = (props) => {
     //     }
     // }
     return (
+
+        // <div className="mb-3">
+        //     <InputGroup.Prepend>
+        //     <Button variant="outline-primary">click me</Button>
+        //     </InputGroup.Prepend>
+        
+        // </InputGroup>
+        //     </div>
+
 
         <div className="container bottomSpace" >
             <div>
@@ -29,17 +77,60 @@ const MatchLobby = (props) => {
                 Continue
             </button>
 
-            <div className='chatbox'>
+            <ul>
+                {messages.forEach(msg=> <li>{msg}</li>)}
+            </ul>
+
+
+            <div className='chatbox' extends Component>
                 <div>
                     ChatBox
                 </div>
-                <form onSubmit={(e) => console.log(e.target.value)}>
-                    <input type="text" />
-                    <input type="submit" value="Submit" />
-                </form>
+                <Form onSubmit={handleSubmit}>
+                    <Input
+                        type= "text"
+                        placeholder="Enter Chat..."
+                        name= "chatbox"
+                        id= "chatbox"
+                        onChange={e => setChatText(e.target.value)}
+                        onClick={e => setChatText(e.target.value)}
+                        // {...TextAppearanceInBox}
+
+                    />
+                    <Input
+                        type= "submit"
+                        name= "submit"
+                        id= "submit"
+                        value= "Submit" 
+                    />
+                    {/* //have another box, that takes the input, then from chatbox onclick
+                    //input the text into the new box by declaring new variables. */}
+                </Form> 
+                
+                <div> Previous messages:
+                
+                {
+                    messages.map((a)=> <div>
+                        <li>{a.chatText}</li>
+                        </div>)
+                }
+             
+                </div>
+                
+
             </div>
+
+            
+
+            
         </div>
+
+        
+
+        
     )
+
+    
 
 
 }
