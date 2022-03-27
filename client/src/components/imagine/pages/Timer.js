8718775a9a61cfe0ef0d6c080f9cfc4ae8885361
fm8191 <fm8191@rit.edu>
2022-03-27 00:18:29 -0400

    a bit more

--- client/src/components/imagine/pages/Timer.js
+++ client/src/components/imagine/pages/Timer.js
@@ -8,11 +8,17 @@ class Timer extends Component{
     //State for timer information
     constructor(props){
         super(props)
+        const {seconds} = this.props;
+        this.state = {secondsLeft: seconds};
     }
 
     //Renderer for system
 
-    //Randomly generates a number for the timer
+    //Randomly generates a number for the timer??
+
+    //checkExpired()
+
+    //render()
 
 
 }
