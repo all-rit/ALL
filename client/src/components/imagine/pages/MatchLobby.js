25bb6e52ef5c4e77352b8725ef20fff9106819b3
KyleMesserle <klm3580@rit.edu>
2022-03-27 13:31:03 -0400

    PlayerBoard

--- client/src/components/imagine/pages/MatchLobby.js
+++ client/src/components/imagine/pages/MatchLobby.js
@@ -1,5 +1,8 @@
 import React from "react";
+import "./App.css"
 import { navigate } from "@reach/router";
+import { Form, Row } from "reactstrap";
+
 
 const MatchLobby = (props)=>{
 
@@ -11,8 +14,90 @@ const MatchLobby = (props)=>{
         
         <div className="container bottomSpace" >
             <div>
-                MatchLobby
+                Match Lobby 
             </div>
+
+            <div className="PlayerBoard">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>NAME</th>
+                            <th>SCORE</th>
+                            <th>STATUS</th>
+                            <th>BIAS</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>User</td>
+                            <td>5/3/2</td>
+                            <td>Waiting</td>
+                            <td>None</td>
+                        </tr>
+                    </tbody>
+                    <tbody>
+                        <tr>
+                            <td>Teammate</td>
+                            <td>5/2/3</td>
+                            <td>In Game</td>
+                            <td>None</td>
+                        </tr>
+                    </tbody>
+                    <tbody>
+                        <tr>
+                            <td>Teammate</td>
+                            <td>9/0/1</td>
+                            <td>Waiting</td>
+                            <td>Bias</td>
+                        </tr>
+                    </tbody>
+                    <tbody>
+                        <tr>
+                            <td>Teammate</td>
+                            <td>2/4/4</td>
+                            <td>In Game</td>
+                            <td>None</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+                    <div className="PlayerBoard2">
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>Opponent</td>
+                            <td>2/5/5</td>
+                            <td>Waiting</td>
+                            <td>None</td>
+                        </tr>
+                    </tbody>
+                    <tbody>
+                        <tr>
+                            <td>Opponent</td>
+                            <td>2/8/2</td>
+                            <td>In Game</td>
+                            <td>None</td>
+                        </tr>
+                    </tbody>
+                    <tbody>
+                        <tr>
+                            <td>Opponent</td>
+                            <td>2/3/3</td>
+                            <td>In Game</td>
+                            <td>None</td>
+                        </tr>
+                    </tbody>
+                    <tbody>
+                        <tr>
+                            <td>Opponent</td>
+                            <td>3/5/0</td>
+                            <td>Waiting</td>
+                            <td>Bias</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
             <button
               className="btn btn-primary text-black btn-xl text-uppercase "
               onClick = {handleNext}
@@ -20,7 +105,7 @@ const MatchLobby = (props)=>{
             >
                 Continue
             </button>
-        </div>
+            </div>
     )
 }
 
