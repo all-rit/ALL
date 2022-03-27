6a3797ce4c677d7df3450a007c958e881e405380
fm8191 <fm8191@rit.edu>
2022-03-26 23:37:13 -0400

    start of timer component

---
+++ client/src/components/imagine/pages/Timer.js
@@ -0,0 +1,18 @@
+import { Component } from "react";
+
+/**
+ * Component for the timer used to penalize
+ */
+class Timer extends Component{
+
+    //State for timer information
+    constructor(props){
+        super(props)
+    }
+
+    //Renderer for system
+
+    //Randomly generates a number for the timer
+
+
+}
