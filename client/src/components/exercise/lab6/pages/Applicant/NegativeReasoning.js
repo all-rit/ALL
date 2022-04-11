03a351b0a0f8bc4550a69adcd0e346174e39368a
Mark Sternfeld <smarky02@gmail.com>
2022-04-11 18:11:10 -0400

    fixed issues fixed avatar selection

---
+++ client/src/components/exercise/lab6/pages/Applicant/NegativeReasoning.js
@@ -0,0 +1,36 @@
+import React, {useEffect } from "react";
+import { navigate } from "@reach/router";
+import {EXERCISE_PLAYING} from "../../../../../constants/lab6";
+
+
+const NegativeReasoning = (props) =>{
+    const {actions} = props;
+
+    useEffect(()=>{
+        actions.updateState(EXERCISE_PLAYING);
+    },[actions]);
+
+    const handleContinue = () =>{
+        navigate("/Lab6/Exercise/EmployerStart");
+    }
+
+    return(
+        <div className="center-div">
+            <h2 class="playthrough__title">Why you were not chosen:</h2>
+            <div className="playthrough__sentence">
+            The reason you were not selected was because there were an overwhelming amount of applicant and an AI was used to help pare down the number of applications. 
+            Under normal circumstances a human would have made the decisions.
+            </div>
+            
+            <button
+                className="btn btn-primary text-black btn-xl text-uppercase "
+                onClick = {handleContinue}
+                key="confirm"
+            >
+                Continue
+            </button>
+      </div>
+    );
+}
+
+export default NegativeReasoning;
