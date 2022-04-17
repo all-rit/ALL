ea5c786c764da40a3304406580bd1d963fc66f74
Stormkiller-01 <jwedner0726@gmail.com>
2022-04-17 19:31:21 -0400

    added var arrays and random selections, but attributes are consistent across all avatars

--- client/src/components/body/lab/GridImages/GridImages.js
+++ client/src/components/body/lab/GridImages/GridImages.js
@@ -68,6 +68,72 @@ const GridImages = (props) => {
         "tw-cursor-pointer tw-w-full tw-rounded tw-max-w-full tw-h-auto ": true,
     });
 
+    var TopType = ["NoHair", "ShortHairShaggyMullet", "EyePatch", "Hat", "Hijab", 
+    "Turban", "WinterHat1", "WinterHat2",  "WinterHat3", "LongHairBob", 
+    "LongHairBigHair", "LongHairCurly", "LongHairCurvy", "LongHairFro", 
+    "LongHairFroBand", "LongHairNotTooLong", "LongHairMiaWallace", 
+    "LongHairStraightStrand", "ShortHairDreads01", "ShortHairDreads02", 
+    "ShortHairFrizzle", "ShortHairShaggyMullet", "ShortHairShortCurly", 
+    "ShortHairShortFlat", "ShortHairShortRound", "ShortHairShortWaved", 
+    "ShortHairSides", "ShortHairTheCaesar", "ShortHairTheCaesarSidePart"]
+
+    var randomTopType = TopType[Math.floor(Math.random()*TopType.length)];
+
+    var Accessories = ["Blank", "Kurt", "Prescription01", "Prescription02", 
+    "Round", "Sunglasses", "Wayfarers"]
+
+    var randomAccessories = Accessories[Math.floor(Math.random()*Accessories.length)];
+
+    
+    var HairColors = ["Auburn", "Black", "Blonde", "BlondeGolden", "Brown", "BrownDark",
+   "PastelPink"]
+
+   var randomHairColors = HairColors[Math.floor(Math.random()*HairColors.length)];
+
+   var FacialHairs = ["Blank", "BeardMedium", "BeardLight", "BeardMajestic", 
+   "MoustacheFancy", "MoustacheMagnum"]
+
+   var randomFacialHairs =  FacialHairs[Math.floor(Math.random()*FacialHairs.length)];
+
+   /**
+     * Is the same as facial hair colors, and wish both hairs to be consistent
+     */
+//    var FacialHairColors = ["Auburn", "Black", "Blonde", "BlondeGolden", "Brown", "BrownDark",
+//    "PastelPink"]
+
+
+    var Clothes = ["BlazerShirt", "BlazerSweater", "CollarSweater", "GraphicShirt", 
+    "Hoodie", "Overall", "ShirtCrewNeck", "ShirtScoopNeck", "ShirtVNeck"]
+
+
+    var randomClothes = Clothes[Math.floor(Math.random()*Clothes.length)];
+
+
+    var ClothesColor = ["Black", "Blue02", "Blue03", "Gray01", "Gray02", "Heather", 
+    "PastelBlue", "PastelGreen", "PastelOrange", "PastelRed", "PastelYellow", "Pink",
+    "Red", "White"]
+
+    var randomClothesColor = ClothesColor[Math.floor(Math.random()*ClothesColor.length)];
+
+    
+
+    var Eyes = ["Default", "Happy", "Wink"]
+
+    var randomEye = Eyes[Math.floor(Math.random()*Eyes.length)];
+
+    var Eyebrow = ["Default", "DefaultNatural", "FlatNatural", "UpDown", "UpDownNatural"]
+
+    var randomEyebrow = Eyebrow[Math.floor(Math.random()*Eyebrow.length)];
+
+    var Mouth = ["Default", "Serious", "Smile", "Twinkle"]
+
+    var randomMouth= Mouth[Math.floor(Math.random()*Mouth.length)];
+
+    var Skin = ["Tanned", "Brown", "DarkBrown", "Black", "Pale", "Light"]
+
+    var randomSkin= Skin[Math.floor(Math.random()*Skin.length)];
+
+
     return (
         <>
             <div className="moduleContainer">
@@ -86,18 +152,23 @@ const GridImages = (props) => {
                                     <Avatar
                                         className={id.includes(data.id) ? 'tw-opacity-50 tw-border-double tw-border-8 tw-max-w-full tw-h-auto' : 'tw-max-w-full tw-h-auto'} alt={data.name} 
                                         avatarStyle='Circle'
-                                        topType={data.avatarAttributes.topType}
-                                        accessoriesType={data.avatarAttributes.accessoriesType}
-                                        hairColor={data.avatarAttributes.hairColor}
-                                        facialHairType={data.avatarAttributes.facialHairType}
-                                        facialHairColor={data.avatarAttributes.facialHairColor}
-                                        clotheType={data.avatarAttributes.clotheType}
-                                        clotheColor={data.avatarAttributes.clotheColor}
-                                        graphicType={data.avatarAttributes.graphicType}
-                                        eyeType={data.avatarAttributes.eyeType}
-                                        eyebrowType={data.avatarAttributes.eyebrowType}
-                                        mouthType={data.avatarAttributes.mouthType}
-                                        skinColor={data.avatarAttributes.skinColor}
+                                        topType={randomTopType}
+                                        accessoriesType={randomAccessories}
+                                        hairColor={randomHairColors}
+                                        facialHairType={randomFacialHairs}
+                                        facialHairColor={randomHairColors}
+                                        clotheType={randomClothes}
+                                        clotheColor={randomClothesColor}
+                                        
+                                        /**
+                                         * Decided in meeting to not use graphic type. i.e. Cumbia
+                                         */
+                                        // graphicType={data.avatarAttributes.graphicType}
+
+                                        eyeType={randomEye}
+                                        eyebrowType={randomEyebrow}
+                                        mouthType={randomMouth}
+                                        skinColor={randomSkin}
 
                                     />
                                 </div>
