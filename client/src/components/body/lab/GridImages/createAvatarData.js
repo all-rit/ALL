fd58897442514cb1ba51afa2bc3a394841af27bb
Mark Sternfeld <smarky02@gmail.com>
2022-04-20 15:06:44 -0400

    cleanedup

--- client/src/components/body/lab/GridImages/createAvatarData.js
+++ client/src/components/body/lab/GridImages/createAvatarData.js
@@ -1,48 +1,3 @@
-    // const fetch = require('isomorphic-fetch');
-
-    // async function fetchData(url) {
-    //   try {
-    //     const response = await fetch(url, { mode: 'no-cors' });
-    //     if (!response.ok) {
-    //       throw new Error('Network response was not ok');
-    //     }
-    //     return response.json();
-    //   } catch (error) {
-    //     console.error('Unable to fetch data:', error);
-    //   }
-    // }
-
-    // function fetchNames(nameType) {
-    //   return fetchData(`https://www.randomlists.com/data/names-${nameType}.json`);
-    // }
-
-    // function pickRandom(list) {
-    //   return list[Math.floor(Math.random() * list.length)];
-    // }
-
-    // async function generateName(gender) {
-    //   try {
-    //     const response = await Promise.all([
-    //       fetchNames(gender || pickRandom(['male', 'female'])),
-    //       fetchNames('surnames')
-    //     ]);
-
-    //     const [firstNames, lastNames] = response;
-
-    //     const firstName = pickRandom(firstNames.data);
-    //     const lastName = pickRandom(lastNames.data);
-
-    //     return `${firstName} ${lastName}`;
-    //   } catch(error) {
-    //     console.error('Unable to generate name:', error);
-    //   }
-    // }
-
-
-
-
-
-
 const createAvatarData = (avatarNumber) =>{
     function generateName(){
         return firstName[Math.floor(Math.random()*firstName.length)]+" "+ lastNames[Math.floor(Math.random()*lastNames.length)];
@@ -62,6 +17,7 @@ const createAvatarData = (avatarNumber) =>{
         "Silvia",
         "Carmen",
         "Teri",
+        "Jaden",
         "Bernadette",
         "Horace",
         "Lois",
