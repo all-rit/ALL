10d2e03eb1be437fe435006fcf8d88c5c379d76e
Stormkiller-01 <jwedner0726@gmail.com>
2022-05-19 13:43:46 -0400

    changed AIAnalysisQuestions styling

--- client/src/components/exercise/lab6/pages/Applicant/AIAnalysisQuestions.js
+++ client/src/components/exercise/lab6/pages/Applicant/AIAnalysisQuestions.js
@@ -28,46 +28,129 @@ const AIAnalysisQuestions = (props) =>{
        * Need to increase font, and align the checkboxes!!
        */
 
-     
+       const onFormSubmit = e => {
+        e.preventDefault()
+        // This cursed line of code will:
+        //  1. Grab the form with e.target
+        //  2. Create an instance of a FormData object from ReactStrap using the form data
+        //  3. Get the fields in that FormData object
+        //  4. Create a new object from those entries, our new object will be an Object and not FormData
+        // Essentially, this is just a really long winded way of casting FormData to Object, since
+        // apparently that's necessary despite this not being an object-oriented or strongly typed language
+        // Please refactor this eventually, for the love of all that is holy
+        const formData = Object.fromEntries((new FormData(e.target)).entries())
+        let survey=[]
+        for (const [key,value] of Object.entries(formData)){
+            if(value==="on"){
+                survey.push(key)
+            }
+        }
+        console.log(survey)
+        if(survey.length!==0){
+            handleContinue()
+        }
+    }
+
+    return(
+        <Form onSubmit={onFormSubmit}>
+        <div className="center-div">
+            <h2 class="cognitive_instructions">Which of these attributes do you think that the AI was looking for in this exercise in order to deny someone?</h2>
+            
+            <div className="analysis__questions">
+                <FormGroup check >
+                    <Label for="gender" check>
+                        <Input id="gender" name="gender" type="checkbox" className="analysis__checkbox"/>
+                        <div className="analysis__question">
+                            Gender
+                        </div>
+                    </Label>
+                </FormGroup>
+                <FormGroup check >
+                    <Label for="availability" check>
+                        <Input id="availability" name="availability" type="checkbox" className="analysis__checkbox" /> 
+                        <div className="analysis__question">
+                            Availability
+                        </div>
+                    </Label>
+                </FormGroup>
+                <FormGroup check >
+                    <Label for="facialhair" check>
+                        <Input id="facialhair" name="facialhair" type="checkbox" className="analysis__checkbox"/>
+                        <div className="analysis__question">
+                            Age
+                        </div>
+                    </Label>
+                </FormGroup>
+                <FormGroup check >
+                    <Label for="pay" check>
+                        <Input id="pay" name="pay" type="checkbox" className="analysis__checkbox"/>
+                        <div className="analysis__question">
+                            Expected Pay
+                        </div>
+                    </Label>
+                </FormGroup>
+                <FormGroup check >
+                    <Label for="years" check>
+                        <Input id="years" name="years" type="checkbox" className="analysis__checkbox"/>
+                        <div className="analysis__question">
+                            Years of Experience
+                        </div>
+                    </Label>
+                </FormGroup>
+            </div>
+
+            <button
+                className="btn btn-primary text-black btn-xl text-uppercase "     
+                type="submit"
+                key="confirm"
+            >
+                Submit
+            </button>
+      </div>
+      </Form>
+    );
+}
+
+export default AIAnalysisQuestions;
   
-        return(
-          <div className="center-div">
-              <h2 class="playthrough__title">Which of these attributes do you believe were reasons why you were not selected:</h2>
+//         return(
+//           <div className="center-div">
+//               <h2 class="playthrough__title">Which of these attributes do you believe were reasons why you were not selected:</h2>
   
-              <div>
-          <input id="box1" onChange={checkValue} type="checkbox" value="Box1"/>
-          <label htmlFor="#box1">Gender</label>
-        </div>
-        <div>
-          <input id="box2" onChange={checkValue} type="checkbox" value="Box2"/>
-          <label htmlFor="#box2">Years of Experience</label>
-        </div>
-        <div>
-          <input id="box3" onChange={checkValue} type="checkbox" value="Box3"/>
-          <label htmlFor="#box3">Physical Appearance</label>
-        </div>
-        <div>
-          <input id="box4" onChange={checkValue} type="checkbox" value="Box4"/>
-          <label htmlFor="#box4">Age</label>
-        </div>
-        <div>
-          <input id="box5" onChange={checkValue} type="checkbox" value="Box5"/>
-          <label htmlFor="#box4">College Major</label>
-        </div>
+//               <div>
+//           <input id="box1" onChange={checkValue} type="checkbox" value="Box1"/>
+//           <label htmlFor="#box1">Gender</label>
+//         </div>
+//         <div>
+//           <input id="box2" onChange={checkValue} type="checkbox" value="Box2"/>
+//           <label htmlFor="#box2">Years of Experience</label>
+//         </div>
+//         <div>
+//           <input id="box3" onChange={checkValue} type="checkbox" value="Box3"/>
+//           <label htmlFor="#box3">Physical Appearance</label>
+//         </div>
+//         <div>
+//           <input id="box4" onChange={checkValue} type="checkbox" value="Box4"/>
+//           <label htmlFor="#box4">Age</label>
+//         </div>
+//         <div>
+//           <input id="box5" onChange={checkValue} type="checkbox" value="Box5"/>
+//           <label htmlFor="#box4">College Major</label>
+//         </div>
   
       
-        <button
-                  className="btn btn-primary text-black btn-xl text-uppercase "
-                  onClick = {handleContinue}
-                  key="confirm"
-              >
-                  Submit
-              </button>
-        </div>
-      );
-  }
+//         <button
+//                   className="btn btn-primary text-black btn-xl text-uppercase "
+//                   onClick = {handleContinue}
+//                   key="confirm"
+//               >
+//                   Submit
+//               </button>
+//         </div>
+//       );
+//   }
 
-  export default AIAnalysisQuestions;
+
 
     // const {actions} = props;
 
