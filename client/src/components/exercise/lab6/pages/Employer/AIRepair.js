a26f67dc59204f63a8ed1ae30089ca44fcec3d21
Stormkiller-01 <jwedner0726@gmail.com>
2022-06-24 15:19:16 -0400

    adding table to AIRepair page

--- client/src/components/exercise/lab6/pages/Employer/AIRepair.js
+++ client/src/components/exercise/lab6/pages/Employer/AIRepair.js
@@ -1,6 +1,10 @@
 import React, {useEffect } from "react";
 import { navigate } from "@reach/router";
 import {EXERCISE_PLAYING} from "../../../../../constants/lab6";
+import "C:/Users/MrRob/ALL/ALL/client/src/components/quiz/App.css";
+
+
+import Applicant from "../../components/Applicant";
 
 const AIRepair = (props) =>{
     const {actions} = props;
@@ -13,16 +17,59 @@ const AIRepair = (props) =>{
         navigate("/Lab6/Exercise/FixedHiringCandidate");
     }
 
+
+    const data = [
+        {Characteristic: " ", Inclusion: " ", Weight: " "},
+        {Characteristic: "Gender", Inclusion: "No",  Weight: "X"},
+        {Characteristic: "Years of Experience", Inclusion: "No", Weight: "X"},
+        {Characteristic: "Availability", Inclusion: "Yes", Weight: "2/10"},
+        {Characteristic: "Salary", Inclusion: "Yes", Weight: "2/10"},
+        {Characteristic: "Age", Inclusion: "No", Weight: "X"},
+        {Characteristic: "Appearance", Inclusion: "Yes", Weight: "6/10"},
+        ]
+
+
     return(
         <div className="center-div">
-            <h2 class="playthrough__title">Now let’s repair the problem in the AI</h2>
+            <h2 class="playthrough__title">Repair the AI</h2>
+            <div className="center-div">
+            <div className="playthrough__sentence">
+            Select row, adjust inclusion of attribute, then adjust weight to determine how significant each attribute is to AI selection.
+            </div>
+        </div>
+
+
+        <div className= "App">
+            <table>
+                <tr>
+                    <th>Characteristic</th>
+                    <th>Inclusion</th>
+                    <th>Weight</th>
+                </tr>
+                {data.map((val, key) => {
+                    return (
+                    <tr key={key}>
+                        <td>{val.Characteristic}</td>
+                        <td>{val.Inclusion}</td>
+                        <td>{val.Weight}</td>
+                    </tr>
+                )
+            })}
+            </table>
+            </div>
+          
+        
+
+
+
+
 
             <button
                 className="btn btn-primary text-black btn-xl text-uppercase "
                 onClick = {handleStart}
                 key="start"
             >
-                Continue
+                Redo With Repair
             </button>
       </div>
     );
