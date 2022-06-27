c18a77ea56f5dd08126dc43ba6a6812fec13874c
Stormkiller-01 <jwedner0726@gmail.com>
2022-06-27 16:01:29 -0400

    repair section, a little buggy, editable table

--- client/src/components/exercise/lab6/pages/Employer/AIRepair.js
+++ client/src/components/exercise/lab6/pages/Employer/AIRepair.js
@@ -2,6 +2,7 @@ import React, {useEffect } from "react";
 import { navigate } from "@reach/router";
 import {EXERCISE_PLAYING} from "../../../../../constants/lab6";
 import "C:/Users/MrRob/ALL/ALL/client/src/components/quiz/App.css";
+import ReactDOM from 'react-dom';
 
 
 import Applicant from "../../components/Applicant";
@@ -17,6 +18,129 @@ const AIRepair = (props) =>{
         navigate("/Lab6/Exercise/FixedHiringCandidate");
     }
 
+    const itemStyle = {
+        padding: '0px',
+      position: 'relative',
+      height: '26px',
+      display: 'flex',
+      alignItems: 'center'
+  };
+  
+  const textStyle = {
+        ...itemStyle,
+        padding: '0px 4px',
+        height: '26px',
+        fontFamily: 'Arial',
+        fontSize: '13px'
+  };
+  
+  const inputStyle = {
+        padding: '0',
+      position: 'absolute',
+      left: '2px',
+      top: '2px',
+      right: '45px',
+      bottom: '2px',
+        minWidth: '20px',
+        fontFamily: 'Arial',
+        fontSize: '13px'
+  };
+  
+  const buttonStyle = {
+      position: 'absolute',
+      top: '2px',
+      right: '2px',
+      bottom: '2px',
+      width: '40px'
+  };
+
+  const Cell = React.memo(({value, mode, onChange}) => {
+    const [localMode, setLocalMode] = React.useState(mode ?? 'read');
+    const[localValue, setLocalValue] = React.useState(value ?? ' ');
+    React.useEffect(() => setLocalMode(mode ?? 'read'), [mode]);
+    React.useEffect(() => setLocalValue(value ?? ''), [value]);
+    if (localMode ===  'edit') {
+        const handleInputChange = (e) => setLocalValue(e.target.value);
+        const handleSaveClick = () => {
+            setLocalMode('read');
+            onChange?.(localValue);
+        };
+        return (
+            <div style={itemStyle}>
+                <input type = "text"
+                value={localValue}
+                style={inputStyle}
+                onChange={handleInputChange} />
+            <button style= {buttonStyle} onClick={handleSaveClick}>Ok</button>
+            </div>
+            
+        );
+    }
+    if (localMode === 'read') {
+        const handleEditClick = () => {
+            setLocalMode('edit');
+        };
+        return (
+            <div style={textStyle} onClick= {handleEditClick}>{localValue}</div>
+        );
+    }
+    return null;
+
+  });
+
+
+  const tdStyle = {
+    padding: '1px',
+    border: '1px solid black',
+};
+
+const optionStyle = {
+    ...tdStyle,
+  padding: '2px 2px',
+    width: '30px'
+};
+
+
+// recheck this part for tomorrow
+
+    const Row = React.demo(({ mode, columns, data, onChange, onDelete}) => {
+        const handleDeleteClick = () => onDelete?.();
+        return (
+        <tr>
+          {columns.map(({path}, columnIndex) => {
+              const handleChange = value => {
+                  if (onChange) {
+                      const changedData = { ...data, [path]: value };
+                      onChange(columnIndex, changedData);
+                  }
+              };
+              return (
+                  <td key={path} style={tdStyle}>
+                    <Cell 
+                      mode={mode}
+                      value={data[path]} 
+                      onChange={handleChange} 
+                    />
+                  </td>
+              );
+          })}
+          <td style={optionStyle}>
+            <button onClick={handleDeleteClick}>Delete</button>
+          </td>
+        </tr>
+    );
+});
+
+
+
+            </td>
+
+
+            )
+                )}
+            </tr>
+        )
+    }
 
     const data = [
         {Characteristic: " ", Inclusion: " ", Weight: " "},
@@ -41,11 +165,13 @@ const AIRepair = (props) =>{
 
         <div className= "App">
             <table>
-                <tr>
-                    <th>Characteristic</th>
-                    <th>Inclusion</th>
-                    <th>Weight</th>
-                </tr>
+                <thead>
+                    <tr>
+                        <th>Characteristic</th>
+                        <th>Inclusion</th>
+                        <th>Weight</th>
+                    </tr>
+                </thead>
                 {data.map((val, key) => {
                     return (
                     <tr key={key}>
@@ -75,4 +201,5 @@ const AIRepair = (props) =>{
     );
 }
 
-export default AIRepair;
+
+export default AIRepair; 
