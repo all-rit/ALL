497c1e0cec1419f54011a58d2c21066bc7cb5811
Stormkiller-01 <jwedner0726@gmail.com>
2022-06-28 15:41:47 -0400

    ai repair table

--- client/src/components/exercise/lab6/pages/Employer/AIRepair.js
+++ client/src/components/exercise/lab6/pages/Employer/AIRepair.js
@@ -7,16 +7,19 @@ import ReactDOM from 'react-dom';
 
 import Applicant from "../../components/Applicant";
 
-const AIRepair = (props) =>{
-    const {actions} = props;
+// const AIRepair = (props) =>{
+//     const {actions} = props;
 
-    useEffect(()=>{
-        actions.updateState(EXERCISE_PLAYING)
-    },[actions]);
+//     useEffect(()=>{
+//         actions.updateState(EXERCISE_PLAYING)
+//     },[actions]);
 
     const handleStart = () =>{
         navigate("/Lab6/Exercise/FixedHiringCandidate");
     }
+
+    //using dirask create dynamic editable table post to then edit to only allow one column to be editable
+    //link: https://dirask.com/posts/React-create-dynamic-editable-table-DNKArp
 
     const itemStyle = {
         padding: '0px',
@@ -48,11 +51,13 @@ const AIRepair = (props) =>{
   
   const buttonStyle = {
       position: 'absolute',
-      top: '2px',
+      top: '0px',
       right: '2px',
-      bottom: '2px',
+      bottom: '0px',
       width: '40px'
   };
+
+  
 
   const Cell = React.memo(({value, mode, onChange}) => {
     const [localMode, setLocalMode] = React.useState(mode ?? 'read');
@@ -103,8 +108,8 @@ const optionStyle = {
 
 // recheck this part for tomorrow
 
-    const Row = React.demo(({ mode, columns, data, onChange, onDelete}) => {
-        const handleDeleteClick = () => onDelete?.();
+    const Row = React.memo(({ mode, columns, data, onChange}) => {
+      
         return (
         <tr>
           {columns.map(({path}, columnIndex) => {
@@ -124,65 +129,196 @@ const optionStyle = {
                   </td>
               );
           })}
-          <td style={optionStyle}>
-            <button onClick={handleDeleteClick}>Delete</button>
-          </td>
+        
         </tr>
     );
 });
 
 
 
-            </td>
+    const tableStyle = {
+        border: '1px solid black',
+        borderCollapse: 'collapse', 
+        width: '100%',
+        backgroundColor: 'grey'
 
-
-            )
-                )}
-            </tr>
-        )
     }
 
-    const data = [
-        {Characteristic: " ", Inclusion: " ", Weight: " "},
-        {Characteristic: "Gender", Inclusion: "No",  Weight: "X"},
-        {Characteristic: "Years of Experience", Inclusion: "No", Weight: "X"},
-        {Characteristic: "Availability", Inclusion: "Yes", Weight: "2/10"},
-        {Characteristic: "Salary", Inclusion: "Yes", Weight: "2/10"},
-        {Characteristic: "Age", Inclusion: "No", Weight: "X"},
-        {Characteristic: "Appearance", Inclusion: "Yes", Weight: "6/10"},
-        ]
+
+    const Table = React.memo(({id, columns, data, onChange}) => {
+        const [addedIndex, setAddedIndex] = React.useState();
+       
+        return (
+
+            <>
+                <h2 className="playthrough__title">Repair the AI</h2>
+                <div className="center-div">
+                    <div className="playthrough__sentence">
+                        Select row, adjust inclusion of attribute, then adjust weight to determine how significant each attribute is to AI selection.
+                    </div>
+                </div>
+            
+                <div>
+                    <table style={tableStyle}>
+                        <tbody>
+                            <tr>
+                                {columns.map(({ path, name }) => (
+                                    <th key={path} style={tdStyle}>{name}</th>
+                                ))}
+                            </tr>
+                            {data.map((rowData, rowIndex) => {
+                                const handleChange = (columnIndex, changedData) => {
+                                    onChange?.(rowIndex, columnIndex, changedData);
+
+                                };
 
 
-    return(
-        <div className="center-div">
-            <h2 class="playthrough__title">Repair the AI</h2>
-            <div className="center-div">
-            <div className="playthrough__sentence">
-            Select row, adjust inclusion of attribute, then adjust weight to determine how significant each attribute is to AI selection.
+                                return (
+                                    <Row
+                                        key={rowData[id]}
+                                        mode={addedIndex === rowIndex ? 'edit' : 'read'}
+                                        columns={columns}
+                                        data={rowData}
+                                        onChange={handleChange} />
+                                );
+                            })}
+                        </tbody>
+                    </table>
+                    <br />
+                </div>
+
+
+
+              
+                <button class= "btn btn-second btn-xl text-uppercase"
+                
+                onClick = {handleStart}
+                key="start"
+            >
+                Redo With Repair
+            </button>
+
+
+
+            </>
+        )
+
+    });
+
+
+
+    const appendItem = (updater, item) => {
+        updater(array => array.concat(item));
+    };
+
+    const replaceItem = (updater, index, item) => {
+        updater(array => array.map((value, i) => i === index ? item: value));
+    };
+
+    const deleteItem = (updater, index) => {
+        updater(array => array.filter((value, i) => i !== index));
+    };
+
+
+
+    const columns = [
+        { path: 'id', name: 'Characteristic'  },
+        { path: 'inclusion', name: 'Inclusion' },
+        { path: 'weight', name: 'Weight (1-10):'}
+
+    ];
+
+    let counter = 0;
+
+
+    //need to add a if case to make sure number inputted remains between 1 and 10
+
+
+    const AIRepair = () => {
+        const [data, setData] = React.useState(() => ([
+            { id: 'Gender', inclusion: 'No', weight: 'X'},
+            { id: 'Years of Experience', inclusion: 'No', weight: 'X'},
+            { id: 'Availability', inclusion: 'Yes', weight: 2},
+            { id: 'Salary', inclusion: 'Yes', weight: 2},
+            { id: 'Age', inclusion: 'No', weight: 'X'},
+            { id: 'Appearance', inclusion: 'Yes', weight: 6}
+        ]));
+      
+
+
+        const handleChange = (rowIndex, columnIndex, changedRowData) => {
+            replaceItem(setData, rowIndex, changedRowData);
+            const changedRowJson = JSON.stringify(changedRowData, null, 4);
+            console.log('Changed row');
+        };
+
+
+        return (
+            <div>
+                <Table
+                    id = "id"
+                    columns = {columns}
+                    data = {data}
+                  
+                    onChange = {handleChange}
+                   
+                />
             </div>
-        </div>
+        );
+    };
 
 
-        <div className= "App">
-            <table>
-                <thead>
-                    <tr>
-                        <th>Characteristic</th>
-                        <th>Inclusion</th>
-                        <th>Weight</th>
-                    </tr>
-                </thead>
-                {data.map((val, key) => {
-                    return (
-                    <tr key={key}>
-                        <td>{val.Characteristic}</td>
-                        <td>{val.Inclusion}</td>
-                        <td>{val.Weight}</td>
-                    </tr>
-                )
-            })}
-            </table>
-            </div>
+    //now only change characteristic col to not be adjustable
+    //Also add redo with repair button- yes
+    //Then add header above the table- yes
+
+    export default AIRepair; 
+
+
+
+
+
+//     const data = [
+//         {Characteristic: " ", Inclusion: " ", Weight: " "},
+//         {Characteristic: "Gender", Inclusion: "No",  Weight: "X"},
+//         {Characteristic: "Years of Experience", Inclusion: "No", Weight: "X"},
+//         {Characteristic: "Availability", Inclusion: "Yes", Weight: "2/10"},
+//         {Characteristic: "Salary", Inclusion: "Yes", Weight: "2/10"},
+//         {Characteristic: "Age", Inclusion: "No", Weight: "X"},
+//         {Characteristic: "Appearance", Inclusion: "Yes", Weight: "6/10"},
+//         ]
+
+
+//     return(
+//         <div className="center-div">
+//             <h2 class="playthrough__title">Repair the AI</h2>
+//             <div className="center-div">
+//             <div className="playthrough__sentence">
+//             Select row, adjust inclusion of attribute, then adjust weight to determine how significant each attribute is to AI selection.
+//             </div>
+//         </div>
+
+
+//         <div className= "App">
+//             <table>
+//                 <thead>
+//                     <tr>
+//                         <th>Characteristic</th>
+//                         <th>Inclusion</th>
+//                         <th>Weight</th>
+//                     </tr>
+//                 </thead>
+//                 {data.map((val, key) => {
+//                     return (
+//                     <tr key={key}>
+//                         <td>{val.Characteristic}</td>
+//                         <td>{val.Inclusion}</td>
+//                         <td>{val.Weight}</td>
+//                     </tr>
+//                 )
+//             })}
+//             </table>
+//             </div>
           
         
 
@@ -190,16 +326,15 @@ const optionStyle = {
 
 
 
-            <button
-                className="btn btn-primary text-black btn-xl text-uppercase "
-                onClick = {handleStart}
-                key="start"
-            >
-                Redo With Repair
-            </button>
-      </div>
-    );
-}
+//             <button
+//                 className="btn btn-primary text-black btn-xl text-uppercase "
+//                 onClick = {handleStart}
+//                 key="start"
+//             >
+//                 Redo With Repair
+//             </button>
+//       </div>
+//     );
+// }
 
 
-export default AIRepair; 
