7649510c254e9194d8e7bf2a0047015482017e6f
Mster115 <mfs5944@rit.edu>
2023-01-14 17:28:51 -0500

    prettier fixes

--- client/src/components/exercise/lab6/pages/Applicant/ExerciseStart.js
+++ client/src/components/exercise/lab6/pages/Applicant/ExerciseStart.js
@@ -16,7 +16,6 @@ const mapDispatchToProps = (dispatch) => {
   };
 };
 
-
 const ExerciseStart = (props) => {
   const { actions } = props;
 
--- client/src/helpers/GetReducer.js
+++ client/src/helpers/GetReducer.js
@@ -14,7 +14,7 @@ const getExerciseState = (state) => {
     case 5:
       return state.exercise5.state;
     case 6:
-        return state.exercise6.state;
+      return state.exercise6.state;
     default:
       return state.exercise1.state;
   }
