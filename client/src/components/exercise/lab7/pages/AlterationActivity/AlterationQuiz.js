a2df0371d4d0c4b8c986304e7f5866da8b637b82
jc1831 <jc1831@rit.edu>
2023-02-15 16:13:50 -0500

    colorPicker warning removed

--- client/src/components/exercise/lab7/pages/AlterationActivity/AlterationQuiz.js
+++ client/src/components/exercise/lab7/pages/AlterationActivity/AlterationQuiz.js
@@ -1,49 +1,46 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import { ThirtyFpsSharp } from "@mui/icons-material";
-import React, { Component } from "react";
-import { EXERCISE_IDLE } from "../../../../../constants";
-import { EXERCISE_ENDED } from "../../../../../constants/lab7";
-import { default as Quiz } from "../../../../quiz/App";
+import React, {Component} from "react";
+import {default as Quiz} from "../../../../quiz/App";
 
 class AlterationQuiz extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      componentName: "AlterationQuiz",
-      updateStateFunc: undefined,
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            componentName: "AlterationQuiz",
+            updateStateFunc: undefined,
+        };
+    }
 
-  componentDidMount() {
-    this.setState({ updateStateFunc: this.props.action.updateState });
-  }
+    componentDidMount() {
+        this.setState({updateStateFunc: this.props.action.updateState});
+    }
 
-  render() {
-    const { updateStateFunc } = this.state;
-    const { state, actions } = this.props;
-    return (
-      <div className="center-div">
-        <p className="playthrough__sentence">Alteration Quiz</p>
+    render() {
+        const {updateStateFunc} = this.state;
+        const {state, actions} = this.props;
+        return (
+            <div className="center-div">
+                <p className="playthrough__sentence">Alteration Quiz</p>
 
-        <p className="playthrough__sentence">
-          Original Utility Equation: File Sensitivity Level / Threat Level
-        </p>
+                <p className="playthrough__sentence">
+                    Original Utility Equation: File Sensitivity Level / Threat Level
+                </p>
 
-        <p className="playthrough__sentence">
-          How does the following utility equation impact the autonomous system
-          compared to the original utility equation?
-        </p>
+                <p className="playthrough__sentence">
+                    How does the following utility equation impact the autonomous system
+                    compared to the original utility equation?
+                </p>
 
-        <Quiz
-          path={`/AlterationQuiz`}
-          user={state.main.user}
-          updateStateFunc={updateStateFunc}
-          hideCertificate={true}
-        />
-      </div>
-    );
-  }
+                <Quiz
+                    path={`/AlterationQuiz`}
+                    user={state.main.user}
+                    updateStateFunc={updateStateFunc}
+                    hideCertificate={true}
+                />
+            </div>
+        );
+    }
 }
 
 export default AlterationQuiz;
