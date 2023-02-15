be9ab4163e541a0111a9697cafab507a8299025b
KyleMesserle <klm3580@rit.edu>
2023-02-15 13:07:46 -0500

    landing page

--- client/src/components/imagine/pages/ImagineStart.js
+++ client/src/components/imagine/pages/ImagineStart.js
@@ -10,11 +10,10 @@ const ImagineStart = (props) => {
 
   return (
     <div className="container bottomSpace center-div">
-      <h2 className="playthrough__title">Tic-Tac-Toe: Start</h2>
+      <h2 className="playthrough__title">Empathy Building Exercise: Start</h2>
       <div className="playthrough__sentence">
-        You are going to be competing against an AI opponent in a game of
-        Tic-Tac-Toe. Try your best to beat the AI in competition. You will be
-        playing alongside other players.
+        You are going to be participating in an Empathy Building exercise in where you will
+        experience how a poorly designed app might cause someone with a Color-Blindness deficiency discomfort or annoyance.
       </div>
       <div className="playthrough__sentence">
         Click the “Start” button to begin this activity!
