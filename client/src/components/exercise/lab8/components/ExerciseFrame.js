3a35f0c2e3bbd2bd458d756319026d54274587a7
Heather Moses <hlm8500@rit.edu>
2023-03-05 15:14:37 -0500

    Working on adding a background image to the exercise frame

--- client/src/components/exercise/lab8/components/ExerciseFrame.js
+++ client/src/components/exercise/lab8/components/ExerciseFrame.js
@@ -1,11 +1,14 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable require-jsdoc */
 
-import React, { Component } from "react";
+import React from "react";
 
-const ExerciseFrame = () =>
-{
-  return <h1>Welcome to my stream </h1>;
-}
+const ExerciseFrame = () => {
+  return (
+    <div className="exercise-frame tw-w-full tw-aspect-video">
+      <h1>Welcome to my stream </h1>
+    </div>
+  );
+};
 
 export default ExerciseFrame;
