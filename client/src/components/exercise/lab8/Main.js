12018465fe89e38dcc35edf16253277fb95720ca
Heather Moses <hlm8500@rit.edu>
2023-03-05 15:14:37 -0500

    Working on adding a background image to the exercise frame

--- client/src/components/exercise/lab8/Main.js
+++ client/src/components/exercise/lab8/Main.js
@@ -1,13 +1,11 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable require-jsdoc */
 /* eslint-disable max-len */
-import React, { Component, Fragment } from "react";
+import React from "react";
 import ExerciseFrame from "./components/ExerciseFrame";
 
 const Main = () => {
-    return (
-        <ExerciseFrame/>
-    );
-}
+  return <ExerciseFrame />;
+};
 
 export default Main;
