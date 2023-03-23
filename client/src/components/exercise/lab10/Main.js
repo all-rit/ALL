838e2dcfd15ae2f1653b7b85fdf370849d5283dd
Mster115 <mfs5944@rit.edu>
2023-03-23 14:29:21 -0400

    additional skelton

---
+++ client/src/components/exercise/lab10/Main.js
@@ -0,0 +1,42 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable require-jsdoc */
+/* eslint-disable max-len */
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Router } from "@reach/router";
+import "../../../assets/stylesheets/main.scss";
+import { actions as exerciseActions } from "../../../reducers/lab10/ExerciseReducer";
+import { bindActionCreators } from "redux";
+import ExerciseStart from "./pages/ExerciseStart";
+import ExerciseEnd from "./pages/ExerciseEnd";
+
+const mapStateToProps = (state) => ({
+  state: state,
+});
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    actions: bindActionCreators({ ...exerciseActions }, dispatch),
+  };
+};
+
+class Main extends Component {
+  render() {
+    const { actions, state, user } = this.props;
+    return (
+      <div className="bottomSpace">
+        <Router className="app">
+          <ExerciseStart default path="/" actions={actions} />
+          <ExerciseEnd
+            path="/ExerciseEnd"
+            actions={actions}
+            state={state}
+            user={user}
+          />
+        </Router>
+      </div>
+    );
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Main);
