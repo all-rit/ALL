c5119822d918d14bd0851de7d0097a01bc5d3e80
Domenic Mangano <dm9965@rit.edu>
2023-03-24 15:30:39 -0400

    Updated Feed Styling

--- client/src/components/exercise/lab9/pages/SocialMedia/Feed.js
+++ client/src/components/exercise/lab9/pages/SocialMedia/Feed.js
@@ -1,5 +1,11 @@
 import React, { Component } from "react";
 import "../../../../../../src/assets/stylesheets/components/PawPrintFeed.scss";
+import user939 from "../../../../../assets/images/Lab9/user939.png"
+import user828 from "../../../../../assets/images/Lab9/user828.png";
+import Heart from '@material-ui/icons/Favorite';
+import Button from '@material-ui/core/Button'
+import { ThumbUp } from "@material-ui/icons";
+import profilepic from "../../../../../assets/images/Lab9/profilepic.png";
 
 class Feed extends Component {
   constructor(props) {
@@ -11,15 +17,38 @@ class Feed extends Component {
       <div className="main-div">
         <div className="content-div">
           <span className="user-post">
-            <div className="post-text guidance">
-              <h1 className="tw-font-bold tw">Feed</h1>
+            <div className="post-text guidance tw-font-bold">
+              <img className={"poster-image"} alt="posting-user" src={user828}/>
+              <p className="tw-font-bold ">User828 Posted: This cat is the cutest!</p>
             </div>
+          </span>
+          <span className={"reply tw-ml-3 tw-mt-3"}>
+            <img
+              src={profilepic}
+              alt="welcome logo"
+              className="profile-pic tw-rounded-3xl"
+            />
+            <p className={"tw-font-bold tw-ml-5"}> Add a Comment... </p>
+            <Button className={"button-design tw-mr-2"}>
+              Reply
+            </Button>
+            <Button
+              className={"button-design"}
+              color={"secondary"}
+              startIcon={<ThumbUp/>}>
+            </Button>
+            <Button
+              className={"button-design"}
+              color="secondary"
+              size={"large"}
+              startIcon={<Heart/>}>
+            </Button>
           </span>
         </div>
         <div className="comment-div">
           <span className="comment-contents">
-            <img alt="userpic" />
-            <p>User Comment</p>
+            <img className="commenter-image" alt="userpic" src={user939}/>
+            <p className= "guidance tw-font-bold"> User 939 barked: &quot;Awe, thats adorbes!&quot; </p>
           </span>
         </div>
       </div>
