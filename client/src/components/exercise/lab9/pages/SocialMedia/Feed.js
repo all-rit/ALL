477a39dee0e8ad606fd212cae3e14c08334c96db
Domenic Mangano <dm9965@rit.edu>
2023-03-24 14:02:41 -0400

    commit

--- client/src/components/exercise/lab9/pages/SocialMedia/Feed.js
+++ client/src/components/exercise/lab9/pages/SocialMedia/Feed.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import "../../../../../../src/assets/stylesheets/components/PawPrintFeed.scss";
 
-
 class Feed extends Component {
   constructor(props) {
     super(props);
@@ -11,23 +10,20 @@ class Feed extends Component {
     return (
       <div className="main-div">
         <div className="content-div">
-            <span className="user-post">
-              <div className="post-text guidance">
-                <h1 className="tw-font-bold tw">Feed</h1>
-              </div>
-            </span>
+          <span className="user-post">
+            <div className="post-text guidance">
+              <h1 className="tw-font-bold tw">Feed</h1>
+            </div>
+          </span>
         </div>
         <div className="comment-div">
           <span className="comment-contents">
-            <img alt="userpic"/>
-            <p>
-              User Comment
-            </p>
+            <img alt="userpic" />
+            <p>User Comment</p>
           </span>
         </div>
       </div>
-
-    )
+    );
   }
 }
 export default Feed;
