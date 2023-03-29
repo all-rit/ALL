77d493de6efc2c3c744214c722fa708f301edb6a
jc1831 <jc1831@rit.edu>
2023-03-29 16:33:48 -0400

    [#301] Creating STEP_COUNT constant

--- client/src/components/exercise/lab10/components/Simulation/Game.js
+++ client/src/components/exercise/lab10/components/Simulation/Game.js
@@ -1,37 +1,40 @@
 import React from "react";
-import {bindActionCreators} from "redux";
-import {actions as exerciseActions} from "../../../../../reducers/lab10/ExerciseReducer";
-import {connect} from "react-redux";
+import { bindActionCreators } from "redux";
+import { actions as exerciseActions } from "../../../../../reducers/lab10/ExerciseReducer";
+import { connect } from "react-redux";
 import WalkingMan from "./WalkingMan";
 
-
 const Game = () => {
+  return (
+    <div
+      className={
+        "tw-flex tw-flex-col tw-shadow-xl tw-border-solid tw-border-2 tw-border-[#BFBFBF] tw-bg-[#F8F8F8] tw-rounded tw-h-96"
+      }
+    >
+      {/* Falling object section */}
+      <div></div>
 
-    return (
-        <div
-            className={"tw-flex tw-flex-col tw-shadow-xl tw-border-solid tw-border-2 tw-border-[#BFBFBF] tw-bg-[#F8F8F8] tw-rounded tw-h-96"}>
-            {/* Falling object section */}
-            <div>
-
-            </div>
-
-            {/* Walking man section */}
-            <div className={"tw-mt-auto tw-pt-6 tw-border-solid tw-border-0 tw-border-t-2 tw-border-[#BFBFBF]"}>
-                <WalkingMan/>
-            </div>
-        </div>
-    );
-}
+      {/* Walking man section */}
+      <div
+        className={
+          "tw-mt-auto tw-pt-6 tw-border-solid tw-border-0 tw-border-t-2 tw-border-[#BFBFBF]"
+        }
+      >
+        <WalkingMan />
+      </div>
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => {
-    return state.exercise10;
-    // return {};
+  return state.exercise10;
+  // return {};
 };
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-        actions: bindActionCreators({...exerciseActions}, dispatch),
-    };
+  return {
+    actions: bindActionCreators({ ...exerciseActions }, dispatch),
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Game);
