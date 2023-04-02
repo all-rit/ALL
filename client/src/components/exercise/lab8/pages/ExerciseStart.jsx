1449f265b1024b62989adb1bf7008f25a09d51d2
Ryan <39078738+2nPlusOne@users.noreply.github.com>
2023-04-02 16:01:22 -0400

    prettier fixes

--- client/src/components/exercise/lab8/pages/ExerciseStart.jsx
+++ client/src/components/exercise/lab8/pages/ExerciseStart.jsx
@@ -17,22 +17,21 @@ const ExerciseStart = ({ actions }) => {
       <div className="center-div">
         <div className="guidance margin-bottom-2">
           <p className="playthrough__sentence">
-            In this exercise, you are taking the role of a chat moderator
-            for a live stream, the streamer for which has recently taken a
-            break and is away from their computer. You are testing a new
-            moderation tool which uses AI to detect if a message should be
-            removed for being inappropriate.{" "}
+            In this exercise, you are taking the role of a chat moderator for a
+            live stream, the streamer for which has recently taken a break and
+            is away from their computer. You are testing a new moderation tool
+            which uses AI to detect if a message should be removed for being
+            inappropriate.{" "}
             <strong>
-              Your task is to monitor the live chat and either keep or
-              remove messages based on their level of appropriateness.
-              You should take the AI’s recommendation into consideration
-              for each message.
+              Your task is to monitor the live chat and either keep or remove
+              messages based on their level of appropriateness. You should take
+              the AI’s recommendation into consideration for each message.
             </strong>
           </p>
 
           <p className="playthrough__sentence">
-            Beside each chat message, you will see a recommendation from the
-            AI, followed by buttons to either remove the message or keep it.
+            Beside each chat message, you will see a recommendation from the AI,
+            followed by buttons to either remove the message or keep it.
           </p>
         </div>
 
