de6c8999543baf8a645d9982ff13599ea53851f9
jc1831 <jc1831@rit.edu>
2023-04-03 19:06:54 -0400

    [#301] Functionality for object hitting edge w/ comments.

--- client/src/constants/lab10/index.js
+++ client/src/constants/lab10/index.js
@@ -2,3 +2,4 @@ export const EXERCISE_PLAYING = "EXERCISE_PLAYING";
 export const EXERCISE_IDLE = "EXERCISE_IDLE";
 export const LAB_ID = 10;
 export const STEP_COUNT = 20;
+export const IMG_SIZE = 96;
