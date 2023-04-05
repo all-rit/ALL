2ea57ebe958187dae37a64c1953aae1484b9184f
jc1831 <jc1831@rit.edu>
2023-04-05 17:59:40 -0400

    [#301] Updated pages to utilize functions rather than classes

--- client/src/components/exercise/lab10/pages/ExerciseEnd.js
+++ client/src/components/exercise/lab10/pages/ExerciseEnd.js
@@ -1,44 +1,42 @@
-/* eslint-disable react/no-unescaped-entities */
-/* eslint-disable react/prop-types */
-/* eslint-disable require-jsdoc */
-import React, { Component, Fragment } from "react";
+import React, { Fragment } from "react";
 import { navigate } from "@reach/router";
 import { AppBar } from "@material-ui/core";
 
-class ExerciseEnd extends Component {
-  handleHome() {
-    navigate("/Lab10/Exercise");
-  }
+const ExerciseEnd = () => {
+  /**
+   * Redirect the user to the following page
+   * @returns {Promise} navigate promise
+   */
+  const handleHome = () => {
+    return navigate("/Lab10/Exercise");
+  };
 
-  render() {
-    // const { user, state, plays } = this.props;
-    return (
-      <Fragment>
-        <div className="center-div">
-          <div className="cognitive_instructions">
-            <div>
-              <AppBar position="static" className="appBar">
-                <h4 className="flex-boxes ">
-                  Congratulations! You have succesfully completed the Machine
-                  Learning Exercise!
-                </h4>
-              </AppBar>
-            </div>
+  return (
+    <Fragment>
+      <div className="center-div">
+        <div className="cognitive_instructions">
+          <div>
+            <AppBar position="static" className="appBar">
+              <h4 className="flex-boxes ">
+                Congratulations! You have succesfully completed the Machine
+                Learning Exercise!
+              </h4>
+            </AppBar>
           </div>
-          <h4 className="flex-boxes">
-            Click the button below to restart the exercise.
-          </h4>
-          <button
-            className="btn btn-primary text-black btn-xl text-uppercase "
-            onClick={this.handleHome}
-            key="start"
-          >
-            Return to Exercise Start
-          </button>
         </div>
-      </Fragment>
-    );
-  }
-}
+        <h4 className="flex-boxes">
+          Click the button below to restart the exercise.
+        </h4>
+        <button
+          className="btn btn-primary text-black btn-xl text-uppercase "
+          onClick={handleHome}
+          key="start"
+        >
+          Return to Exercise Start
+        </button>
+      </div>
+    </Fragment>
+  );
+};
 
 export default ExerciseEnd;
