e3962a7997e1b7565d6dd430dc28f33fa585fc32
jjm8759 <89434744+jjm8759@users.noreply.github.com>
2023-04-06 15:43:17 -0400

    Converted to a functional componenet, also improved code block

--- client/src/components/exercise/lab10/pages/TrainingAICodeBlock.js
+++ client/src/components/exercise/lab10/pages/TrainingAICodeBlock.js
@@ -1,57 +1,48 @@
 /* eslint-disable react/prop-types */
-import React, { Component } from "react";
+import React from "react";
 import Highlight from "react-highlight";
 import "highlight.js/styles/atom-one-dark-reasonable.css";
 import "../../../../assets/stylesheets/components/CodeBlock.css";
-class TrainingAICodeBlock extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            componentName: "TrainingAICodeBlock.js",
-        };
-    }
 
-    isNumberInRange(value, lowerBound, upperBound) {
-        return value >= lowerBound && value <= upperBound;
-    }
+export default function TrainingAICodeBlock() {
 
-    validateRepair() {
-    }
 
-    render() {
-        const { timeValue } = this.props;
-        const preInput = `
-import React, { Component } from "react"; 
-class TrainNetwork extends Component {
-    //To gather more data, increase the simulation timer to 30-45 seconds
+
+    const { timeValue } = this.props;
+    const preInput = `
+import React from "react"; 
+export default function TrainNetwork() {
+    //Here is where you will update the time it take the training to run
+    //So that more data can be gathered
+
+    //Enter a value between 30 and 45 seconds into the input below
     const time = 
 `;
-        const postInput = `
+    const postInput = `
 } 
-export default TrainNetwork;`;
+`;
 
-        return (
-            <div>
-                <div className="filenameHeader">TrainingAICodeBlock.js</div>
-                <div style={{ textAlign: "left" }}>
-                    <Highlight>
-                        <pre>
-                            <code className="language-jsx">{preInput.trim()}</code>
-                            &nbsp;
-                            <input type="text" value={timeValue}></input>&#59;
-                            <br></br>
-                            <code>{postInput.trim()}</code>
-                        </pre>
-                    </Highlight>
-                </div>
-                <button
-                    onClick={this.validateRepair.bind(this)}
-                    type="submit"
-                    className="button button--green button--block">
-                    Update
-                </button>
+    return (
+        <div>
+            <div className="filenameHeader">TrainingAICodeBlock.js</div>
+            <div style={{ textAlign: "left" }}>
+                <Highlight>
+                    <pre>
+                        <code className="language-jsx">{preInput.trim()}</code>
+                        &nbsp;
+                        <input type="text" value={timeValue}></input>&#59;
+                        <br></br>
+                        <code>{postInput.trim()}</code>
+                    </pre>
+                </Highlight>
             </div>
-        );
-    }
+            <button
+                onClick={this.validateRepair.bind(this)}
+                type="submit"
+                className="button button--green button--block"
+            >
+                Update
+            </button>
+        </div>
+    );
 }
-export default TrainingAICodeBlock;
