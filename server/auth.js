4a2902971ba54bd1aab6373b864e3ceb8bdb69cb
jc1831 <jc1831@rit.edu>
2023-04-10 17:04:50 -0400

    Revert "#283 updated passport package for google sso"
    This reverts commit c8318f6819e74a65e70086ffde36d839bea1336b.

--- server/auth.js
+++ server/auth.js
@@ -1,4 +1,5 @@
-const GoogleStrategy = require('passport-google-oidc');
+// let GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const GoogleStrategy = require('passport-google-oauth20');
 
 module.exports = (passport) => {
   passport.serializeUser((user, done) => {
@@ -9,19 +10,15 @@ module.exports = (passport) => {
     done(null, user);
   });
 
-  passport.use(
-      new GoogleStrategy(
-          {
-            clientID: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_SECRET,
-            callbackURL: process.env.GOOGLE_CALLBACK_URL,
-          },
-          (token, refreshToken, profile, done) => {
-            return done(null, {
-              profile: profile,
-              token: token,
-            });
-          },
-      ),
-  );
+  passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK_URL,
+  },
+  (token, refreshToken, profile, done) => {
+    return done(null, {
+      profile: profile,
+      token: token,
+    });
+  }));
 };
