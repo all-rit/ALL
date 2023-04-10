2ba786dffbfa8e8a4527a5026e8beade5cd18638
jjm8759 <89434744+jjm8759@users.noreply.github.com>
2023-04-10 16:06:11 -0400

    Added popup messages for time input validation

--- client/src/components/exercise/lab10/pages/TrainingAICodeBlock.js
+++ client/src/components/exercise/lab10/pages/TrainingAICodeBlock.js
@@ -4,7 +4,7 @@ import Highlight from "react-highlight";
 import { actions } from "../../../../reducers/lab10/RepairReducer";
 import "highlight.js/styles/atom-one-dark-reasonable.css";
 import "../../../../assets/stylesheets/components/CodeBlock.css";
-
+import { POPUP_MESSAGES } from "../../../../constants/lab10";
 export default function TrainingAICodeBlock() {
     const timeValue = "";
 
@@ -20,14 +20,50 @@ export default function TrainNetwork() {
     const postInput = `
 } 
 `;
-    function validateRepair() {
+    function validateTimeValue() {
+        let error = null;
+        if (!Number.isInteger(timeValue)) {
+            error = POPUP_MESSAGES.INVALID_INTEGER;
+        } else {
+            try {
+                const upperBound = 45;
+                const lowerBound = 30;
+                if (timeValue < lowerBound && timeValue > upperBound) {
+                    error = POPUP_MESSAGES.OUTSIDE_RANGE;
+                }
+            } catch (e) {
+                switch (e.data?.category) {
+                    case "wrongType":
+                        error = POPUP_MESSAGES.INVALID_INTEGER;
+                        break;
+                    default:
+                        error = e.message;
+                }
+            }
+            const result = {
+                timeValue,
+                error,
+                passed: !error,
+            }
 
+            if (!result.passed) {
+                actions.updateRepairError(result.error);
+            } else {
+                actions.updateRepairError(null);
+            }
+            return result;
+        }
+    }
+
+
+    function validateRepair() {
+        const timeValue = validateTimeValue();
+        alert(timeValue);
     }
 
     function handleTimeValueChange(e) {
         actions.updateTimeValue(e.target.value);
     }
-
 
     return (
         <div>
@@ -37,13 +73,22 @@ export default function TrainNetwork() {
                     <pre>
                         <code className="language-jsx">{preInput.trim()}</code>
                         &nbsp;
-                        <input type="text" value={timeValue} onChange={handleTimeValueChange}></input>&#59;
+                        <input
+                            type="text"
+                            value={timeValue}
+                            onChange={handleTimeValueChange}
+                        ></input>
+                        &#59;
                         <br></br>
                         <code>{postInput.trim()}</code>
                     </pre>
                 </Highlight>
             </div>
-            <button type="submit" className="button button--green button--block" onClick={validateRepair}>
+            <button
+                type="submit"
+                className="button button--green button--block"
+                onClick={validateRepair}
+            >
                 Update
             </button>
         </div>
--- client/src/constants/lab10/index.js
+++ client/src/constants/lab10/index.js
@@ -1,3 +1,11 @@
 export const EXERCISE_PLAYING = "EXERCISE_PLAYING";
 export const EXERCISE_IDLE = "EXERCISE_IDLE";
 export const LAB_ID = 10;
+
+export const POPUP_MESSAGES = {
+    INVALID_INTEGER: "You must pass in a valid integer.",
+    SUCCESS: "The repairs have successfully been implemented.",
+    OUTSIDE_RANGE: "Please enter a value between 30 and 45",
+
+};
+export const POPUP_DELAY = 6000;
