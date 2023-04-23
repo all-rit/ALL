573d4cc232ef46d2a06113c80ec2d2fea05dfc54
Ryan <39078738+2nPlusOne@users.noreply.github.com>
2023-04-23 15:21:47 -0400

    Modified chat messages object to remove randomness

--- client/src/components/exercise/lab8/selectMessagesFns.js
+++ client/src/components/exercise/lab8/selectMessagesFns.js
@@ -1,56 +1,20 @@
 import { CHAT_MESSAGES } from "../../../constants/lab8";
 
-const totalMessages = 6;
-const halfMessages = Math.floor(totalMessages / 2);
 
-// Select random messages to display in the chat room including AI incorrect messages.
+// Select and shuffle messages to display in the chat room before the repair.
 export function selectMessagesAiBias() {
-  // Process all the messages into buckets based on their type
-  const incorrectKeep = CHAT_MESSAGES.recommend_keep.filter(
-    (msg) => !msg.ai_correct
-  );
-  const incorrectRemove = CHAT_MESSAGES.recommend_remove.filter(
-    (msg) => !msg.ai_correct
-  );
-
-  const correctKeep = CHAT_MESSAGES.recommend_keep.filter(
-    (msg) => msg.ai_correct
-  );
-  const correctRemove = CHAT_MESSAGES.recommend_remove.filter(
-    (msg) => msg.ai_correct
-  );
-
-  // Select random messages from each bucket to display in the chat room.
-  const selectedKeep = [
-    ...shuffleArray(incorrectKeep).slice(0, 1),
-    ...shuffleArray(correctKeep).slice(0, halfMessages - 1),
-  ];
-
-  const selectedRemove = [
-    ...shuffleArray(incorrectRemove).slice(0, 1),
-    ...shuffleArray(correctRemove).slice(0, halfMessages - 1),
-  ];
-
   // Shuffle the messages and return them.
-  return shuffleArray([...selectedKeep, ...selectedRemove]);
+  const messages = shuffleArray(CHAT_MESSAGES.before_repair);
+
+  return messages;
 }
 
-// // Select random messages to display in the chat room, but only correct messages.
-// export function selectMessagesAiRepaired() {
-//   // Process all the messages into buckets based on their type
-//   const correctKeep = CHAT_MESSAGES.recommend_keep.filter(
-//     (msg) => msg.ai_correct
-//   );
-//   const correctRemove = CHAT_MESSAGES.recommend_remove.filter(
-//     (msg) => msg.ai_correct
-//   );
-
-//   // Select random messages from each bucket to display in the chat room.
-//   const selectedKeep = shuffleArray(correctKeep).slice(0, halfMessages);
-//   const selectedRemove = shuffleArray(correctRemove).slice(0, halfMessages);
-
+// // Select and shuffle messages to display in the chat room after the repair.
+// export function selectMessagesRepair() {
 //   // Shuffle the messages and return them.
-//   return shuffleArray([...selectedKeep, ...selectedRemove]);
+//   const messages = shuffleArray(CHAT_MESSAGES.after_repair);
+
+//   return messages;
 // }
 
 function shuffleArray(array) {
--- client/src/constants/lab8/messages.js
+++ client/src/constants/lab8/messages.js
@@ -3,45 +3,12 @@
 // 1 - neutral
 // 2 - positive
 const CHAT_MESSAGES = {
-  // The AI recommends these messages to be kept
-  recommend_keep: [
-    {
-      username: "gamerking42",
-      content: "bro, your stream is straight up fire",
-      ai_polarity: 2,
-      ai_correct: true,
-      intended_polarity: 2,
-    },
+  before_repair: [
     {
       username: "epic_panda",
       content: "watching your streams always puts me in a good mood",
       ai_polarity: 2,
       ai_correct: true,
-      intended_polarity: 2,
-    },
-    {
-      username: "l33t_haxor",
-      content: "ur gameplay is so t3rr1b1e",
-      ai_polarity: 1,
-      ai_correct: false,
-      intended_polarity: 0,
-    },
-    {
-      username: "xX_dark_dragon_Xx",
-      content: "u r the w0r$t streamer ive ever seen",
-      ai_polarity: 1,
-      ai_correct: false,
-      intended_polarity: 0,
-    },
-    // ... more messages
-  ],
-  // The AI recommends these messages to be removed
-  recommend_remove: [
-    {
-      username: "ninja_wombat",
-      content: "sweet usage of that potion to poison that playa.",
-      ai_polarity: 0,
-      ai_correct: false,
       intended_polarity: 2,
     },
     {
@@ -53,16 +20,53 @@ const CHAT_MESSAGES = {
       intended_polarity: 0,
     },
     {
-      username: "racerdude100",
-      content: "this game is terrible and ur stream isnt making it any better",
+      username: "xX_dark_dragon_Xx",
+      content: "u r the w0r$t streamer ive ever seen",
+      ai_polarity: 1,
+      ai_correct: false,
+      intended_polarity: 0,
+    },
+    {
+      username: "cosmic_crusher404",
+      content: "seen more skill from someone playing with their feet",
       ai_polarity: 0,
       ai_correct: true,
       intended_polarity: 0,
     },
     {
-      username: "EpicGorillaGamer",
+      username: "ninja_wombat",
+      content: "sweet usage of that potion to poison that playa.",
+      ai_polarity: 0,
+      ai_correct: false,
+      intended_polarity: 2,
+    },
+    {
+      username: "gamerking42",
+      content: "bro, your stream is straight up fire",
+      ai_polarity: 2,
+      ai_correct: true,
+      intended_polarity: 2,
+    },
+  ],
+  after_repair: [
+    {
+      username: "epic_panda",
+      content: "watching your streams always puts me in a good mood",
+      ai_polarity: 2,
+      ai_correct: true,
+      intended_polarity: 2,
+    },
+    {
+      username: "pixelmom",
       content:
-        "so bored id rather watch paint dry than continue watching ur stream",
+        "mom here, this game is so bad for our children. STOP THE STREAM!!!!!!",
+      ai_polarity: 0,
+      ai_correct: true,
+      intended_polarity: 0,
+    },
+    {
+      username: "xX_dark_dragon_Xx",
+      content: "u r the w0r$t streamer ive ever seen",
       ai_polarity: 0,
       ai_correct: true,
       intended_polarity: 0,
@@ -75,13 +79,19 @@ const CHAT_MESSAGES = {
       intended_polarity: 0,
     },
     {
-      username: "xXvenomBroXx",
-      content: "pretty sure my cat could play this game better than u",
-      ai_polarity: 0,
+      username: "ninja_wombat",
+      content: "sweet usage of that potion to poison that playa.",
+      ai_polarity: 2,
       ai_correct: true,
-      intended_polarity: 0,
+      intended_polarity: 2,
     },
-    // ... more messages
+    {
+      username: "gamerking42",
+      content: "bro, your stream is straight up fire",
+      ai_polarity: 2,
+      ai_correct: true,
+      intended_polarity: 2,
+    },
   ],
 };
 
