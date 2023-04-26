9488bef666b214673457d5c5eacaa5638bc9f2a4
Heather Moses <hlm8500@rit.edu>
2023-04-26 11:28:02 -0400

    Popup error handling done

--- client/src/components/exercise/lab8/pages/DataRepair.js
+++ client/src/components/exercise/lab8/pages/DataRepair.js
@@ -7,7 +7,7 @@ import { actions as exerciseActions } from "../../../../reducers/lab8/ExerciseRe
 import React, { useState, useEffect } from "react";
 import { EXERCISE_PLAYING } from "../../../../constants/lab8";
 import CodeUpdateHeader from "../../lab3/components/CodeUpdateHeader";
-import Popup from "../../shared/Popup";
+import Popup from "../../shared/Popup"
 
 const DataRepair = (props) => {
   const { actions } = props;
@@ -124,6 +124,7 @@ const DataRepair = (props) => {
       // eventually need to send repair data to the backend
       console.log("Repairs made with no errors");
       setRepairOpen(false);
+      setUserError(false);
       popUpHandler("The repairs have been made.");
     } else {
       setUserError(true);
