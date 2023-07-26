b22872eec44ee2f4fc2c13e3fd9aff602f3727d5
Domenic Mangano <dm9965@rit.edu>
2023-07-26 18:43:07 -0400

    Updated educator cards to be React component compliant

---
+++ client/src/components/body/educators/Educator_Resource_Card.jsx
@@ -0,0 +1,49 @@
+import * as React from 'react'
+import { navigate } from "@reach/router";
+
+Educator_Resource_Card.propTypes = {
+	Card: {
+		cardImage: "",
+		cardTitle: "",
+		cardLink: "",
+		cardLinkTag: "",
+		cardBody: "",
+	},
+};
+export function Educator_Resource_Card(props) {
+	return (
+		<div className=" module__col module__lab_col">
+			<span>
+				<div
+					className="img-fluid module__image module__educators_image"
+					style={{ backgroundImage: props.Card.cardImage}}
+				/>
+			</span>
+			<ul className="module__caption">
+				<li className="tw-mt-3 tw-mb-0 module__title module__lab_title">
+					<a
+						className={
+							"tw-text-dark tw-text-3xl tw-font-semibold tw-tracking-wide"
+						}
+						onClick={() => navigate(props.Card.cardLink)}
+					>
+						{props.Card.cardTitle}
+					</a>
+					<ul className="tw-mt-5 tw-mb-5">
+						<li>
+							<a
+								className="tw-mb-0 tw-font-labBlue tw-text-xl tw-tracking-wide"
+								onClick={() => navigate(props.Card.cardLink)}
+							>
+								{props.Card.cardLinkTag}
+							</a>
+						</li>
+					</ul>
+					<ul className={"tw-text-labGreen tw-mb-3"}>
+						<li>{props.Card.cardBody}</li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+	)
+}
---
+++ client/src/constants/educatorsContent/resource-cards.js
@@ -0,0 +1,35 @@
+import lectureImage from "../../assets/images/educators/LectureSlides.png";
+import walkthroughImage from "../../assets/images/educators/walkthroughVideos.png";
+import assignHW from "../../assets/images/educators/AssignHW.png";
+import contactUs from "../../assets/images/educators/contactUs.jpg";
+
+export default [
+  {
+    cardImage: lectureImage,
+    cardTitle: 'Lab Lecture Slides',
+    cardLink: '/Educators/Content',
+    cardLinkTag: 'Go to Lecture Slides',
+    cardBody: 'All lecture slides are available for free download'
+  },
+  {
+    cardImage: walkthroughImage,
+    cardTitle: 'Walkthrough Videos',
+    cardLink: '/Educators/Content',
+    cardLinkTag: 'Go to Walkthrough Videos',
+    cardBody: 'Comprehensive, step-by-step videos for each lab'
+  },
+  {
+    cardImage: assignHW,
+    cardTitle: 'Assign Labs',
+    cardLink: '',
+    cardLinkTag: 'Go to Lab Assignments',
+    cardBody: 'User-friendly lab assignment and grading functionality'
+  },
+  {
+    cardImage: contactUs,
+    cardTitle: 'Contact Us',
+    cardLink: '',
+    cardLinkTag: 'Go to Contact Information',
+    cardBody: 'Have a question or suggestion? Let us know!'
+  },
+]
