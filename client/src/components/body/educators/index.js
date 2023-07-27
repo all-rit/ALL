ab73716d64d676f09700a137dd337e5a86e42689
Domenic Mangano <dm9965@rit.edu>
2023-07-27 15:45:14 -0400

    prettier

--- client/src/components/body/educators/index.js
+++ client/src/components/body/educators/index.js
@@ -24,19 +24,25 @@ function Educators(props) {
             <p className={"educator-welcome"}>Hello, {firstname}!</p>
           </div>
           <div>
-            <p  className={'educator-main--body'}> Thank you for choosing Accessible Learning Labs as your partner
-              in providing web accessibility and STEM education to your students.
+            <p className={"educator-main--body"}>
+              {" "}
+              Thank you for choosing Accessible Learning Labs as your partner in
+              providing web accessibility and STEM education to your students.
               We are thrilled to work alongside you in fostering an inclusive
-              learning environment. Our team has curated engaging
-              resources and quality support to empower your teaching and inspire
-              young minds. Together, let&apos;s create a brighter future where every
-              student can thrive and excel in the digital world. We&apos;re honored to
+              learning environment. Our team has curated engaging resources and
+              quality support to empower your teaching and inspire young minds.
+              Together, let&apos;s create a brighter future where every student
+              can thrive and excel in the digital world. We&apos;re honored to
               be a part of your educational journey.
             </p>
           </div>
         </div>
-        <div className={'image-container'}>
-          <img className={'educator-main--image'} src={educatorImage} alt={"Educator Image"} />
+        <div className={"image-container"}>
+          <img
+            className={"educator-main--image"}
+            src={educatorImage}
+            alt={"Educator Image"}
+          />
         </div>
       </span>
 
