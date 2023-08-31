4d3d1cdac8d9bdedc09725835072e5044a5ebc4d
Heather Moses <heathermoses@heathers-mbp-2.wireless.rit.edu>
2023-08-31 12:51:41 -0400

    prettier?

--- client/src/components/exercise/lab8/pages/Conclusion.js
+++ client/src/components/exercise/lab8/pages/Conclusion.js
@@ -8,29 +8,30 @@ import { connect } from "react-redux";
 import { actions as exerciseActions } from "../../../../reducers/lab8/ExerciseReducer";
 
 const Conclusion = (props) => {
-    const { actions } = props;
+  const { actions } = props;
 
-    useEffect(() => {
-        actions.updateState(EXERCISE_IDLE);
-      }, [actions]);
+  useEffect(() => {
+    actions.updateState(EXERCISE_IDLE);
+  }, [actions]);
 
-    return (
-        <div className="center-div">
-            <h3>
-                Congratulations - you have completed the Algorithmic Bias exercise!
-            </h3>
-            <div className="playthrough__sentence">
-                Click the &#39;Exercise Home&#39; button to return to the Exercise start page,
-                or click the &#39;Next&#39; button to continue to the Reinforcement section of the lab.
-            </div>
-        </div>  
-    )
-}
+  return (
+    <div className="center-div">
+      <h3>
+        Congratulations - you have completed the Algorithmic Bias exercise!
+      </h3>
+      <div className="playthrough__sentence">
+        Click the &#39;Exercise Home&#39; button to return to the Exercise start
+        page, or click the &#39;Next&#39; button to continue to the
+        Reinforcement section of the lab.
+      </div>
+    </div>
+  );
+};
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-      actions: bindActionCreators({ ...exerciseActions }, dispatch),
-    };
+  return {
+    actions: bindActionCreators({ ...exerciseActions }, dispatch),
   };
-  
-  export default connect(null, mapDispatchToProps)(Conclusion);
+};
+
+export default connect(null, mapDispatchToProps)(Conclusion);
