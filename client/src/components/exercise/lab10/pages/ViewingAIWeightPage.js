04ea2ab19f5db5733851a5dd6551d44328242658
Domenic Mangano <dm9965@rit.edu>
2023-09-05 12:08:20 -0400

    pushing rebase and updating viewing ai weight page to functional component

--- client/src/components/exercise/lab10/pages/ViewingAIWeightPage.js
+++ client/src/components/exercise/lab10/pages/ViewingAIWeightPage.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 import { navigate } from "@reach/router";
 import { EXERCISE_PLAYING } from "../../../../constants/lab10";
 import { actions as exerciseActions } from "../../../../reducers/lab10/ExerciseReducer";
@@ -7,55 +7,46 @@ import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import AIWeightCodeBlock from "../components/AIWeightCodeBlock";
 
-class ViewAIWeightPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      componentName: "ViewAIWeightPage",
-    };
-  }
+const ViewAIWeightPage = (props) => {
 
-  componentDidMount() {
-    const { actions } = this.props;
-    actions.updateState(EXERCISE_PLAYING);
-  }
+  useEffect(() => {
+    props.actions.updateState(EXERCISE_PLAYING);
+  }, []);
 
-  handleNav() {
+  const handleNav = () => {
     navigate("/Lab10/Exercise/UpdatedSim");
   }
 
-  render() {
-    return (
-      <div>
-        <Fragment>
-          <div className="center-div">
-            <div className="guidance margin-bottom-2">
-              <p className="playthrough__sentence">
-                Looks like you got hit by a couple blocks! With each colored
-                block you were hit by, the AI was able to gather data that
-                created higher weight based on the color of block you were hit
-                by most, and lower weight by those you avoided.
-              </p>
-              <p className={"playthrough__sentence"}>
-                However, we now have developed an uneven weight for different
-                colored blocks, therefore creating unwanted bias in our system
-                as the AI should want to avoid all blocks, not just specifically
-                colored ones.
-              </p>
-            </div>
+  return (
+    <div>
+      <Fragment>
+        <div className="center-div">
+          <div className="guidance margin-bottom-2">
+            <p className="playthrough__sentence">
+              Looks like you got hit by a couple blocks! With each colored
+              block you were hit by, the AI was able to gather data that
+              created higher weight based on the color of block you were hit
+              by most, and lower weight by those you avoided.
+            </p>
+            <p className={"playthrough__sentence"}>
+              However, we now have developed an uneven weight for different
+              colored blocks, therefore creating unwanted bias in our system
+              as the AI should want to avoid all blocks, not just specifically
+              colored ones.
+            </p>
           </div>
-        </Fragment>
-        <AIWeightCodeBlock />
-        <button
-          className="btn btn-primary text-black btn-xl text-uppercase "
-          onClick={this.handleNav.bind(this)}
-          key="Next"
-        >
-          Next
-        </button>
-      </div>
-    );
-  }
+        </div>
+      </Fragment>
+      <AIWeightCodeBlock />
+      <button
+        className="btn btn-primary text-black btn-xl text-uppercase "
+        onClick={handleNav.bind(this)}
+        key="Next"
+      >
+        Next
+      </button>
+    </div>
+  );
 }
 
 const mapDispatchToProps = (dispatch) => {
