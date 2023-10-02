0d5315b581758e423ba140fbd1bd1825d8f79c7e
AndreasLc1103 <Andreaslc1103@gmail.com>
2023-10-02 16:26:52 -0400

    fixed layout and re-added in context

--- client/src/components/exercise/lab9/pages/Repair.js
+++ client/src/components/exercise/lab9/pages/Repair.js
@@ -1,37 +1,55 @@
-/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import { useContext, useState } from "react";
+import Proptypes from "prop-types";
 import GameStateContext from "../Lab9Context";
 import CodeUpdateHeader from "../../lab3/components/CodeUpdateHeader";
 import React from "react";
 import Button from "../../../../common/Navigation/Button";
 
 const Repair = (props) => {
-  const { user, heading, repairText=[] } = props;
+  const GameContext = useContext(GameStateContext);
+  // eslint-disable-next-line react/prop-types
+  const { user="", headingText="", repairText = [] } = props;
   const [isRepairActive, setIsRepairActive] = useState(false);
-  const [Next, setNext] = useState(false);
+  const [next, setNext] = useState(false);
 
+
+  const handleRepair = () => {
+    setIsRepairActive(true);
+  }
+  const handleNext = () => {
+    setNext();
+   }
+  
   return (
     <div>
-        <CodeUpdateHeader heading={heading} justifyAlignment={"space-between"} />
-        <div>
-        {repairText.map((text, index) => <p className="indent-2" key={index}>{text }</p>) }
-        </div>
-        <div className="tw-flex tw-flex tw-justify-center ">
-      <div className="tw-pr-10">
-        <Button buttonText={"Repair"} isPrimary={false} />
+      <CodeUpdateHeader heading={headingText} justifyAlignment={"space-between"} />
+      <div>
+        {repairText.map((text, index) => (
+          <p className="indent-2" key={index}>
+            {text}
+          </p>
+        ))}
+      </div>
+      <div className="tw-flex tw-flex tw-justify-center ">
+        <div className="tw-pr-10">
+          <Button buttonText={"Repair"} isPrimary={false} onClick={handleRepair} />
         </div>
         <div className="tw-pl-10">
-        <Button buttonText={"Next"} disabled={true} />
+          <Button buttonText={"Next"} disabled={next} />
+        </div>
       </div>
 
-      </div>
-      
-      {isRepairActive && <>
-        placeholder
-      </>}
+      {isRepairActive && <>placeholder</>}
     </div>
   );
 };
+
+Button.PropTypes = {
+  user: Proptypes.string.isRequired,
+  headingText: Proptypes.string.isRequired,
+  repairText: Proptypes.array.isRequired,
+
+}
 
 export default Repair;
