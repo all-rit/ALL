4cdf221f47e6709458272a78e690011d57ec154a
AndreasLc1103 <Andreaslc1103@gmail.com>
2023-10-03 10:00:39 -0400

    updated tailwind and made some changes to the code block component

--- client/src/common/CodeBlock/Codeblock.js
+++ client/src/common/CodeBlock/Codeblock.js
@@ -3,28 +3,26 @@ import PropTypes from "prop-types";
 
 // eslint-disable-next-line react/prop-types
 const CodeBlock = ({ fileName, children }) => {
-    return (
+  return (
     <>
-    <div className="tw-flex tw-flex-col tw-m-2 tw-bg-white tw-rounded-lg tw-shadow-2xl tw-text-left">
-            <div className="tw-flex-row">
-                <div className=" tw-bg-labGray tw-inline-block tw-border-t-dark tw-border-t-2 tw-border-r-4 tw-border-r-dark tw-border-l-4 tw-border-l-dark tw-cursor-pointer tw-p-6">
-                    {fileName}
-                </div>
-                <div className="tw-grow">
-                </div>
-            </div>
-        <div className="tw-bg-gray tw-p-1 font-mono">
-                    {children}
-
-                    Hello
+      <div className="tw-flex tw-flex-col tw-m-2 tw-bg-white tw-rounded-lg tw-shadow-2xl tw-text-left tw-border-2">
+        <div className="tw-flex-row">
+          <div className=" tw-bg-labGray tw-inline-block tw-border-t-dark tw-border-t-2 tw-border-r-2 tw-border-r-dark tw-border-l-4 tw-border-l-dark tw-cursor-pointer tw-p-3">
+            {fileName}
+          </div>
+          <div className="tw-grow">
+          </div>
         </div>
-    </div>
-    </>);
-}
-
+        <div className="tw-ease-in tw-p-5 tw-rounded-lg tw-shadow-2xl font-mono">
+          {children}
+        </div>
+      </div>
+    </>
+  );
+};
 
 CodeBlock.PropTypes = {
-    fileName: PropTypes.string.isRequired,
-    children: PropTypes.array,
+  fileName: PropTypes.string.isRequired,
+  children: PropTypes.array,
 };
 export default CodeBlock;
