388b838a049b549af40a8eafaa8c2381abbc30c9
Heather Moses <heather-moses@outlook.com>
2023-10-11 17:32:55 -0400

    Fix quotes

---
+++ client/src/components/exercise/lab9/components/Webpage.js
@@ -0,0 +1,161 @@
+import React from "react";
+
+const Webpage = () => {
+  return (
+    <div className="w-96 h-96 relative bg-white">
+      <img
+        className="w-96 h-96 left-0 top-[78px] absolute"
+        src="https://via.placeholder.com/1440x609"
+      />
+      <img
+        className="w-96 h-96 left-[-681px] top-[-1200px] absolute"
+        src="https://via.placeholder.com/4096x2726"
+      />
+      <div className="w-96 h-96 left-0 top-0 absolute bg-gradient-to-r from-stone-300 via-stone-300 to-stone-300" />
+      <div className="w-96 h-20 left-0 top-0 absolute bg-gray-200 shadow" />
+      <div className="w-96 h-96 left-0 top-[592px] absolute bg-stone-900" />
+      <div className="left-[83px] top-[16px] absolute text-stone-900 text-4xl font-light font-['Kumbh Sans']">
+        ALL University
+      </div>
+      <div className="left-[55px] top-[199px] absolute">
+        <span style="text-stone-900 text-6xl font-light font-['Kumbh Sans']">
+          Learning
+        </span>
+        <span style="text-stone-900 text-6xl font-extralight font-['Kumbh Sans']">
+          {" "}
+        </span>
+        <span style="text-stone-900 text-6xl font-bold font-['Kumbh Sans']">
+          Excellence
+        </span>
+      </div>
+      <div className="w-12 h-px left-[19px] top-[17px] absolute origin-top-left rotate-90 border-4 border-stone-900"></div>
+      <div className="w-14 h-px left-[19px] top-[17px] absolute border-4 border-stone-900"></div>
+      <div className="w-14 h-px left-[19px] top-[65px] absolute border-4 border-stone-900"></div>
+      <img
+        className="w-9 h-9 left-[31px] top-[20px] absolute"
+        src="https://via.placeholder.com/35x36"
+      />
+      <div className="w-36 h-7 left-[1138px] top-[28px] absolute text-center text-stone-900 text-base font-normal font-['Kumbh Sans']">
+        Who We Are
+      </div>
+      <div className="w-36 h-7 left-[57px] top-[318px] absolute text-center text-stone-900 text-base font-normal font-['Kumbh Sans']">
+        Who We Are
+      </div>
+      <div className="w-28 h-7 left-[1299px] top-[28px] absolute text-center text-stone-900 text-base font-normal font-['Kumbh Sans']">
+        Contact Us
+      </div>
+      <div className="w-32 h-8 left-[64px] top-[312px] absolute border-2 border-stone-900" />
+      <div className="w-24 h-7 left-[846px] top-[28px] absolute">
+        <div className="w-24 h-7 left-0 top-0 absolute text-center text-stone-900 text-base font-normal font-['Kumbh Sans']">
+          Careers
+        </div>
+      </div>
+      <div className="w-32 h-7 left-[978px] top-[28px] absolute">
+        <div className="w-32 h-7 left-0 top-0 absolute text-center text-stone-900 text-base font-['KyivType Sans']">
+          Education
+        </div>
+      </div>
+      <div className="w-80 h-8 left-[63px] top-[273px] absolute text-stone-900 text-xl font-light font-['Kumbh Sans']">
+        Here we make dreams a reality
+      </div>
+      <div className="w-96 h-96 left-[920px] top-[512px] absolute bg-zinc-300 shadow" />
+      <img
+        className="w-96 h-44 left-[920px] top-[958px] absolute shadow"
+        src="https://via.placeholder.com/457x172"
+      />
+      <div className="w-28 h-8 left-[1239px] top-[850px] absolute shadow border-2 border-stone-900" />
+      <div className="w-28 h-7 left-[1242px] top-[855px] absolute text-center text-stone-900 text-base font-normal font-['Kumbh Sans']">
+        Subscribe
+      </div>
+      <div className="w-72 h-72 left-[1058px] top-[542px] absolute">
+        <div className="w-72 h-8 left-0 top-0 absolute">
+          <div className="w-28 h-8 left-0 top-0 absolute text-stone-900 text-base font-normal font-['Kumbh Sans']">
+            Address Line
+          </div>
+          <div className="w-48 h-8 left-[105px] top-0 absolute border-2 border-black" />
+        </div>
+        <div className="w-72 h-8 left-0 top-[42px] absolute">
+          <div className="w-28 h-8 left-0 top-0 absolute text-stone-900 text-base font-normal font-['Kumbh Sans']">
+            Address Line 2
+          </div>
+          <div className="w-44 h-8 left-[118px] top-0 absolute border-2 border-black" />
+        </div>
+        <div className="w-9 h-7 left-0 top-[93px] absolute text-stone-900 text-base font-normal font-['Kumbh Sans']">
+          City
+        </div>
+        <div className="w-40 h-8 left-[43px] top-[93px] absolute border-2 border-black" />
+        <div className="w-44 h-8 left-0 top-[195px] absolute">
+          <div className="w-10 h-7 left-0 top-[3px] absolute text-stone-900 text-base font-normal font-['Kumbh Sans']">
+            State
+          </div>
+          <div className="w-32 h-8 left-[49px] top-0 absolute border-2 border-black" />
+          <div className="w-9 h-8 left-[140px] top-0 absolute bg-orange-950 bg-opacity-40 border-2 border-black" />
+        </div>
+        <div className="w-6 h-7 left-0 top-[144px] absolute text-stone-900 text-base font-normal font-['Kumbh Sans']">
+          Zip
+        </div>
+        <div className="w-20 h-8 left-[34px] top-[144px] absolute border-2 border-black" />
+        <div className="w-72 h-8 left-0 top-[246px] absolute">
+          <div className="w-12 h-7 left-0 top-0 absolute text-stone-900 text-base font-normal font-['Kumbh Sans']">
+            Phone
+          </div>
+          <div className="w-3.5 h-7 left-[125px] top-0 absolute text-center text-stone-900 text-base font-normal font-['Kumbh Sans']">
+            -
+          </div>
+          <div className="w-3.5 h-7 left-[213px] top-0 absolute text-center text-stone-900 text-base font-normal font-['Kumbh Sans']">
+            -
+          </div>
+          <div className="w-16 h-8 left-[58px] top-0 absolute border-2 border-black" />
+          <div className="w-16 h-8 left-[146px] top-0 absolute border-2 border-black" />
+          <div className="w-16 h-8 left-[232px] top-0 absolute border-2 border-black" />
+        </div>
+      </div>
+      <div className="w-96 h-96 left-[63px] top-[512px] absolute">
+        <div className="w-96 h-96 left-0 top-0 absolute bg-zinc-300 shadow" />
+        <div className="w-96 h-96 left-[50.86px] top-[15px] absolute">
+          <div className="w-96 h-px left-[54.39px] top-[19px] absolute border-4 border-stone-900"></div>
+          <div className="w-32 h-7 left-[582.70px] top-0 absolute text-center text-stone-900 text-2xl font-normal font-['Kumbh Sans']">
+            8/28/2027
+          </div>
+          <div className="w-96 h-96 left-0 top-[46px] absolute">
+            <div className="w-96 h-96 left-0 top-[67px] absolute text-justify text-stone-900 text-2xl font-light font-['Kumbh Sans']">
+              {" "}
+              Welcome to another edition of your weekly newsletter, your go-to
+              source for the latest updates, events, and highlights from our
+              vibrant campus community. As we settle into the rhythm of the
+              academic year, we&quot;re excited to keep you informed and engaged with
+              all that&quot;s happening here at ALL University.
+              <br />
+              <br /> In this week&quot;s issue, you&quot;ll find a curated selection of
+              news articles, student spotlights, and upcoming events that cater
+              to diverse interests. Whether you&quot;re a student, faculty member,
+              staff member, or an alum, there&quot;s something for everyone to
+              discover and enjoy. Stay connected, stay informed, and stay
+              inspired with your weekly newsletter. We look forward to sharin
+            </div>
+            <div className="w-96 left-[0.25px] top-0 absolute">
+              <span style="text-stone-900 text-4xl font-normal font-['Kumbh Sans']">
+                Weekly{" "}
+              </span>
+              <span style="text-stone-900 text-4xl font-bold font-['Kumbh Sans']">
+                Newsletter
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="w-40 h-px left-[1058px] top-[871px] absolute border-4 border-stone-900"></div>
+      <div className="left-[960px] top-[533px] absolute text-center">
+        <span style="text-stone-900 text-4xl font-light font-['Kumbh Sans']">
+          Join
+          <br />
+        </span>
+        <span style="text-stone-900 text-4xl font-bold font-['Kumbh Sans']">
+          Us
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Webpage;
