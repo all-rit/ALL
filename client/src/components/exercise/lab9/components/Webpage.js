42bafc36569640ee8da440490b4bcdc11c4c24b7
AndreasLc1103 <Andreaslc1103@gmail.com>
2023-10-12 12:26:15 -0400

    Layed out website component

--- client/src/components/exercise/lab9/components/Webpage.js
+++ client/src/components/exercise/lab9/components/Webpage.js
@@ -2,157 +2,168 @@ import React from "react";
 
 const Webpage = () => {
   return (
-    <div className="w-96 h-96 relative bg-white">
-      <img
-        className="w-96 h-96 left-0 top-[78px] absolute"
-        src="https://via.placeholder.com/1440x609"
-      />
-      <img
-        className="w-96 h-96 left-[-681px] top-[-1200px] absolute"
-        src="https://via.placeholder.com/4096x2726"
-      />
-      <div className="w-96 h-96 left-0 top-0 absolute bg-gradient-to-r from-stone-300 via-stone-300 to-stone-300" />
-      <div className="w-96 h-20 left-0 top-0 absolute bg-gray-200 shadow" />
-      <div className="w-96 h-96 left-0 top-[592px] absolute bg-stone-900" />
-      <div className="left-[83px] top-[16px] absolute text-stone-900 text-4xl font-light font-['Kumbh Sans']">
-        ALL University
-      </div>
-      <div className="left-[55px] top-[199px] absolute">
-        <span style="text-stone-900 text-6xl font-light font-['Kumbh Sans']">
-          Learning
-        </span>
-        <span style="text-stone-900 text-6xl font-extralight font-['Kumbh Sans']">
-          {" "}
-        </span>
-        <span style="text-stone-900 text-6xl font-bold font-['Kumbh Sans']">
-          Excellence
-        </span>
-      </div>
-      <div className="w-12 h-px left-[19px] top-[17px] absolute origin-top-left rotate-90 border-4 border-stone-900"></div>
-      <div className="w-14 h-px left-[19px] top-[17px] absolute border-4 border-stone-900"></div>
-      <div className="w-14 h-px left-[19px] top-[65px] absolute border-4 border-stone-900"></div>
-      <img
-        className="w-9 h-9 left-[31px] top-[20px] absolute"
-        src="https://via.placeholder.com/35x36"
-      />
-      <div className="w-36 h-7 left-[1138px] top-[28px] absolute text-center text-stone-900 text-base font-normal font-['Kumbh Sans']">
-        Who We Are
-      </div>
-      <div className="w-36 h-7 left-[57px] top-[318px] absolute text-center text-stone-900 text-base font-normal font-['Kumbh Sans']">
-        Who We Are
-      </div>
-      <div className="w-28 h-7 left-[1299px] top-[28px] absolute text-center text-stone-900 text-base font-normal font-['Kumbh Sans']">
-        Contact Us
-      </div>
-      <div className="w-32 h-8 left-[64px] top-[312px] absolute border-2 border-stone-900" />
-      <div className="w-24 h-7 left-[846px] top-[28px] absolute">
-        <div className="w-24 h-7 left-0 top-0 absolute text-center text-stone-900 text-base font-normal font-['Kumbh Sans']">
-          Careers
-        </div>
-      </div>
-      <div className="w-32 h-7 left-[978px] top-[28px] absolute">
-        <div className="w-32 h-7 left-0 top-0 absolute text-center text-stone-900 text-base font-['KyivType Sans']">
-          Education
-        </div>
-      </div>
-      <div className="w-80 h-8 left-[63px] top-[273px] absolute text-stone-900 text-xl font-light font-['Kumbh Sans']">
-        Here we make dreams a reality
-      </div>
-      <div className="w-96 h-96 left-[920px] top-[512px] absolute bg-zinc-300 shadow" />
-      <img
-        className="w-96 h-44 left-[920px] top-[958px] absolute shadow"
-        src="https://via.placeholder.com/457x172"
-      />
-      <div className="w-28 h-8 left-[1239px] top-[850px] absolute shadow border-2 border-stone-900" />
-      <div className="w-28 h-7 left-[1242px] top-[855px] absolute text-center text-stone-900 text-base font-normal font-['Kumbh Sans']">
-        Subscribe
-      </div>
-      <div className="w-72 h-72 left-[1058px] top-[542px] absolute">
-        <div className="w-72 h-8 left-0 top-0 absolute">
-          <div className="w-28 h-8 left-0 top-0 absolute text-stone-900 text-base font-normal font-['Kumbh Sans']">
-            Address Line
-          </div>
-          <div className="w-48 h-8 left-[105px] top-0 absolute border-2 border-black" />
-        </div>
-        <div className="w-72 h-8 left-0 top-[42px] absolute">
-          <div className="w-28 h-8 left-0 top-0 absolute text-stone-900 text-base font-normal font-['Kumbh Sans']">
-            Address Line 2
-          </div>
-          <div className="w-44 h-8 left-[118px] top-0 absolute border-2 border-black" />
-        </div>
-        <div className="w-9 h-7 left-0 top-[93px] absolute text-stone-900 text-base font-normal font-['Kumbh Sans']">
-          City
-        </div>
-        <div className="w-40 h-8 left-[43px] top-[93px] absolute border-2 border-black" />
-        <div className="w-44 h-8 left-0 top-[195px] absolute">
-          <div className="w-10 h-7 left-0 top-[3px] absolute text-stone-900 text-base font-normal font-['Kumbh Sans']">
-            State
-          </div>
-          <div className="w-32 h-8 left-[49px] top-0 absolute border-2 border-black" />
-          <div className="w-9 h-8 left-[140px] top-0 absolute bg-orange-950 bg-opacity-40 border-2 border-black" />
-        </div>
-        <div className="w-6 h-7 left-0 top-[144px] absolute text-stone-900 text-base font-normal font-['Kumbh Sans']">
-          Zip
-        </div>
-        <div className="w-20 h-8 left-[34px] top-[144px] absolute border-2 border-black" />
-        <div className="w-72 h-8 left-0 top-[246px] absolute">
-          <div className="w-12 h-7 left-0 top-0 absolute text-stone-900 text-base font-normal font-['Kumbh Sans']">
-            Phone
-          </div>
-          <div className="w-3.5 h-7 left-[125px] top-0 absolute text-center text-stone-900 text-base font-normal font-['Kumbh Sans']">
-            -
-          </div>
-          <div className="w-3.5 h-7 left-[213px] top-0 absolute text-center text-stone-900 text-base font-normal font-['Kumbh Sans']">
-            -
-          </div>
-          <div className="w-16 h-8 left-[58px] top-0 absolute border-2 border-black" />
-          <div className="w-16 h-8 left-[146px] top-0 absolute border-2 border-black" />
-          <div className="w-16 h-8 left-[232px] top-0 absolute border-2 border-black" />
-        </div>
-      </div>
-      <div className="w-96 h-96 left-[63px] top-[512px] absolute">
-        <div className="w-96 h-96 left-0 top-0 absolute bg-zinc-300 shadow" />
-        <div className="w-96 h-96 left-[50.86px] top-[15px] absolute">
-          <div className="w-96 h-px left-[54.39px] top-[19px] absolute border-4 border-stone-900"></div>
-          <div className="w-32 h-7 left-[582.70px] top-0 absolute text-center text-stone-900 text-2xl font-normal font-['Kumbh Sans']">
-            8/28/2027
-          </div>
-          <div className="w-96 h-96 left-0 top-[46px] absolute">
-            <div className="w-96 h-96 left-0 top-[67px] absolute text-justify text-stone-900 text-2xl font-light font-['Kumbh Sans']">
-              {" "}
-              Welcome to another edition of your weekly newsletter, your go-to
-              source for the latest updates, events, and highlights from our
-              vibrant campus community. As we settle into the rhythm of the
-              academic year, we&quot;re excited to keep you informed and engaged with
-              all that&quot;s happening here at ALL University.
-              <br />
-              <br /> In this week&quot;s issue, you&quot;ll find a curated selection of
-              news articles, student spotlights, and upcoming events that cater
-              to diverse interests. Whether you&quot;re a student, faculty member,
-              staff member, or an alum, there&quot;s something for everyone to
-              discover and enjoy. Stay connected, stay informed, and stay
-              inspired with your weekly newsletter. We look forward to sharin
+    <div className="tw-bg-white tw-flex tw-flex-col">
+      <div className="tw-flex-col tw-self-stretch tw-relative tw-flex tw-min-h-[1024px] tw-w-full tw-max-md:tw-max-w-full">
+        <img
+          loading="lazy"
+          srcSet="..."
+          className="tw-absolute tw-h-full tw-w-full tw-object-cover tw-object-center"
+        />
+        <div className="tw-relative tw-shadow-[3px_4px_16px_0px_rgba(0,0,0,0.25)] tw-bg-gray-200 tw-flex tw-w-full tw-flex-col tw-pl-5 tw-pr-5 tw-py-3 tw-max-md:tw-max-w-full">
+          <div className="tw-self-center tw-flex tw-ml-0 tw-w-full tw-max-w-[1373px] tw-items-start tw-justify-between tw-gap-5 tw-mt-px tw-mb-px tw-max-md:tw-max-w-full tw-max-md:tw-flex-wrap">
+            <div className="tw-self-stretch tw-flex tw-flex-col">
+              <div className="tw-text-[#260D0D]  tw-w-[54px] tw-h-1.5" />
+              <div className="tw-flex tw-w-[282px] tw-max-w-full tw-items-start tw-gap-2.5">
+                <img
+                  loading="lazy"
+                  srcSet="..."
+                  className="tw-aspect-[1.13] tw-object-cover tw-object-center tw-w-[54px] tw-shrink-0 tw-border-[6px] tw-border-solid"
+                />
+                <div className="tw-text-[#260D0D] tw-text-4xl tw-font-light tw-tracking-tighter tw-self-center tw-my-auto">
+                  ALL University
+                </div>
+              </div>
             </div>
-            <div className="w-96 left-[0.25px] top-0 absolute">
-              <span style="text-stone-900 text-4xl font-normal font-['Kumbh Sans']">
-                Weekly{" "}
-              </span>
-              <span style="text-stone-900 text-4xl font-bold font-['Kumbh Sans']">
-                Newsletter
-              </span>
+            <div className="tw-self-center tw-flex tw-items-start tw-justify-between tw-gap-2 tw-my-auto tw-max-md:tw-max-w-full tw-max-md:tw-flex-wrap tw-max-md:tw-justify-center">
+              <div className="tw-text-[#260D0D] tw-text-center tw-text-base">
+                Careers
+              </div>
+              <img
+                loading="lazy"
+                srcSet="..."
+                className="tw-aspect-[2] tw-object-cover tw-object-center tw-w-3.5 tw-shrink-0 mt-1"
+              />
+              <div className="tw-text-[#260D0D] tw-text-center tw-text-base tw-w-md">
+                Education
+              </div>
+              <img
+                loading="lazy"
+                srcSet="..."
+                className="tw-aspect-[2] tw-object-cover tw-object-center tw-w-3.5 tw-shrink-0 mt-1"
+              />
+              <div className="tw-text-[#260D0D] tw-text-center tw-text-base tw-w-md">
+                Who We Are
+              </div>
+              <div className="tw-text-[#260D0D] tw-text-center tw-text-base tw-w-md">
+                Contact Us
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="w-40 h-px left-[1058px] top-[871px] absolute border-4 border-stone-900"></div>
-      <div className="left-[960px] top-[533px] absolute text-center">
-        <span style="text-stone-900 text-4xl font-light font-['Kumbh Sans']">
-          Join
-          <br />
-        </span>
-        <span style="text-stone-900 text-4xl font-bold font-['Kumbh Sans']">
-          Us
-        </span>
+        <div className="tw-relative tw-flex tw-w-[567px] tw-max-w-full tw-flex-col tw-ml-14 tw-mt-36 tw-px-5">
+          <div className="tw-text-[#260D0D] tw-text-6xl tw-font-bold tw-tracking-tighter tw-max-md:tw-max-w-full tw-max-md:tw-text-4xl">
+            <span className="tw-font-light">Learning</span>
+            <span className="tw-font-extralight"> </span>
+            <span className="tw-font-bold">Excellence</span>
+          </div>
+          <div className="tw-text-[#260D0D] tw-text-xl tw-font-light tw-tracking-tighter tw-max-w-[354px] tw-ml-2">
+            Here we make dreams a reality
+          </div>
+          <div className="tw-text-[#260D0D] tw-text-center tw-text-base tw-w-[205px] tw-max-w-full tw-grow tw-ml-2.5 tw-mt-3.5 tw-pl-5 tw-pr-5 tw-pt-2.5 tw-pb-3 border-[3px] tw-border-solid tw-border-stone-900">
+            Who We Are
+          </div>
+        </div>
+        <div className="tw-relative tw-bg-[#260D0D] tw-flex tw-w-full tw-flex-col tw-mt-60 tw-px-5 tw-max-md:tw-max-w-full tw-max-md:mt-52">
+          <div className="tw-self-center tw-z-[1] tw-w-full tw-max-w-[1337px] tw-ml-4 tw-mt-20 tw-max-md:tw-max-w-full">
+            <div className="tw-gap-5 tw-flex tw-max-md:tw-flex-col tw-max-md:tw-items-stretch tw-max-md:tw-gap-0">
+              <div className="tw-flex tw-flex-col tw-items-stretch tw-w-[28%] tw-max-md:tw-w-full">
+                <div className="tw-flex tw-grow tw-flex-col tw-w-[355px] tw-max-md:mt-12">
+                  <div className="tw-shadow-[3px_4px_7px_0px_rgba(0,0,0,0.25)] tw-bg-[#D9D9D9] tw-self-stretch tw-flex tw-w-full tw-flex-col tw-pt-7 tw-pb-4 tw-px-5">
+                    <div className="tw-flex tw-w-[265px] tw-max-w-full tw-items-start tw-justify-between tw-gap-5 tw-ml-3 tw-max-md:tw-ml-2.5">
+                      <div className="tw-text-[#260D0D] tw-text-center tw-text-4xl tw-font-bold tw-tracking-tighter tw-mt-px">
+                        <span className="tw-font-light">
+                          Join
+                          <br />
+                        </span>
+                        <span className="tw-font-bold">Us</span>
+                      </div>
+                      <div className="tw-text-[#260D0D] tw-text-xl tw-font-light tw-w-[207px]">
+                        Unlock your full potential and embrace endless
+                        opportunities by applying to ALL University, where
+                        innovation meets excellence in education.
+                      </div>
+                    </div>
+                    <div className="tw-flex w-[310px] tw-max-w-full tw-items-start tw-justify-between tw-gap-5 tw-ml-0.5 tw-mt-9">
+                      <div className="tw-bg-stone-900 tw-self-center tw-w-[198px] tw-h-[5px] tw-my-auto" />
+                      <div className="tw-text-[#260D0D] tw-text-center tw-text-base tw-font-[350] tw-self-stretch tw-w-[78px] tw-shadow-[0px_4px_7px_0px_rgba(0,0,0,0.25)] tw-max-w-full tw-pl-4 tw-pr-4 tw-pt-2.5 tw-pb-2.5 tw-border-[3px] tw-border-solid tw-border-stone-900">
+                        Apply
+                      </div>
+                    </div>
+                  </div>
+                  <img
+                    loading="lazy"
+                    srcSet="..."
+                    className="tw-aspect-[2.57] tw-object-cover tw-object-center tw-w-full tw-shadow-[3px_4px_7px_rgba(0,0,0,0.25)] tw-self-stretch tw-grow tw-mt-12"
+                  />
+                </div>
+              </div>
+              <div className="tw-flex tw-flex-col tw-items-stretch tw-w-[72%] tw-ml-5 tw-max-md:tw-w-full">
+                <div className="tw-shadow-[3px_4px_7px_0px_rgba(0,0,0,0.25)] tw-bg-[#D9D9D9] tw-flex tw-grow tw-flex-col tw-w-full tw-mx-auto tw-px-5 tw-py-5 tw-max-md:tw-max-w-full tw-max-md:tw-mt-12">
+                  <div className="tw-self-stretch tw-flex tw-mb-0 tw-flex-col tw-ml-0.5 tw-mr-4 tw-mt-0.5 tw-max-md:tw-max-w-full tw-max-md:mr-2.5">
+                    <div className="tw-flex tw-w-[723px] tw-max-w-full tw-items-start tw-justify-between tw-gap-5 tw-max-md:flex-wrap">
+                      <div className="tw-bg-gray tw-self-center tw-w-[577px] tw-h-[5px] tw-grow tw-shrink-0 tw-basis-auto tw-my-auto tw-max-md:tw-max-w-full" />
+                      <div className="tw-text-[#260D0D] tw-text-center tw-text-2xl tw-self-stretch tw-w-[155px]">
+                        8/28/2027
+                      </div>
+                    </div>
+                    <div className="tw-text-[#260D0D] tw-text-4xl tw-font-bold tw-tracking-tighter tw-w-[373px] tw-max-w-full tw-ml-1.5 tw-mt-4">
+                      <span className="">Weekly </span>
+                      <span className="font-bold">Newsletter</span>
+                    </div>
+                    <div className="tw-text-[#260D0D] tw-text-justify tw-text-2xl tw-font-light tw-max-w-[711px] tw-ml-2 tw-mt-6 tw-max-md:tw-max-w-full">
+                      {" "}
+                      Welcome to another edition of your weekly newsletter, your
+                      go-to source for the latest updates, events, and
+                      highlights from our vibrant campus community. As we settle
+                      into the rhythm of the academic year, we`&apos;`re excited
+                      to keep you informed and engaged with all that &apos s
+                      happening here at ALL University.
+                    </div>
+                    <div className="tw-flex tw-w-[717px] tw-max-w-full tw-items-start tw-gap-2.5 tw-mt-14 tw-max-md:tw-flex-wrap">
+                      <div className="tw-justify-center tw-text-[#260D0D] tw-text-base tw-self-center tw-w-[113px] tw-my-auto">
+                        Address Line
+                      </div>
+                      <div className="tw-self-stretch tw-flex tw-w-[612px] tw-h-[31px] tw-flex-col tw-grow tw-shrink-0 tw-basis-auto tw-border-[3px] tw-border-solid tw-border-black tw-max-md:max-w-full" />
+                    </div>
+                    <div className="tw-self-center tw-flex tw-ml-0 w-[656px] tw-max-w-full tw-items-start tw-justify-between tw-gap-5 tw-mt-7 tw-max-md:flex-wrap">
+                      <div className="tw-flex tw-grow tw-shrink-0 tw-basis-auto tw-items-start tw-justify-between tw-gap-3.5 tw-max-md:max-w-full tw-max-md:tw-flex-wrap tw-max-md:tw-justify-center">
+                        <div className="tw-justify-center tw-text-[#260D0D] tw-text-base tw-self-center tw-my-auto">
+                          City
+                        </div>
+                        <div className="tw-self-stretch tw-flex tw-w-[162px] tw-h-[31px] tw-flex-col tw-border-[3px] tw-border-solid tw-border-black" />
+                        <div className="tw-justify-center tw-text-[#260D0D] tw-text-base tw-self-center tw-my-auto">
+                          State
+                        </div>
+                        <img
+                          loading="lazy"
+                          srcSet="..."
+                          className="tw-aspect-[5.23] tw-object-cover tw-object-center tw-w-[162px] tw-shrink-0 tw-border-[3px] tw-border-solid tw-border-black"
+                        />
+                      </div>
+                      <div className="tw-flex tw-items-start tw-gap-3 tw-mt-0.5">
+                        <div className="tw-justify-center tw-text-[#260D0D] tw-text-base tw-self-center tw-my-auto">
+                          Zip
+                        </div>
+                        <div className="tw-self-stretch tw-flex tw-w-[141px] tw-h-[31px] tw-flex-col tw-border-[3px] tw-border-solid tw-border-black" />
+                      </div>
+                    </div>
+                    <div className="tw-flex w-[813px] tw-max-w-full tw-items-start tw-justify-between tw-gap-3 tw-mr-1 tw-mt-7 tw-max-md:tw-flex-wrap tw-max-md:tw-justify-center">
+                      <div className="tw-justify-center tw-text-[#260D0D] tw-text-base tw-self-center tw-my-auto">
+                        Phone
+                      </div>
+                      <div className="tw-flex tw-w-[141px] tw-h-[31px] tw-flex-col tw-border-[3px] tw-border-solid tw-border-black" />
+                      <div className="tw-shadow-[0px_4px_7px_0px_rgba(0,0,0,0.25)] tw-self-stretch tw-flex tw-w-[137px] tw-max-w-full tw-flex-col tw-px-5 tw-py-2.5 border-[3px] border-solid border-stone-900">
+                        <div className="tw-justify-center tw-text-[#260D0D] tw-text-center tw-text-base tw-self-center tw-mb-0 w-[147px] mt-0.5">
+                          Sign Up
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
