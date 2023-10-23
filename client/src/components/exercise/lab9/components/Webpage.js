e5002d18078ae0d477b88db68ee8125add7a30a9
Heather Moses <heather-moses@outlook.com>
2023-10-23 16:35:22 -0400

    Componentizing webpage and small style changes

--- client/src/components/exercise/lab9/components/Webpage.js
+++ client/src/components/exercise/lab9/components/Webpage.js
@@ -1,7 +1,11 @@
 import React from "react";
-import logo from "../../../../assets/images/lab9/logo.png";
 import uni from "../../../../assets/images/lab9/uni.jpeg";
-import person from "../../../../assets/images/lab9/person.jpeg";
+
+import NewsletterForm from "./webpage-subcomponents/NewsletterForm";
+import WebpageNav from "./webpage-subcomponents/WebpageNav";
+import WebpageHeader from "./webpage-subcomponents/WebpageHeader";
+import WebpageSidebar from "./webpage-subcomponents/WebpageSidebar";
+
 const Webpage = () => {
   return (
     <div className="tw-bg-white tw-flex tw-flex-col">
@@ -11,158 +15,15 @@ const Webpage = () => {
           src={uni}
           className="tw-absolute tw-h-full tw-w-full tw-object-cover tw-object-center"
         />
-        <div className="tw-relative tw-shadow-[3px_4px_16px_0px_rgba(0,0,0,0.25)] tw-bg-[#E8E8E8] tw-flex tw-w-full tw-flex-col tw-pl-5 tw-pr-5 tw-py-3 tw-max-md:tw-max-w-full">
-          <div className="tw-self-center tw-flex tw-ml-0 tw-w-full tw-max-w-[1373px] tw-items-start tw-justify-between tw-gap-5 tw-mt-px tw-mb-px tw-max-md:tw-max-w-full tw-max-md:tw-flex-wrap">
-            <div className="tw-self-stretch tw-flex tw-flex-col">
-              <div className="tw-text-[#260D0D]  tw-w-[54px] tw-h-1.5" />
-              <div className="tw-flex tw-w-[282px] tw-max-w-full tw-items-start tw-gap-2.5">
-                <img
-                  loading="lazy"
-                  srcSet={logo}
-                  className="tw-aspect-[1.13] tw-object-cover tw-object-center tw-w-[54px] tw-shrink-0 tw-border-[6px] tw-border-solid tw-border-stone-900"
-                />
-                <div className="tw-text-[#260D0D] tw-text-4xl tw-font-light tw-tracking-tighter tw-self-center tw-my-auto">
-                  ALL University
-                </div>
-              </div>
-            </div>
-            <div className="tw-self-center tw-flex tw-items-start tw-justify-between tw-gap-2 tw-my-auto tw-max-md:tw-max-w-full tw-max-md:tw-flex-wrap tw-max-md:tw-justify-center">
-              <div className="tw-text-[#260D0D] tw-text-center tw-text-base">
-                Careers
-              </div>
-              <img
-                loading="lazy"
-                srcSet="..."
-                className="tw-aspect-[2] tw-object-cover tw-object-center tw-w-3.5 tw-shrink-0 mt-1"
-              />
-              <div className="tw-text-[#260D0D] tw-text-center tw-text-base tw-w-md">
-                Education
-              </div>
-              <img
-                loading="lazy"
-                srcSet="..."
-                className="tw-aspect-[2] tw-object-cover tw-object-center tw-w-3.5 tw-shrink-0 mt-1"
-              />
-              <div className="tw-text-[#260D0D] tw-text-center tw-text-base tw-w-md">
-                Who We Are
-              </div>
-              <div className="tw-text-[#260D0D] tw-text-center tw-text-base tw-w-md">
-                Contact Us
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="tw-relative tw-flex tw-w-[567px] tw-max-w-full tw-flex-col tw-ml-14 tw-mt-36 tw-px-5">
-          <div className="tw-text-[#260D0D] tw-text-6xl tw-font-bold tw-tracking-tighter tw-max-md:tw-max-w-full tw-max-md:tw-text-4xl">
-            <span className="tw-font-light">Learning</span>
-            <span className="tw-font-extralight"> </span>
-            <span className="tw-font-bold">Excellence</span>
-          </div>
-          <div className="tw-text-[#260D0D] tw-text-xl tw-font-light tw-tracking-tighter tw-max-w-[354px] tw-ml-2">
-            Here we make dreams a reality
-          </div>
-          <div className="tw-text-[#260D0D] tw-text-center tw-text-base tw-w-[205px] tw-max-w-full tw-grow tw-ml-2.5 tw-mt-3.5 tw-pl-5 tw-pr-5 tw-pt-2.5 tw-pb-3 border-[3px] tw-border-solid tw-border-stone-900">
-            Who We Are
-          </div>
-        </div>
+        <WebpageNav />
+        <WebpageHeader />
         <div className="tw-relative tw-bg-[#260D0D] tw-flex tw-w-full tw-flex-col tw-mt-60 tw-px-5 tw-max-md:tw-max-w-full tw-max-md:mt-52">
-          <div className="tw-self-center tw-z-[1] tw-w-full tw-ml-4 tw-mt-20 tw-max-md:tw-max-w-full">
+          <div className="tw-self-center tw-w-full tw-ml-4 tw-mt-20 tw-max-md:tw-max-w-full">
             <div className="tw-gap-5 tw-flex tw-max-md:tw-flex-col tw-max-md:tw-items-stretch tw-max-md:tw-gap-0">
               <div className="tw-flex tw-flex-col tw-items-stretch tw-w-[28%] tw-max-md:tw-w-full">
-                <div className="tw-flex tw-grow tw-flex-col tw-max-md:mt-12">
-                  <div className="tw-shadow-[3px_4px_7px_0px_rgba(0,0,0,0.25)] tw-bg-[#D9D9D9] tw-self-stretch tw-flex tw-w-full tw-flex-col tw-pt-7 tw-pb-4 tw-px-5">
-                    <div className="tw-flex tw-w-xl tw-max-w-full tw-items-start tw-justify-between tw-gap-5 tw-ml-3 tw-max-md:tw-ml-2.5">
-                      <div className="tw-text-[#260D0D] tw-text-center tw-text-3xl tw-font-bold tw-tracking-tighter tw-mt-px">
-                        <span className="tw-font-light">
-                          Join
-                          <br />
-                        </span>
-                        <span className="tw-font-bold">Us</span>
-                      </div>
-                      <div className="tw-text-[#260D0D] tw-text-lg tw-font-light">
-                        Unlock your full potential and embrace endless
-                        opportunities by applying to ALL University, where
-                        innovation meets excellence in education.
-                      </div>
-                    </div>
-                    <div className="tw-flex tw-max-w-full tw-items-start tw-justify-between tw-gap-5 tw-ml-0.5 tw-mt-9">
-                      <div className="tw-bg-[#D9D9D9] tw-self-center tw-my-auto" />
-                      <div className="tw-text-[#260D0D] tw-text-center tw-text-base tw-self-stretch tw-shadow-[0px_4px_7px_0px_rgba(0,0,0,0.25)] tw-max-w-full tw-pl-4 tw-pr-4 tw-pt-2.5 tw-pb-2.5 tw-border-[3px] tw-border-solid">
-                        Apply
-                      </div>
-                    </div>
-                  </div>
-                  <img
-                    loading="lazy"
-                    srcSet={person}
-                    className="tw-aspect-[2.57] tw-object-cover tw-object-center tw-w-full tw-shadow-[3px_4px_7px_rgba(0,0,0,0.25)] tw-self-stretch tw-grow tw-mt-12"
-                  />
-                </div>
+                <WebpageSidebar />
               </div>
-              <div className="tw-flex tw-flex-col tw-items-stretch tw-w-[72%] tw-ml-5 tw-max-md:tw-w-full">
-                <div className="tw-shadow-[3px_4px_7px_0px_rgba(0,0,0,0.25)] tw-bg-[#D9D9D9] tw-flex tw-grow tw-flex-col tw-w-full tw-mx-auto tw-px-5 tw-py-5 tw-max-md:tw-max-w-full tw-max-md:tw-mt-12">
-                  <div className="tw-self-stretch tw-flex tw-mb-0 tw-flex-col tw-ml-0.5 tw-mr-4 tw-mt-0.5 tw-max-md:tw-max-w-full tw-max-md:mr-2.5">
-                    <div className="tw-flex tw-w-[723px] tw-max-w-full tw-items-start tw-justify-between tw-gap-5 tw-max-md:flex-wrap">
-                      <div className="tw-bg-gray tw-self-center tw-w-[577px] tw-h-[5px] tw-grow tw-shrink-0 tw-basis-auto tw-my-auto tw-max-md:tw-max-w-full" />
-                      <div className="tw-text-[#260D0D] tw-text-center tw-text-2xl tw-self-stretch tw-w-[155px]">
-                        8/28/2027
-                      </div>
-                    </div>
-                    <div className="tw-text-[#260D0D] tw-text-4xl tw-font-bold tw-tracking-tighter tw-w-[373px] tw-max-w-full tw-ml-1.5 tw-mt-4">
-                      <span className="">Weekly </span>
-                      <span className="font-bold">Newsletter</span>
-                    </div>
-                    <div className="tw-text-[#260D0D] tw-text-justify tw-text-2xl tw-font-light tw-max-w-[711px] tw-ml-2 tw-mt-6 tw-max-md:tw-max-w-full">
-                      {" "}
-                      Welcome to another edition of your weekly newsletter, your
-                      go-to source for the latest updates, events, and
-                      highlights from our vibrant campus community. As we settle
-                      into the rhythm of the academic year, we&apos;re excited
-                      to keep you informed and engaged with all that &apos s
-                      happening here at ALL University.
-                    </div>
-                    <div className="tw-flex tw-w-[717px] tw-max-w-full tw-items-start tw-gap-2.5 tw-mt-14 tw-max-md:tw-flex-wrap">
-                      <div className="tw-justify-center tw-text-[#260D0D] tw-text-base tw-self-center tw-w-[113px] tw-my-auto">
-                        Address Line
-                      </div>
-                      <div className="tw-self-stretch tw-flex tw-w-[612px] tw-h-[31px] tw-flex-col tw-grow tw-shrink-0 tw-basis-auto tw-border-[3px] tw-border-solid tw-border-black tw-max-md:max-w-full" />
-                    </div>
-                    <div className="tw-self-center tw-flex tw-ml-0 tw-max-w-full tw-items-start tw-justify-between tw-gap-5 tw-mt-7 tw-max-md:flex-wrap">
-                      <div className="tw-flex tw-grow tw-shrink-0 tw-basis-auto tw-items-start tw-justify-between tw-gap-3.5 tw-max-md:max-w-full tw-max-md:tw-flex-wrap tw-max-md:tw-justify-center">
-                        <div className="tw-justify-center tw-text-[#260D0D] tw-text-base tw-self-center tw-my-auto">
-                          City
-                        </div>
-                        <div className="tw-self-stretch tw-flex tw-w-[162px] tw-h-[31px] tw-flex-col tw-border-[3px] tw-border-solid tw-border-black" />
-                        <div className="tw-justify-center tw-text-[#260D0D] tw-text-base tw-self-center tw-my-auto">
-                          State
-                        </div>
-                        <img
-                          loading="lazy"
-                          srcSet="..."
-                          className="tw-aspect-[5.23] tw-object-cover tw-object-center tw-w-[162px] tw-shrink-0 tw-border-[3px] tw-border-solid tw-border-black"
-                        />
-                      </div>
-                      <div className="tw-flex tw-items-start tw-gap-3 tw-mt-0.5">
-                        <div className="tw-justify-center tw-text-[#260D0D] tw-text-base tw-self-center tw-my-auto">
-                          Zip
-                        </div>
-                        <div className="tw-self-stretch tw-flex tw-w-[141px] tw-h-[31px] tw-flex-col tw-border-[3px] tw-border-solid tw-border-black" />
-                      </div>
-                    </div>
-                    <div className="tw-flex w-[813px] tw-max-w-full tw-items-start tw-justify-between tw-gap-3 tw-mr-1 tw-mt-7 tw-max-md:tw-flex-wrap tw-max-md:tw-justify-center">
-                      <div className="tw-justify-center tw-text-[#260D0D] tw-text-base tw-self-center tw-my-auto">
-                        Phone
-                      </div>
-                      <div className="tw-flex tw-w-[141px] tw-h-[31px] tw-flex-col tw-border-[3px] tw-border-solid tw-border-black" />
-                      <div className="tw-shadow-[0px_4px_7px_0px_rgba(0,0,0,0.25)] tw-self-stretch tw-flex tw-w-[137px] tw-max-w-full tw-flex-col tw-px-5 tw-py-2.5 border-[3px] border-solid border-stone-900">
-                        <div className="tw-justify-center tw-text-[#260D0D] tw-text-center tw-text-base tw-self-center tw-mb-0 w-[147px] mt-0.5">
-                          Sign Up
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
+              <NewsletterForm />
             </div>
           </div>
         </div>
---
+++ client/src/components/exercise/lab9/components/webpage-subcomponents/NewsletterForm.js
@@ -0,0 +1,68 @@
+import React from "react";
+
+const WebpageForm = () => {
+    return (
+        <div className="tw-flex tw-flex-col tw-items-stretch tw-w-[72%] tw-ml-5 tw-max-md:tw-w-full">
+                <div className="tw-shadow-[3px_4px_7px_0px_rgba(0,0,0,0.25)] tw-bg-[#D9D9D9] tw-flex tw-grow tw-flex-col tw-w-full tw-mx-auto tw-px-5 tw-py-5 tw-max-md:tw-max-w-full tw-max-md:tw-mt-12">
+                  <div className="tw-self-stretch tw-flex tw-mb-0 tw-flex-col tw-ml-0.5 tw-mr-4 tw-mt-0.5 tw-max-md:tw-max-w-full tw-max-md:mr-2.5">
+                    <div className="tw-flex tw-w-[723px] tw-max-w-full tw-items-start tw-justify-between tw-gap-5 tw-max-md:flex-wrap">
+                      <div className="tw-bg-gray tw-self-center tw-w-[577px] tw-h-[5px] tw-grow tw-shrink-0 tw-basis-auto tw-my-auto tw-max-md:tw-max-w-full" />
+                      <div className="tw-text-[#260D0D] tw-text-center tw-text-2xl tw-self-stretch tw-w-[155px]">
+                        8/28/2027
+                      </div>
+                    </div>
+                    <div className="tw-text-[#260D0D] tw-text-4xl tw-font-bold tw-tracking-tighter tw-w-[373px] tw-max-w-full tw-ml-1.5 tw-mt-4">
+                      <span className="">Weekly </span>
+                      <span className="font-bold">Newsletter</span>
+                    </div>
+                    <div className="tw-text-[#260D0D] tw-text-justify tw-text-2xl tw-font-light tw-max-w-[711px] tw-ml-2 tw-mt-6 tw-max-md:tw-max-w-full">
+                      {" "}
+                      Welcome to another edition of your weekly newsletter, your
+                      go-to source for the latest updates, events, and
+                      highlights from our vibrant campus community. As we settle
+                      into the rhythm of the academic year, we&apos;re excited
+                      to keep you informed and engaged with all that &apos s
+                      happening here at ALL University.
+                    </div>
+                    <div className="tw-flex tw-w-[717px] tw-max-w-full tw-items-start tw-gap-2.5 tw-mt-14 tw-max-md:tw-flex-wrap">
+                      <div className="tw-justify-center tw-text-[#260D0D] tw-text-base tw-self-center tw-w-[113px] tw-my-auto">
+                        Address Line
+                      </div>
+                      <div className="tw-self-stretch tw-flex tw-w-[612px] tw-h-[31px] tw-flex-col tw-grow tw-shrink-0 tw-basis-auto tw-border-[3px] tw-border-solid tw-border-black tw-max-md:max-w-full" />
+                    </div>
+                    <div className="tw-self-center tw-flex tw-ml-0 tw-max-w-full tw-items-start tw-justify-between tw-gap-5 tw-mt-7 tw-max-md:flex-wrap">
+                      <div className="tw-flex tw-grow tw-shrink-0 tw-basis-auto tw-items-start tw-justify-between tw-gap-3.5 tw-max-md:max-w-full tw-max-md:tw-flex-wrap tw-max-md:tw-justify-center">
+                        <div className="tw-justify-center tw-text-[#260D0D] tw-text-base tw-self-center tw-my-auto">
+                          City
+                        </div>
+                        <div className="tw-self-stretch tw-flex tw-w-[162px] tw-h-[31px] tw-flex-col tw-border-[3px] tw-border-solid tw-border-black" />
+                        <div className="tw-justify-center tw-text-[#260D0D] tw-text-base tw-self-center tw-my-auto">
+                          State
+                        </div>
+                        <div className="tw-self-stretch tw-flex tw-w-[162px] tw-h-[31px] tw-flex-col tw-border-[3px] tw-border-solid tw-border-black" />
+                      </div>
+                      <div className="tw-flex tw-items-start tw-gap-3 tw-mt-0.5">
+                        <div className="tw-justify-center tw-text-[#260D0D] tw-text-base tw-self-center tw-my-auto">
+                          Zip
+                        </div>
+                        <div className="tw-self-stretch tw-flex tw-w-[141px] tw-h-[31px] tw-flex-col tw-border-[3px] tw-border-solid tw-border-black" />
+                      </div>
+                    </div>
+                    <div className="tw-flex w-[813px] tw-max-w-full tw-items-start tw-justify-between tw-gap-3 tw-mr-1 tw-mt-7 tw-max-md:tw-flex-wrap tw-max-md:tw-justify-center">
+                      <div className="tw-flex tw-items-start tw-gap-3 tw-mt-0.5">
+                        <div className="tw-justify-center tw-text-[#260D0D] tw-text-base tw-self-center tw-my-auto">
+                          Phone
+                        </div>
+                        <div className="tw-self-stretch tw-flex tw-w-[141px] tw-h-[31px] tw-flex-col tw-border-[3px] tw-border-solid tw-border-black" />
+                      </div>
+                      <div className="tw-text-[#260D0D] tw-text-center tw-text-base tw-self-stretch tw-shadow-[0px_4px_7px_0px_rgba(0,0,0,0.25)] tw-max-w-full tw-pl-4 tw-pr-4 tw-pt-2.5 tw-pb-2.5 tw-border-[3px] tw-border-solid">
+                        Sign Up
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+    )
+}
+
+export default WebpageForm;
