3b25fd114eb2f2033e66209e7d3b0b1dbbae9ba9
AndreasLc1103 <Andreaslc1103@gmail.com>
2023-10-23 10:48:31 -0400

    minor style changes

--- client/src/components/exercise/lab9/hooks/useRepairNav.js
+++ client/src/components/exercise/lab9/hooks/useRepairNav.js
@@ -2,7 +2,7 @@
  * useRepairNav(): is a function that is responsible for handling the logic
  * behind the repair navigation section of the new Lab 9 localization. This
  * hook is decoupled to allow for the ease in handling custom logic for the
- * componentgit
+ * component
  */
 const useRepairNav = () => {};
 
