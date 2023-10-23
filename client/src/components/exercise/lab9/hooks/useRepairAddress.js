b66b8150ab6a30a127591ffc408f6599a346c518
Andres Leonard-Calcano <darkninjawolf1@gmail.com>
2023-10-23 11:33:48 -0400

    Update useRepairAddress.js

--- client/src/components/exercise/lab9/hooks/useRepairAddress.js
+++ client/src/components/exercise/lab9/hooks/useRepairAddress.js
@@ -1,5 +1,5 @@
 /**
- * UseRepairAddress(): sis a custom hook responsible for handling the behavior
+ * UseRepairAddress(): is a custom hook responsible for handling the behavior
  * of the Repair address repair section for the new LAB 9 localization.
  * This is to be used to allow for decoupling of the display and logic based portions of
  * repair section.
