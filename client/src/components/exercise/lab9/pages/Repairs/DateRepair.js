df6879496b0dcff2b0f89b653f37612b102951d8
AndreasLc1103 <Andreaslc1103@gmail.com>
2023-10-23 10:46:48 -0400

    documented new hooks and components

--- client/src/components/exercise/lab9/pages/Repairs/DateRepair.js
+++ client/src/components/exercise/lab9/pages/Repairs/DateRepair.js
@@ -4,7 +4,13 @@ import { PropTypes } from "prop-types";
 import useRepairDate from "../../hooks/useRepairDate";
 import Repair from "../../../../body/Repair/Repair";
 import { GAME_STATES, REPAIR } from "../../../../../constants/lab9";
-
+/**
+ * Date Repair is a component that is responsible for passing logic into the universal
+ * repair page component, what this allows us to do is call the re-useable repair component
+ * with custom logic pertaining to that repair section.
+ * @param {String} user contains user id for data state and logging user input
+ * @returns 
+ */
 const DateRepair = ({ user = "" }) => {
   const { data, functions } = useRepairDate(user);
   const {} = data;
--- client/src/components/exercise/lab9/pages/Repairs/NavBarRepair.js
+++ client/src/components/exercise/lab9/pages/Repairs/NavBarRepair.js
@@ -2,7 +2,12 @@ import React from "react";
 import { PropTypes } from "prop-types";
 import Repair from "../../../../body/Repair/Repair";
 import { GAME_STATES, REPAIR } from "../../../../../constants/lab9";
-
+/**
+ * NavBarRepair() is a react component that is responsible for passing down
+ * the visual information needed to render the repair page itself. This
+ * component will integrate the individualized custom hook to handle custom logic.
+ * @returns Repair Component to render to the browser.
+ */
 const NavBarRepair = () => {
   return (
     <Repair
