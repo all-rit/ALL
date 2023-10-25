5c380dcf99228f090949f2fdb0f3fc5d7d04ca57
Heather Moses <heather-moses@outlook.com>
2023-10-25 14:59:28 -0400

    Style

--- client/src/constants/lab9/AddressRepairData.js
+++ client/src/constants/lab9/AddressRepairData.js
@@ -1,40 +1,46 @@
 export const AddressRepairData = {
-    countries: [
-        {
-            id: 0,
-            countryVariable: "us",
-            countryName: "united_states",
-            comment: "// enter the following address infomation: streetAddress, city, state, postalCode",
-            userInput: "",
-            validate_expression: "",
-            correctAddressFormat: "streetAddress, city, state, postalCode"
-        },
-        {
-            id: 1,
-            countryVariable: "uk",
-            countryName: "united_kingdom",
-            comment: "// enter the following address infomation: streetAddress, locality, city, postalCode",
-            userInput: "",
-            validate_expression: "",
-            correctAddressFormat: "streetAddress, locality, city, postalCode"
-        },
-        {
-            id: 2,
-            countryVariable: "jp",
-            countryName: "japan",
-            comment: "// enter the following address infomation: postalCode, prefecture, city, ward, block, buildingName",
-            userInput: "",
-            validate_expression: "",
-            correctAddressFormat: "postalCode, prefecture, city, ward, block, buildingName"
-        },
-        {
-            id: 2,
-            countryVariable: "db",
-            countryName: "dubai",
-            comment: "// enter the following address infomation: streetAddress, locality, city, emirate, postalCode",
-            userInput: "",
-            validate_expression: "",
-            correctAddressFormat: "streetAddress, locality, city, emirate, postalCode"
-        }
-    ]
-}
+  countries: [
+    {
+      id: 0,
+      countryVariable: "us",
+      countryName: "united_states",
+      comment:
+        "// enter the following address infomation: streetAddress, city, state, postalCode",
+      userInput: "",
+      validate_expression: "",
+      correctAddressFormat: "streetAddress, city, state, postalCode",
+    },
+    {
+      id: 1,
+      countryVariable: "uk",
+      countryName: "united_kingdom",
+      comment:
+        "// enter the following address infomation: streetAddress, locality, city, postalCode",
+      userInput: "",
+      validate_expression: "",
+      correctAddressFormat: "streetAddress, locality, city, postalCode",
+    },
+    {
+      id: 2,
+      countryVariable: "jp",
+      countryName: "japan",
+      comment:
+        "// enter the following address infomation: postalCode, prefecture, city, ward, block, buildingName",
+      userInput: "",
+      validate_expression: "",
+      correctAddressFormat:
+        "postalCode, prefecture, city, ward, block, buildingName",
+    },
+    {
+      id: 2,
+      countryVariable: "db",
+      countryName: "dubai",
+      comment:
+        "// enter the following address infomation: streetAddress, locality, city, emirate, postalCode",
+      userInput: "",
+      validate_expression: "",
+      correctAddressFormat:
+        "streetAddress, locality, city, emirate, postalCode",
+    },
+  ],
+};
