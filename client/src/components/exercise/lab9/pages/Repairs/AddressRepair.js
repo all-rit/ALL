ecbd7145a56acc222f98a3effeefad7c3e183608
AndreasLc1103 <Andreaslc1103@gmail.com>
2023-11-03 16:54:18 -0400

    formatting changes

--- client/src/components/exercise/lab9/pages/Repairs/AddressRepair.js
+++ client/src/components/exercise/lab9/pages/Repairs/AddressRepair.js
@@ -1,8 +1,10 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
 import { PropTypes } from "prop-types";
 import Repair from "../../../../body/Repair/Repair";
 import { GAME_STATES, REPAIR } from "../../../../../constants/lab9";
 import useRepairAddress from "../../hooks/useRepairAddress";
+import DateFormRepair from "../DateFormRepair";
 /**
  * AddressRepair: is a Component responsible for passing in both logic and information
  * into the universal repair component. This allows for the ability to handle the custom routing
@@ -11,12 +13,24 @@ import useRepairAddress from "../../hooks/useRepairAddress";
  * @returns Component to handle custom logic for the lab.
  */
 const AddressRepair = (user = "") => {
-  // eslint-disable-next-line no-unused-vars
   const { data, functions } = useRepairAddress(user);
+  const { exercisePromptsState } = data;
+  const { handleUserInputChange, checkInputValid, fetchRepair, postRepair } =
+    functions;
   return (
     <Repair
+      fileName={"DateFormat.js"}
       path={`${REPAIR}/${GAME_STATES.REPAIR_ADDRESS_FORM}`}
-      CodeImplementation={() => {}}
+      headingText={""}
+      validateRepair={checkInputValid}
+      fetchRepair={() => fetchRepair()}
+      submitRepair={() => postRepair()}
+      repairText={[
+        "in this section you will be making changes to the repair data file below",
+      ]}
+      CodeImplementation={
+        () => { }
+      }
       navigateNext={() => {}}
     />
   );
