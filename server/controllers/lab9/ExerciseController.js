f7a13a8f09c3cbd192ebe19143bb6e9329ba8f55
AndreasLc1103 <Andreaslc1103@gmail.com>
2023-11-04 19:56:37 -0400

    stubbed in controller

---
+++ server/controllers/lab9/ExerciseController.js
@@ -0,0 +1,33 @@
+/**
+ * getExercise(): is a function responsible for retrieving the
+ * user id from the query params from the route to the endpoint.
+ * this allows for the ability to retrieve the last game state.
+ * @param {Object} req request object containing userId;
+ */
+async function getExercise(req) {
+  try {
+
+  } catch (error) {
+
+  }
+}
+
+/**
+ * postExercise(): is a function that is responsible for sending the
+ * user's state to the database for when they are completed with a section
+ * of the lab's repair. This function is responsible for getting the contents
+ * of the body of the request and pass it off to the Exercise service.
+ * @param {Object} req request object containing user information and payload.
+ */
+async function postExercise(req) {
+  try {
+
+  } catch (error) {
+
+  }
+}
+
+module.exports = {
+  getExercise,
+  postExercise,
+};
--- server/routes/index.js
+++ server/routes/index.js
@@ -33,10 +33,15 @@ const ExerciseControllerLab6 = require('../controllers/lab6/ExerciseController')
 const RepairControllerLab7 = require('../controllers/lab7/RepairController');
 const ExerciseControllerLab7 = require('../controllers/lab7/ExerciseController');
 
-// Lab 8 Controller
+// LAB 8 Controller
 const ExerciseControllerLab8 = require('../controllers/lab8/ExerciseController');
+
+// LAB 9 Controller
+const ExerciseControllerLab9 = require('../controllers/lab9/ExerciseController');
+
 // Lab Controller
 const LabController = require('../controllers/LabController');
+
 
 // Team Members Controller
 const TeamMemberController = require('../controllers/TeamMemberController');
@@ -115,6 +120,13 @@ router.get('/lab8/exercise/:userID', async function(req, res) {
 });
 router.post('/lab8/exercise/submit', async function(req, res) {
   const id = await ExerciseControllerLab8.submitChange(req);
+  res.send(id);
+});
+router.get('/lab9/exercise/:userID', async function(req, res) {
+  res.json(await ExerciseControllerLab9.getRepair(req, true));
+});
+router.post('/lab9/exercise/submit', async function(req, res) {
+  const id = await ExerciseControllerLab9.submitChange(req);
   res.send(id);
 });
 
