f47d5307a0b3c1850ea79e061c98b506dee475a9
AndreasLc1103 <Andreaslc1103@gmail.com>
2023-11-06 15:23:57 -0500

    changed database model to sigular table

--- server/database/models/lab9/Repair.js
+++ server/database/models/lab9/Repair.js
@@ -1,5 +1,9 @@
+/* eslint-disable new-cap */
+const ADDRESS_REPAIR = 'address-repair';
+const DATE_REPAIR = 'date-repair';
+const NAV_REPAIR = 'nav-repair';
 module.exports = (sequelize, DataTypes) => {
-  const DateRepair = sequelize.define('DateRepairLab9',
+  const DateRepair = sequelize.define('RepairLab9',
       {
         repairId: {
           type: DataTypes.INTEGER,
@@ -9,6 +13,9 @@ module.exports = (sequelize, DataTypes) => {
         },
         userid: {
           type: DataTypes.BIGINT,
+        },
+        section: {
+          type: DataTypes.ENUM(ADDRESS_REPAIR, DATE_REPAIR, NAV_REPAIR),
         },
         repair: {
           type: DataTypes.JSON,
@@ -24,7 +31,7 @@ module.exports = (sequelize, DataTypes) => {
         },
       },
       {
-        tableName: 'lab9_date_repair',
+        tableName: 'lab9_repair',
       },
   );
 
