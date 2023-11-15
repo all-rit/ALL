7ec6a1f98db0bcf55c8d305498a57594c7ec0f41
Heather Moses <heather-moses@outlook.com>
2023-11-15 16:08:14 -0500

    prettier

--- client/src/components/exercise/lab9/pages/ExerciseStart.js
+++ client/src/components/exercise/lab9/pages/ExerciseStart.js
@@ -10,12 +10,12 @@ const ExerciseStart = () => {
       <div className="center-div">
         <div className="guidance margin-bottom-2">
           <p className="playthrough__sentence">
-            Welcome to ALL University! ALL University’s primary campus is located
-            in the US, but there are also several satellite campuses around the
-            world, including the United Kingdom, Japan, and Dubai. ALL
-            University’s website was built for the US campus, but
-            they want to expand their website to be accessible to their satellite
-            campuses as well.
+            Welcome to ALL University! ALL University’s primary campus is
+            located in the US, but there are also several satellite campuses
+            around the world, including the United Kingdom, Japan, and Dubai.
+            ALL University’s website was built for the US campus, but they want
+            to expand their website to be accessible to their satellite campuses
+            as well.
           </p>
           <p className="playthrough__sentence">
             In this exercise, you will assist ALL University in localizing their
