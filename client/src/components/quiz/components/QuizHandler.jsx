262e7ac2b3d7442bf557fb858e6e3e960211ecb7
Heather Moses <heather-moses@outlook.com>
2023-11-29 17:02:44 -0500

    Added quiz questions for lab 11

--- client/src/components/quiz/components/QuizHandler.jsx
+++ client/src/components/quiz/components/QuizHandler.jsx
@@ -12,6 +12,7 @@ import QuestionsLab4 from "../api/Lab4/quizQuestions";
 import QuestionsLab5 from "../api/Lab5/quizQuestions";
 import QuestionsLab6 from "../api/Lab6/quizQuestions";
 import QuestionsLab8 from "../api/Lab8/quizQuestions";
+import QuestionsLab11 from "../api/Lab11/quizQuestions";
 import UserLabService from "../../../services/UserLabService";
 import alterationQuizQuestions from "../api/Lab7/alterationQuizQuestions";
 import quizQuestionsLab7 from "../api/Lab7/quizQuestions";
@@ -44,6 +45,8 @@ function assignQuizQuestions(labId, isFinalQuiz) {
       }
     case 8:
       return QuestionsLab8;
+    case 11:
+      return QuestionsLab11;
     default:
       return [
         {
