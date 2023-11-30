207874aea14b34b1a45544cfac391f3c9ba0c1c9
Domenic Mangano <dm9965@rit.edu>
2023-11-30 12:51:11 -0500

    Updated nav repair to reflect data service

--- client/src/components/exercise/lab9/pages/Repairs/NavRepairPage.js
+++ client/src/components/exercise/lab9/pages/Repairs/NavRepairPage.js
@@ -3,25 +3,37 @@ import PropTypes from "prop-types";
 import Repair from "../../../../body/Repair/Repair";
 import { HEADINGS, GAME_STATES, REPAIR } from "../../../../../constants/lab9";
 import NavBarRepair from "../NavBarRepair";
-import useRepairNav from "../../hooks/useRepairNav";
+import useDataService from "../hooks/useDataService";
+import { navigate } from "@reach/router";
+import { NavBarData } from "src/constants/lab9/NavBarData";
+
 /**
  * Nav Repair is a component that is responsible for passing logic into the universal
  * repair page component, what this allows us to do is call the re-useable repair component
  * with custom logic pertaining to that repair section.
- * @param {String} user contains user id for data state and logging user input
+ * @param {Object} user contains user id for data state and logging user input
  * @returns
  */
 
 const NavRepairPage = ({ user }) => {
-  const { data, functions } = useRepairNav(user);
+  const { data, functions } = useDataService(
+    user,
+    GAME_STATES.REPAIR_NAV_BAR,
+    NavBarData.navItems
+  );
   const { exercisePromptsState } = data;
-  const { handleUserInputChange, checkInputValid } = functions;
+  const { handleUserInputChange, checkInputValid, fetchRepair, postRepair } = functions;
+  const handleNav = () => {
+    navigate("/Lab9/Exercise/page");
+  };
   return (
     <Repair
       fileName={"NavBar.js"}
       path={`${REPAIR}/${GAME_STATES.REPAIR_NAV_BAR}`}
       headingText={HEADINGS.REPAIR_NAV_HEADING}
       validateRepair={checkInputValid}
+      fetchRepair={() => fetchRepair()}
+      submitRepair={() => postRepair()}
       repairText={[
         "in this section you will be making changes to the repair data file below",
       ]}
@@ -31,7 +43,7 @@ const NavRepairPage = ({ user }) => {
           userInput={handleUserInputChange}
         />
       }
-      navigateNext={() => {}}
+      navigateNext={() => handleNav()}
     />
   );
 };
