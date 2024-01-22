0839719bb0d1db0c41faad4fc5d32d3b5b547c7c
AndreasLc1103 <Andreaslc1103@gmail.com>
2024-01-22 17:36:22 -0500

    fixed server routing for imagine

--- server/routes/index.js
+++ server/routes/index.js
@@ -180,14 +180,32 @@ router.get('/professors', TeamMemberController.getAllProfessors);
 router.get('/alumni', TeamMemberController.getAllAlumni);
 
 // Imagine
-router.post('/imagine/discomfortCount', ImagineController.discomfortCount);
-router.post('/imagine/experientialMain', ImagineController.experientialMain);
-router.post('/imagine/experientialProtanopia', ImagineController.experientialProtanopia);
-router.post('/imagine/preSurvey', ImagineController.preSurvey);
-router.post('/imagine/postSurvey', ImagineController.postSurvey);
-router.get('/imagine/users', ImagineController.getUsers);
-
-router.post('/imagine/readMoreCount', ImagineController.readMoreCount);
-router.post('/imagine/readMoreTimeElapsed', ImagineController.readMoreTimeElapsed);
-router.post('/imagine/readingSectionPagePosition', ImagineController.readingSectionPagePosition);
+router.post('/imagine/submitStudy', async function(req, res) {
+  const resp = await ImagineController.submitStudy(req);
+  res.send(resp);
+});
+router.post('/imagine/preSurvey', async function(req, res) {
+  const resp = await ImagineController.preSurvey(req);
+  res.send(resp);
+});
+router.post('/imagine/postSurvey', async function(req, res) {
+  const resp = await ImagineController.postSurvey(req);
+  res.send(resp);
+});
+router.post('/imagine/users', async function(req, res) {
+  const resp = await ImagineController.getUsers(req);
+  res.send(resp);
+});
+router.post('/imagine/readMoreCount', async function(req, res) {
+  const resp = await ImagineController.readMoreCount(req);
+  res.send(resp);
+});
+router.post('/imagine/readMoreTimeElapsed', async function(req, res) {
+  const resp = await ImagineController.readMoreTimeElapsed(req);
+  res.send(resp);
+});
+router.post('/imagine/readingSectionPagePosition', async function(req, res) {
+  const resp = await ImagineController.readingSectionPagePosition(req);
+  res.send(resp);
+});
 module.exports = router;
