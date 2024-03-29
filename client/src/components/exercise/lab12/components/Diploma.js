428bd5cf974da188ed3d4078df263a9a8427f80d
Heather Moses <heather-moses@outlook.com>
2024-03-29 13:35:22 -0400

    diploma styled

--- client/src/components/exercise/lab12/components/Diploma.js
+++ client/src/components/exercise/lab12/components/Diploma.js
@@ -2,42 +2,38 @@ import React from "react";
 import BackgroundImage from "../../../../assets/images/lab12/diploma_background.png";
 import SealImage from "../../../../assets/images/lab12/diploma_seal.png";
 import "../../../../assets/stylesheets/main.css";
-
-const fancyText = {
-  fontFamily: "'UnifrakturMaguntia', cursive",
-};
-
-const horizontalBarStyling = {
-  flex: "0 0 20%",
-  height: "2px",
-  backgroundColor: "black",
-};
+import "../../../../assets/stylesheets/components/Diploma.css";
 
 const Diploma = () => {
   return (
     <div
       style={{
         backgroundImage: `url(${BackgroundImage})`,
+        justifyContent: "center",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        padding: "2rem",
       }}
     >
-      <h1 style={fancyText}>ALL University</h1>
-      <p style={fancyText}>
+      <h1 className="fancy-text tw-text-7xl">ALL University</h1>
+      <p className="fancy-text tw-text-3xl">
         Upon the recommendation of the President and Faculty of the
         <span id="college-name">COLLEGE NAME</span> and by the Board of Trustees
-        has conferred upon
+        has conferred upo
       </p>
-      <h2>
+      <h2 className="diploma-name tw-text-5xl">
         <span>FIRST NAME</span> Phillip <span>LAST NAME</span>
       </h2>
-      <p style={fancyText}>for the degree of</p>
-      <h3>
+      <p className="fancy-text tw-text-3xl">for the degree of</p>
+      <h3 className="fancy-text tw-text-3xl">
         <span>DEGREE</span>
       </h3>
-      <p style={fancyText}>
+      <p className="fancy-text tw-text-3xl">
         In testimony, thereof, the Board of Trustees has granted this diploma
         bearing the Seal of the Institution.
       </p>
-      <p style={fancyText}>
+      <p className="fancy-text tw-text-3xl">
         Earned on this <span>DATE</span>
       </p>
       <div
@@ -46,11 +42,12 @@ const Diploma = () => {
           flexDirection: "row",
           justifyContent: "space-evenly",
           alignItems: "center",
+          width: "100%",
         }}
       >
-        <hr style={horizontalBarStyling} />
+        <hr className="horizontal-bar-styling" />
         <img src={SealImage} />
-        <hr style={horizontalBarStyling} />
+        <hr className="horizontal-bar-styling" />
       </div>
     </div>
   );
