03157302c6829dabdfe6360566bef987f823f72f
Owen Luts <owenlutsdev@gmail.com>
2024-04-02 15:07:30 -0400

    better font and tailwinded

--- client/src/components/exercise/lab12/components/Diploma.js
+++ client/src/components/exercise/lab12/components/Diploma.js
@@ -1,63 +1,48 @@
 import React from "react";
-import BackgroundImage from "../../../../assets/images/lab12/diploma_background.png";
 import SealImage from "../../../../assets/images/lab12/diploma_seal.png";
-import "../../../../assets/stylesheets/main.css";
 import "../../../../assets/stylesheets/components/Diploma.css";
 
-const smallLine = {
-  lineHeight: "normal",
-};
-const largeLine = {
-  lineHeight: "1.5",
+// TEMPORARY: TODO make State-ified
+const info = {
+  collegeName: "ALL University",
+  firstName: "John",
+  middleName: "Elden",
+  lastName: "Ring",
+  degree: "Software Engineering",
+  date: "September 21st",
 };
 
 const Diploma = () => {
   return (
-    <div
-      style={{
-        backgroundImage: `url(${BackgroundImage})`,
-        justifyContent: "center",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        padding: "1rem",
-      }}
-    >
-      <h1 className="fancy-text tw-text-7xl" style={largeLine}>
+    <div className="tw-flex tw-bg-diploma-background tw-flex-col tw-justify-center tw-items-center tw-p-4">
+      <h1 className="tw-font-diploma tw-text-7xl tw-leading-normal">
         ALL University
       </h1>
-      <p className="fancy-text tw-text-3xl" style={smallLine}>
-        Upon the recommendation of the President and Faculty of the
-        <span id="college-name">&nbsp;(College Name)&nbsp;</span>
-        and by the Board of Trustees has conferred upon
+      <p className="fancy-text tw-text-3xl tw-leading-normal">
+        Upon the recommendation of the President and Faculty of the{" "}
+        <span className="tw-font-diploma">{info.collegeName}</span> and by the
+        Board of Trustees has conferred upon
       </p>
-      <h2 className="diploma-name tw-text-5xl" style={largeLine}>
-        <span>(First Name)</span> Phillip <span>(Last Name)</span>
+      <h2 className="tw-font-bold tw-text-5xl tw-leading-normal">
+        {info.firstName + " " + info.middleName + " " + info.lastName}
       </h2>
-      <p className="fancy-text tw-text-3xl">for the degree of</p>
-      <h3 className="fancy-text tw-text-5xl" style={largeLine}>
-        <span>(Degree)</span>
+      <p className="fancy-text tw-text-3xl tw-leading-normal">
+        for the degree of
+      </p>
+      <h3 className="tw-font-diploma tw-text-5xl tw-leading-normal">
+        {info.degree}
       </h3>
-      <p className="fancy-text tw-text-3xl" style={smallLine}>
+      <p className="fancy-text tw-text-3xl tw-leading-none">
         In testimony, thereof, the Board of Trustees has granted this diploma
         bearing the Seal of the Institution.
       </p>
-      <p className="fancy-text tw-text-3xl" style={smallLine}>
-        Earned on this <span>(Date)</span>
+      <p className="fancy-text tw-text-3xl tw-pb-8 tw-leading-normal">
+        Earned on this <span className="tw-font-diploma">{info.date}</span>
       </p>
-      <br></br>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-evenly",
-          alignItems: "center",
-          width: "100%",
-        }}
-      >
-        <hr className="horizontal-bar-styling" />
+      <div className="tw-flex tw-row tw-justify-evenly tw-items-center tw-w-full">
+        <hr className="tw-flex-none tw-flex-grow-0 tw-w-1/5 tw-h-2" />
         <img src={SealImage} />
-        <hr className="horizontal-bar-styling" />
+        <hr className="tw-flex-none tw-flex-grow-0 tw-w-1/5 tw-h-2" />
       </div>
     </div>
   );
