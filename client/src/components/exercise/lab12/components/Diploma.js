b5ccf57c6c25e2e12e21fd4d2bac22f675fd067f
Owen Luts <owenlutsdev@gmail.com>
2024-04-02 12:27:33 -0400

    layout looks nice, totally unoptimized

--- client/src/components/exercise/lab12/components/Diploma.js
+++ client/src/components/exercise/lab12/components/Diploma.js
@@ -4,6 +4,13 @@ import SealImage from "../../../../assets/images/lab12/diploma_seal.png";
 import "../../../../assets/stylesheets/main.css";
 import "../../../../assets/stylesheets/components/Diploma.css";
 
+const smallLine = {
+  lineHeight: "normal",
+};
+const largeLine = {
+  lineHeight: "1.5",
+};
+
 const Diploma = () => {
   return (
     <div
@@ -16,26 +23,29 @@ const Diploma = () => {
         padding: "1rem",
       }}
     >
-      <h1 className="fancy-text tw-text-7xl">ALL University</h1>
-      <p className="fancy-text tw-text-3xl">
+      <h1 className="fancy-text tw-text-7xl" style={largeLine}>
+        ALL University
+      </h1>
+      <p className="fancy-text tw-text-3xl" style={smallLine}>
         Upon the recommendation of the President and Faculty of the
-        <span id="college-name">COLLEGE NAME</span>
+        <span id="college-name">&nbsp;(College Name)&nbsp;</span>
         and by the Board of Trustees has conferred upon
       </p>
-      <h2 className="diploma-name tw-text-5xl">
-        <span>FIRST NAME</span> Phillip <span>LAST NAME</span>
+      <h2 className="diploma-name tw-text-5xl" style={largeLine}>
+        <span>(First Name)</span> Phillip <span>(Last Name)</span>
       </h2>
       <p className="fancy-text tw-text-3xl">for the degree of</p>
-      <h3 className="fancy-text tw-text-3xl">
-        <span>DEGREE</span>
+      <h3 className="fancy-text tw-text-5xl" style={largeLine}>
+        <span>(Degree)</span>
       </h3>
-      <p className="fancy-text tw-text-3xl">
+      <p className="fancy-text tw-text-3xl" style={smallLine}>
         In testimony, thereof, the Board of Trustees has granted this diploma
         bearing the Seal of the Institution.
       </p>
-      <p className="fancy-text tw-text-3xl">
-        Earned on this <span>DATE</span>
+      <p className="fancy-text tw-text-3xl" style={smallLine}>
+        Earned on this <span>(Date)</span>
       </p>
+      <br></br>
       <div
         style={{
           display: "flex",
