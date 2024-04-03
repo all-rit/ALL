70b67927edda3b264972a4f842297a87c974045e
AndreasLc1103 <Andreaslc1103@gmail.com>
2024-04-02 22:58:38 -0400

    fixed hanging issue

--- server/test/services/LabServiceTest.test.js
+++ server/test/services/LabServiceTest.test.js
@@ -1,6 +1,9 @@
 const labService = require('../../services/LabService');
 const db = require('../../database');
 
+afterAll(() => {
+  db.sequelize.close();
+});
 
 test('test Retrieving all Labs ', async () => {
   const getLabs = (await labService.getAllLabs());
