109f4dcc37682bce3f7344936ea13e5f689e14ba
AndreasLc1103 <Andreaslc1103@gmail.com>
2024-04-04 12:04:25 -0400

    got the about controller test out

--- server/test/controllers/LabController.test.js
+++ server/test/controllers/LabController.test.js
@@ -1,11 +1,22 @@
 const LabController = require('../../controllers/LabController');
 const ControllerTestUtil = require('../controllers/ControllerTestUtil');
-
-describe( '', () => {
+const LabService = require('../../services/LabService');
+const labCount = 11;
+describe( 'Test LabController for successful Payloads', () => {
   test('Test get all labs', async () => {
     const req = ControllerTestUtil.formatRequest({});
     const res = ControllerTestUtil.formatResponse();
-    await LabController.getAllLabsController(req, res);
-    // expect().toBe(200);
+    const response = await LabController.getAllLabsController(req, res);
+    expect(response).toBeDefined();
+  });
+
+  test('test get all about sections info', async () => {
+    for (let i = 1; i <= labCount; i++) {
+      const req = ControllerTestUtil.formatRequest({params: {labID: i}});
+      const res = ControllerTestUtil.formatResponse();
+      const response = await LabController.getLabAboutController(req, res);
+      const expected = await LabService.getLabAbout(i);
+      expect(response).toBe(JSON.stringify([expected]));
+    }
   });
 });
