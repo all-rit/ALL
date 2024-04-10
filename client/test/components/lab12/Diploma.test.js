277269ba3286beb15882ccb5a021f3c7d5283b76
Heather Moses <heather-moses@outlook.com>
2024-04-10 17:27:02 -0400

    trying things

--- client/test/components/lab12/Diploma.test.js
+++ client/test/components/lab12/Diploma.test.js
@@ -1,3 +1,11 @@
-test('simple test', () => {
-    expect(1).toBe(1);
-})
+// const Diploma = require("../../../src/components/exercise/lab12/components/Diploma");
+
+test("simple test", () => {
+  expect(1).toBe(1);
+});
+
+// test('some text shows up', () => {
+//     const diploma = <Diploma/>;
+//     const header = <h1>ALL University</h1>;
+//     expect(diploma.contains(header)).toEqual(true);
+// })
