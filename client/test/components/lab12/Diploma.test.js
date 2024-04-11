e74fcd5e69ee28e17ce06f8058826d303f3648c2
AndreasLc1103 <Andreaslc1103@gmail.com>
2024-04-11 13:53:17 -0400

    fixed it to help

--- client/test/components/lab12/Diploma.test.js
+++ client/test/components/lab12/Diploma.test.js
@@ -1,11 +1,24 @@
-// const Diploma = require("../../../src/components/exercise/lab12/components/Diploma");
+import React from "react";
+import renderer from "react-test-renderer";
+import Diploma from "../../../src/components/exercise/lab12/components/Diploma";
+// '../../../../assets/images/lab12/diploma_seal.png'
 
 test("simple test", () => {
   expect(1).toBe(1);
 });
 
-// test('some text shows up', () => {
-//     const diploma = <Diploma/>;
-//     const header = <h1>ALL University</h1>;
-//     expect(diploma.contains(header)).toEqual(true);
-// })
+describe("first", () => {
+  test("some text shows up", () => {
+    const diploma = <Diploma />;
+    const component = renderer.create(diploma).toJSON();
+    const header = renderer
+      .create(
+        <h1 className="tw-font-diploma tw-text-7xl tw-leading-none tw-pb-8 tw-pt-4">
+          ALL University
+        </h1>,
+      )
+      .toJSON();
+    console.log(header);
+    expect(JSON.stringify(component.children[0])).toBe(JSON.stringify(header));
+  });
+});
