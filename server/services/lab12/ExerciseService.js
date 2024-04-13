05aa67c44f6ab94e03078543f4da83334a8455c7
Domenic Mangano <domenicmangano@Domenics-MacBook-Pro.local>
2024-04-12 21:29:53 -0400

    Lab 12 Exercise and Repair Services Created

---
+++ server/services/lab12/ExerciseService.js
@@ -0,0 +1,76 @@
+const db = require('../../database');
+/**
+ * getExercise(): is a function that is responsible for retrieving
+ * the last played exercise by a particular user. this function is
+ * responsible for querying the database and retrieving the correct
+ * information from the database.
+ * @param {Object} data Contains information about the user to search
+ * the database.
+ */
+async function getExercise(data) {
+  try {
+    const exerciseResponse = await db.ExerciseLab12.findOne(
+        {
+          order: [['attemptCount', 'DESC']],
+          where: {
+            userid: data,
+          },
+          raw: true,
+        },
+    );
+    return exerciseResponse;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+/**
+ * postExercise(): is a function that is responsible for storing
+ * changes to the database. This allows the user's information to
+ * be properly stored and allow for the ability to be retrieved later
+ * when needed.
+ * @param {Object} data Contains information that is intended to be stored
+ * in the database,
+ */
+async function postExercise(data) {
+  try {
+    const {userId, isRepairComplete,
+      isExerciseComplete,
+      hasViewed} = data;
+    const getExerciseResponse = await getExercise(userId);
+    const currentTime = new Date().toISOString();
+    const newExercise = {
+      userid: userId,
+      isRepairComplete: false,
+      isExerciseComplete: false,
+      attemptTime: currentTime,
+      attemptCount: 1,
+      hasViewed: false,
+    };
+    if (!getExerciseResponse) {
+      // adds in new entry
+      return await db.ExerciseLab12.create(newExercise).id;
+    } else {
+      const convert = parseInt(getExerciseResponse.attemptCount);
+      const newVal = convert + 1;
+      const updatedExercise = {
+        userid: userId,
+        isRepairComplete: isRepairComplete,
+        isExerciseComplete: isExerciseComplete,
+        attemptTime: currentTime,
+        attemptCount: newVal,
+        hasViewed: hasViewed,
+      };
+        // reset state
+      return await db.ExerciseLab12.create(updatedExercise).id;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+
+module.exports = {
+  getExercise,
+  postExercise,
+};
