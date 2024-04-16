dd2415de1666293685e1f247a0b675b23f75cf69
Owen Luts <owenlutsdev@gmail.com>
2024-04-16 10:20:07 -0700

    reworded exercise instructions

--- client/src/components/exercise/lab12/pages/Explanations/ExerciseIntro.js
+++ client/src/components/exercise/lab12/pages/Explanations/ExerciseIntro.js
@@ -21,14 +21,14 @@ const ExerciseIntro = () => {
     <div className="center-div">
       <div className="guidance margin-bottom-2">
         <p className="playthrough__sentence">
-          In the following exercise you are applying for graduation at ALL
-          University. Please enter your personal information in a small form.
-          This information will appear on your diploma and we will see how to
-          improve the questionnaire. Click “Start” to begin!
+          You just graduated from ALL University! Congratulations on completing your degree! Now that you have finished, you will apply for graduation and transition from being a student to being an alum. 
+        </p>
+        <p className="playthrough__sentence">
+          In this exercise, you will enter your personal information in a small form. This information will appear on your diploma and we will see how to improve the questionnaire. Click the “Start” button to begin this exercise!
         </p>
       </div>
       <div className="playthrough__sentence">
-        Click the &quot;Continue to Exercise&quot; button.
+        Click the &quot;Start&quot; button to begin the exercise!
       </div>
       <div className="tw-flex tw-justify-evenly">
         <button
@@ -36,7 +36,7 @@ const ExerciseIntro = () => {
           onClick={handleContinue}
           key="start"
         >
-          Continue to Exercise
+          Fart
         </button>
       </div>
     </div>
