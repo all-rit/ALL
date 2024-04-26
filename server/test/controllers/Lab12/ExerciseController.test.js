9c685700df21465bee85b5145f5dff7426d3d37c
Domenic Mangano <domenicmangano@Domenics-MacBook-Pro.local>
2024-04-26 10:18:38 -0400

    Cleaned up ControllerService Test lint and failing getExercise

--- server/test/controllers/Lab12/ExerciseController.test.js
+++ server/test/controllers/Lab12/ExerciseController.test.js
@@ -1,6 +1,7 @@
 // eslint-disable-next-line max-len
 const ControllerTestUtil = require('../ControllerTestUtil');
-const ExerciseController = require('../../../controllers/lab12/ExerciseController');
+const ExerciseController = require(
+    '../../../controllers/lab12/ExerciseController');
 const ExerciseService = require('../../../services/lab12/ExerciseService');
 
 // eslint-disable-next-line max-len
@@ -14,8 +15,8 @@ describe('Test successful payloads in Lab 12 ExerciseController functions', () =
     expect(JSON.parse(response)).toStrictEqual(([
       {
         ...expected,
-        attemptTime: new Date(expected.attemptTime).toJSON()
-      }
+        attemptTime: new Date(expected.attemptTime).toJSON(),
+      },
     ]));
   });
 
@@ -33,17 +34,18 @@ describe('Test successful payloads in Lab 12 ExerciseController functions', () =
     const res = ControllerTestUtil.formatResponse();
     const response = await ExerciseController.postExercise(req, res);
     const expected = await ExerciseService.getExercise(100);
-    expect(JSON.parse(response)[0].attemptCount).toStrictEqual((expected.attemptCount + 1));
+    expect(JSON.parse(
+        response)[0].attemptCount).toStrictEqual(
+        (expected.attemptCount + 1));
   });
 });
 
 describe('Test failed payloads in Lab 12 ExerciseController functions', () => {
   test('Test getExercise function fails', async () => {
-    const req = ControllerTestUtil.formatRequest({userID: "101"});
+    const req = ControllerTestUtil.formatRequest({params: {userID: 101}});
     const res = ControllerTestUtil.formatResponse();
     const response = await ExerciseController.getExercise(req, res);
-    console.log(response);
-    expect(response).toBeUndefined();
+    expect(JSON.parse(response)[0]).toBeNull();
   });
 
   test('Test postExercise function fails', async () => {
