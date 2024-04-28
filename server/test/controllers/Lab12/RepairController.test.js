0ae6d47de69104e5f72280f3d7d1e077d4dea1d9
Domenic Mangano <domenicmangano@Domenics-MacBook-Pro.local>
2024-04-27 20:34:55 -0400

    Functioning Repair Service Successful payload tests

--- server/test/controllers/Lab12/RepairController.test.js
+++ server/test/controllers/Lab12/RepairController.test.js
@@ -6,11 +6,28 @@ const RepairService = require('../../../services/lab12/RepairService');
 // eslint-disable-next-line max-len
 describe('Test successful payloads in Lab 12 RepairController functions', () => {
   test('Test getRepair function', async () => {
-    // const req = ControllerTestUtil.formatRequest({params: {userID: "100", section: "FormRepair"}});
-    // const response = await RepairController.getRepair(req);
-    // const expected = await RepairService.getRepair(100);
-    // expect(response).toStrictEqual(expected);
-    // expect(response).toBeNull();
+    const req = ControllerTestUtil.formatRequest(
+        {params: {userID: 100, section: 'FormRepair'}});
+    const response = await RepairController.getRepair(req);
+    const expected = await RepairService.getRepair(100, 'FormRepair');
+    expect(response).toStrictEqual(expected);
+    expect(response).toBeNull();
+    expect(null).toBeNull();
+  });
+
+  test('Test submitRepair function', async () => {
+    expect(null).toBeNull();
+  });
+});
+
+describe('Test failed payloads in Lab 12 RepairController functions', () => {
+  test('Test getRepair function', async () => {
+    const req = ControllerTestUtil.formatRequest(
+        {params: {userID: 101, section: 'FormRepair'}});
+    const response = await RepairController.getRepair(req);
+    const expected = await RepairService.getRepair(101, 'FormRepair');
+    expect(response).toStrictEqual(expected);
+    expect(response).toBeNull();
     expect(null).toBeNull();
   });
 
