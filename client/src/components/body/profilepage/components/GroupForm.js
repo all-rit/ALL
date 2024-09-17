031ad493546eb9e2e449d73cd40677afeaec5dfc
Domenic Mangano <domenicmangano@domenics-mbp.wireless.rit.edu>
2024-09-17 11:34:11 -0400

    Removed console log

--- client/src/components/body/profilepage/components/GroupForm.js
+++ client/src/components/body/profilepage/components/GroupForm.js
@@ -43,7 +43,6 @@ const GroupForm = (props) => {
   }, [assignedLabs]);
 
   useEffect(() => {
-    console.log(crypto.randomUUID().toUpperCase().slice(1, 7));
     console.log(checkedLabs);
   }, [checkedLabs]);
 
