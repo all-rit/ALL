65833ebb6a164a77c68499279a8c188b5e6c9a7e
komeally <kasim.omeally@gmail.com>
2024-10-04 15:00:01 -0400

    Rendering component on main

--- client/src/components/body/landingpage/DevPartners.js
+++ client/src/components/body/landingpage/DevPartners.js
@@ -3,21 +3,20 @@ import React from "react";
 
 const DevPartners = () => {
   return (
-    <div className="tw-bg-labBlue tw-flex tw-flex-col tw-justify-center tw-items-center tw-py-8 tw-px-16">
-      <div>
-        <h2 className="tw-text-white tw-text-3xl tw-font-bold tw-mb-4">
+    <div className="tw-bg-labBlue tw-py-8 tw-px-16">
+      <div className="dev-info tw-bg-bgwhite">
+        <h2 className="tw-text-black tw-text-3xl tw-font-bold tw-mb-4">
           Development Partners
         </h2>
-        <p className="tw-text-white tw-text-lg tw-mb-4">
+        <p className="tw-text-black tw-text-lg tw-mb-4">
           Aenean a venenatis metus, ut varius quam. Quisque lobortis odio
           libero, quis blandit nibh feugiat malesuada. Interdum et malesuada
           fames ac ante ipsum primis in faucibus.
         </p>
-        <button className="tw-text-black tw-font-medium tw-rounded-full tw-px-4 tw-py-2 tw-border-2 tw-border-b-labYellow tw-border-r-labBlue">
+        <button className="tw-text-black tw-font-medium tw-py-2 tw-border-b-labYellow tw-border-t-labBlue">
           Learn More
         </button>
       </div>
-      <div className="tw-grid tw-grid-cols-1 md:tw-grid-cols-3 tw-gap-8"></div>
     </div>
   );
 };
