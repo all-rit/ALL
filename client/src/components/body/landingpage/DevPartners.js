94dc6680f466c5e16b7c7cb2b7fea32f64129a57
komeally <kasim.omeally@gmail.com>
2024-10-07 14:40:20 -0400

    Styled development partners learn more section

--- client/src/components/body/landingpage/DevPartners.js
+++ client/src/components/body/landingpage/DevPartners.js
@@ -3,19 +3,23 @@ import React from "react";
 
 const DevPartners = () => {
   return (
-    <div className="tw-bg-labBlue tw-py-8 tw-px-16">
-      <div className="dev-info tw-bg-bgwhite">
-        <h2 className="tw-text-black tw-text-3xl tw-font-bold tw-mb-4">
-          Development Partners
-        </h2>
-        <p className="tw-text-black tw-text-lg tw-mb-4">
-          Aenean a venenatis metus, ut varius quam. Quisque lobortis odio
-          libero, quis blandit nibh feugiat malesuada. Interdum et malesuada
-          fames ac ante ipsum primis in faucibus.
-        </p>
-        <button className="tw-text-black tw-font-medium tw-py-2 tw-border-b-labYellow tw-border-t-labBlue">
-          Learn More
-        </button>
+    <div className="tw-bg-labBlue tw-py-28 tw-pl-36 ">
+      <div className="tw-bg-labYellow tw-rounded-bl-md  tw-pb-3 tw-pl-3">
+        <div className="tw-bg-bgwhite tw-text-left tw-px-16 tw-py-6">
+          <h2 className="tw-text-black tw-text-3xl tw-font-bold tw-py-6 tw-text-left">
+            Development Partners
+          </h2>
+          <div className="">
+            <p className="tw-text-black tw-text-base tw-mb-4 tw-max-w-sm">
+              Aenean a venenatis metus, ut varius quam. Quisque lobortis odio
+              libero, quis blandit nibh feugiat malesuada. Interdum et malesuada
+              fames ac ante ipsum primis in faucibus.
+            </p>
+            <button className="btn tw-w-36 tw-border-b-labYellow tw-border-l-labYellow tw-border-t-labBlue tw-border-r-labBlue tw-border-4">
+              Learn More
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
