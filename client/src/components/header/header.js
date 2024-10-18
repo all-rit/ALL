325cf0b737b968dcbb8d1a98337b586c4989179c
Owen Luts <owenlutsdev@gmail.com>
2024-10-18 13:42:03 -0400

    switches between horizontal and collapsable view on window resize, need to add vertical window though

--- client/src/components/header/header.js
+++ client/src/components/header/header.js
@@ -2,7 +2,7 @@
 /* eslint-disable max-len */
 /* eslint-disable react/prop-types */
 /* eslint-disable camelcase */
-import React, { useEffect, useState } from "react";
+import React, { useLayoutEffect, useState } from "react";
 import Logo from "../../assets/images/logos/ALL_Logo.svg"; 
 import { connect } from "react-redux";
 import {
@@ -25,17 +25,44 @@ const mapStateToProps = (state) => {
   };
 };
 
-
 const mapDispatchToProps = (dispatch) => {
   return {
     actions: bindActionCreators(mainActions, dispatch),
   };
 };
 
+function useWindowSize() {
+	const [size, setSize] = useState([0, 0]);
+	useLayoutEffect(() => {
+	  function updateSize() {
+		setSize([window.innerWidth, window.innerHeight]);
+	  }
+	  window.addEventListener('resize', updateSize);
+	  updateSize();
+	  return () => window.removeEventListener('resize', updateSize);
+	}, []);
+	return size;
+}
+
 const Header = (props) => {
 	const { state } = useMainStateContext()
+	const [isSmallWindow, setisSmallWindow] = useState(false)
+	const [navbarOpen, setNavbarOpen] = useState(false)
 
-	// user is logged in if their name isn't null, kinda scuffed but lmk if there's a better idea
+	const toggleNavbar = () => setNavbarOpen(!navbarOpen)
+	const windowSize = useWindowSize()
+
+	if (isSmallWindow == false && windowSize[0] > 840) {
+		setisSmallWindow(true)
+	}
+	else if (isSmallWindow == true && windowSize[0] <= 840) {
+		setisSmallWindow(false)
+	}
+
+
+
+	// user is logged in if their profile image isn't null, kinda scuffed but lmk if there's a simpler way
+	// (state.main.user is never null)
 	const loggedIn = state.main.user?.userpfp !== null
 	
 	return (
@@ -45,14 +72,9 @@ const Header = (props) => {
 		expand="lg"
 		className="tw-pt-3 poppins tw-font-bold mb-0"
 	>
-		<div className="tw-flex tw-flex-col tw-gap-4 tw-z-30 tw-text-2xl tw-bg-white tw-fixed tw-top-0 p-3 tw-left-0 tw-right-0 w-100 d-flex flex-row">
+		<div className="tw-lg:flex tw-flex-col tw-gap-2 tw-z-30 tw-text-2xl tw-bg-white tw-fixed tw-top-0 p-2 tw-left-0 tw-right-0 d-flex">
 			<div className="tw-flex tw-flex-row tw-gap-4">
-				<a
-					href="# "
-					onClick={() =>
-						console.log("Click Logo")
-					}
-				>
+				<a href="# ">
 					<img
 						className="tw-cursor-pointer tw-h-16"
 						src={Logo}
@@ -61,15 +83,20 @@ const Header = (props) => {
 				</a>
 
 				{/* TODO:
-						- Replace temporary href anchor names (i.e. #about)
-						- Add new logo + styling
-						- hamburger menu for mobile devices
-						- Site Accessibility Settings
+					- Replace temporary href anchor names (i.e. #about)
+					- hamburger menu for mobile devices
+					- Site Accessibility Settings
 				*/}
-				<div className='tw-flex tw-flex-grow tw-justify-end'>
-					<Nav className='tw-flex tw-flex-grow tw-justify-end tw-flex-row tw-items-center tw-border-solid tw-border-t-0 tw-border-r-0 tw-border-8 tw-rounded-bl-md tw-border-l-labYellow tw-border-b-labYellow' navbar>				
-						
-						{/* TODO -- REMOVE THIS NAVITEM (only here for testing) */}
+				
+
+				
+				<Collapse 
+					className="tw-flex tw-flex-grow tw-justify-end"
+					isOpen = {navbarOpen}
+				>
+					<NavbarToggler className={`${isSmallWindow ? "tw-hidden" : "tw-flex tw-flex-grow tw-justify-end tw-flex-row tw-items-center tw-border-solid tw-border-t-0 tw-border-r-0 tw-border-8 tw-rounded-bl-md tw-border-l-labYellow tw-border-b-labYellow"}`} onClick={toggleNavbar} />
+					<Nav className={`${!isSmallWindow ? "tw-hidden" : "tw-flex tw-flex-grow tw-justify-end tw-flex-row tw-items-center tw-border-solid tw-border-t-0 tw-border-r-0 tw-border-8 tw-rounded-bl-md tw-border-l-labYellow tw-border-b-labYellow"}`} navbar>				
+						{/* TODO -- REMOVE THIS NAVITEM (logs out user, only here for testing) */}
 						{/* <NavItem>
 							<a href={`${process.env.REACT_APP_SERVER_URL}/logout`}> LOG OUT </a>
 						</NavItem> */}
@@ -97,11 +124,10 @@ const Header = (props) => {
 						{
 							!loggedIn && (
 								<NavItem className="px-4">
-									{/* USER NOT LOGGED IN */}
+									{/* USER NOT LOGGED IN, renders sign in button*/}
 									<div className="tw-flex tw-items-center tw-justify-center tw-p-0" href="#sign-in">
 										{/* OPEN LOG IN MODAL HERE */}
-
-										{/* TODO -- TRANSFER THIS INTO SIGN IN MODAL */}
+										{/* REMOVE BELOW */}
 										<a
 											href="# "
 											className='tw-no-underline'
@@ -117,12 +143,14 @@ const Header = (props) => {
 											<div className="google__button" />
 											<p className='tw-text-base tw-text-labBlue tw-font-bold'>Sign In</p>
 										</a>
+										{/* REMOVE ABOVE */}
 									</div>
 								</NavItem>
 							)
 						}				
 					</Nav>
-				</div>
+				</Collapse>
+				{/* USER IS LOGGED IN, renders google pfp */}
 				{
 					loggedIn && (
 						<div className="tw-aspect-square tw-flex tw-items-center tw-justify-center tw-border-solid tw-border-labBlue tw-border-4 tw-rounded-full tw-overflow-clip tw-max-h-16">
@@ -131,7 +159,9 @@ const Header = (props) => {
 					)
 				}
 			</div>
-			<p className="tw-flex tw-justify-end tw-text-labBlue">Site Accessibility Settings</p>
+			<a className="tw-flex tw-no-underline tw-justify-end tw-text-labBlue tw-cursor-pointer">
+				<p>Site Accessibility Settings</p>
+			</a>
 		</div>
 	</Navbar>
 	
