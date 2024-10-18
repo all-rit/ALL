9f5b9e15274515f4b3e9b6fa28811d31945fdf64
Owen Luts <owenlutsdev@gmail.com>
2024-10-18 16:48:15 -0400

    navbar complete!!

--- client/src/components/header/header.js
+++ client/src/components/header/header.js
@@ -17,7 +17,6 @@ import { bindActionCreators } from "redux";
 import { actions as mainActions } from "../../reducers/MainReducer";
 
 import useMainStateContext from "src/reducers/MainContext";
-import API from "src/services/API";
 
 const mapStateToProps = (state) => {
   return {
@@ -71,8 +70,8 @@ const Header = (props) => {
 		expand="lg"
 		className="tw-pt-3 poppins tw-font-bold mb-0"
 	>
-		<div className="tw-lg:flex tw-flex-col tw-gap-2 tw-z-30 tw-text-2xl tw-bg-white tw-fixed tw-top-0 p-2 tw-left-0 tw-right-0 d-flex">
-			<div className="tw-flex tw-flex-row tw-gap-4">
+		<div className="tw-flex tw-flex-col tw-gap-2 tw-z-30 tw-text-2xl tw-bg-white tw-fixed tw-top-0 p-2 tw-left-0 tw-right-0 d-flex">
+			<div className={`${isSmallWindow ? "tw-flex tw-flex-row tw-justify-between" : "tw-flex tw-flex-row tw-gap-4"}`}>
 				<a href="# ">
 					<img
 						className="tw-cursor-pointer tw-h-16"
@@ -81,90 +80,82 @@ const Header = (props) => {
 					/>
 				</a>
 
-				{/* TODO:
-					- Replace temporary href anchor names (i.e. #about)
-					- hamburger menu for mobile devices
-					- Site Accessibility Settings
-				*/}
-				
-
 				<NavbarToggler 
-						className={`${isSmallWindow ? "" : "tw-hidden"}`} 
-						onClick={toggleNavbar}
+					className={`${isSmallWindow ? "tw-absolute tw-right-0 tw-top-0 tw-m-4 tw-z-20" : "tw-hidden"}`} 
+					onClick={toggleNavbar}
 				/>
 				<Collapse 
-					className={`${isSmallWindow ? "tw-flex flex-col tw-flex-grow tw-justify-end tw-items-center tw-border-solid tw-border-t-0 tw-border-r-0 tw-border-8 tw-rounded-bl-md tw-border-l-labYellow tw-border-b-labYellow" : "tw-flex tw-flex-grow tw-justify-end"}`}
+					className={`${isSmallWindow ? "tw-items-center tw-border-solid tw-border-t-0 tw-border-r-0 tw-border-8 tw-rounded-bl-md tw-border-l-labYellow tw-border-b-labYellow" : "tw-flex tw-flex-grow tw-justify-end"}`}
 					isOpen = {navbarOpen}
-					
+
 				>
-					<Nav className={`${isSmallWindow ? "tw-flex tw-flex-col" : "tw-flex tw-flex-grow tw-justify-end tw-flex-row tw-items-center tw-border-solid tw-border-t-0 tw-border-r-0 tw-border-8 tw-rounded-bl-md tw-border-l-labYellow tw-border-b-labYellow"}`}>				
+					<Nav className={`${isSmallWindow ? "tw-relative tw-flex-col" : "tw-flex tw-flex-grow tw-justify-end tw-flex-row tw-items-center tw-border-solid tw-border-t-0 tw-border-r-0 tw-border-8 tw-rounded-bl-md tw-border-l-labYellow tw-border-b-labYellow"}`}>				
 						{/* TODO -- REMOVE THIS NAVITEM (logs out user, only here for testing) */}
 						{/* <NavItem>
 							<a href={`${process.env.REACT_APP_SERVER_URL}/logout`}> LOG OUT </a>
 						</NavItem> */}
-						
-						<NavItem className="px-4 tw-border-solid tw-border-labBlue tw-border-t-0 tw-border-l-0 tw-border-b-0 tw-border-r-2">
+
+						{/* TODO -- REMOVE THIS */}
+						{/* <NavItem>
+							<a
+								href="# "
+								className='tw-no-underline'
+								onClick={() =>
+									API.postWithBody(process.env.REACT_APP_SERVER_URL + "/url", {
+										url: window.location,
+									}).then(() => {
+										window.location.href =
+										process.env.REACT_APP_SERVER_URL + "/auth/google";
+									})
+								}
+							>
+								LOG IN
+								
+							</a>
+						</NavItem> */}
+						<NavItem className={`${"px-4"} ${!isSmallWindow && "tw-border-labBlue tw-border-t-0 tw-border-l-0 tw-border-b-0 tw-border-r-2 tw-border-solid"}`}>
 							<NavLink className="tw-flex tw-items-center tw-justify-center tw-p-0" href="#">
 								<p className='tw-text-base tw-text-labBlue tw-font-bold'>Home</p>
 							</NavLink>
 						</NavItem>
-						<NavItem className="px-4 tw-border-solid tw-border-labBlue tw-border-t-0 tw-border-l-0 tw-border-b-0 tw-border-r-2">
+						<NavItem className={`${"px-4"} ${!isSmallWindow && "tw-border-labBlue tw-border-t-0 tw-border-l-0 tw-border-b-0 tw-border-r-2 tw-border-solid"}`}>
 							<NavLink className="tw-flex tw-items-center tw-justify-center tw-p-0" href="#labs">
 								<p className='tw-text-base tw-text-labBlue tw-font-bold'>Labs</p>
 							</NavLink>
 						</NavItem>
-						<NavItem className="px-4 tw-border-solid tw-border-labBlue tw-border-t-0 tw-border-l-0 tw-border-b-0 tw-border-r-2">
+						<NavItem className={`${"px-4"} ${!isSmallWindow && "tw-border-labBlue tw-border-t-0 tw-border-l-0 tw-border-b-0 tw-border-r-2 tw-border-solid"}`}>
 							<NavLink className="tw-flex tw-items-center tw-justify-center tw-p-0" href="#about">
 								<p className='tw-text-base tw-text-labBlue tw-font-bold'>About Us</p>
 							</NavLink>
 						</NavItem>
-						<NavItem className="px-4 tw-border-solid tw-border-labBlue tw-border-t-0 tw-border-l-0 tw-border-b-0 tw-border-r-2">
+						<NavItem className={`${"px-4"} ${!isSmallWindow && "tw-border-labBlue tw-border-t-0 tw-border-l-0 tw-border-b-0 tw-border-r-2 tw-border-solid"}`}>
 							<NavLink className="tw-flex tw-items-center tw-justify-center tw-p-0" href="#resources">
 								<p className='tw-text-base tw-text-labBlue tw-font-bold'>Educator Resources</p>
 							</NavLink>
 						</NavItem>
-						{
-							!loggedIn && (
-								<NavItem className="px-4">
-									{/* USER NOT LOGGED IN, renders sign in button*/}
-									<div className="tw-flex tw-items-center tw-justify-center tw-p-0" href="#sign-in">
-										{/* OPEN LOG IN MODAL HERE */}
-										{/* REMOVE BELOW */}
-										<a
-											href="# "
-											className='tw-no-underline'
-											onClick={() =>
-												API.postWithBody(process.env.REACT_APP_SERVER_URL + "/url", {
-													url: window.location,
-												}).then(() => {
-													window.location.href =
-													process.env.REACT_APP_SERVER_URL + "/auth/google";
-												})
-											}
-										>
-											<div className="google__button" />
-											<p className='tw-text-base tw-text-labBlue tw-font-bold'>Sign In</p>
-										</a>
-										{/* REMOVE ABOVE */}
-									</div>
-								</NavItem>
-							)
-						}				
+						<NavItem className="tw-px-4 tw-py-2 tw-flex tw-justify-center tw-items-center">
+							{
+								loggedIn ? (
+									// TO-DO: PROFILE LINK HERE
+									<NavLink className="tw-aspect-square tw-p-0 tw-border-solid tw-border-4 tw-border-labBlue tw-rounded-full tw-overflow-hidden">
+										<img src={state.main.user?.userpfp} alt="Google Profile Photo" className="tw-h-12" ></img>
+									</NavLink>
+								) : (
+									// TO-DO: SIGN-IN MODAL HERE
+									<NavLink className="tw-flex tw-items-center tw-justify-center tw-p-0" href="#sign-in">
+										<p className='tw-text-base tw-text-labBlue tw-font-bold'>Sign In</p>
+									</NavLink>
+								)
+							}
+						</NavItem>		
 					</Nav>
 				</Collapse>
-				{/* USER IS LOGGED IN, renders google pfp */}
-				{
-					loggedIn && (
-						<div className="tw-aspect-square tw-flex tw-items-center tw-justify-center tw-border-solid tw-border-labBlue tw-border-4 tw-rounded-full tw-overflow-clip tw-max-h-16">
-							<img src={state.main.user?.userpfp} alt="Google Profile Photo" className="" ></img>
-						</div>
-					)
-				}
 			</div>
-			<a className="tw-flex tw-no-underline tw-justify-end tw-text-labBlue tw-cursor-pointer">
+			<a className="tw-flex tw-justify-end tw-no-underline tw-items-center tw-text-labBlue tw-cursor-pointer">
 				<p>Site Accessibility Settings</p>
 			</a>
 		</div>
+		
 	</Navbar>
 	
 	);
