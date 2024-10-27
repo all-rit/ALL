294bfd99ea9f89c5098ee35f0f6b7de5eac2e7c6
Domenic Mangano <domenicmangano@Domenics-MacBook-Pro.local>
2024-10-27 09:11:38 -0400

    updated mobile styling for About Us section

--- client/src/components/all-components/AboutUs.js
+++ client/src/components/all-components/AboutUs.js
@@ -1,56 +1,46 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useRef } from "react";
 import ALLButton from "./ALLButton";
 import { navigate } from "@reach/router";
 const AboutUs = () => {
   const yellowBlock = useRef(null);
-  const [width, setWidth] = useState(0);
 
   const handleNav = () => {
     navigate("/aboutus");
   };
 
-  useEffect(() => {
-    const changeWidth = () => {
-      if (yellowBlock.current) {
-        setWidth(yellowBlock.current.offsetWidth);
-      }
-    };
-    changeWidth();
-    window.addEventListener("resize", changeWidth);
-  }, []);
-
   return (
-    <div className={"tw-h-[30rem] tw-w-full"}>
+    <div className={"tw-h-[20rem] tw-w-full"}>
       <div
         className={
-          "tw-h-1/2 tw-border-t-[4rem] tw-border-r-[1.5rem] tw-border-b-0 tw-border-l-0 tw-border-solid tw-border-primary-yellow tw-bg-primary-yellow"
+          "tw-h-3/4 xs:tw-flex-col md:tw-flex-row tw-border-t-[4rem] tw-border-r-[1.5rem] tw-border-b-0 tw-border-l-0 tw-border-solid tw-border-primary-yellow tw-bg-primary-yellow"
         }
       >
         <div
           className={
-            "tw-h-full tw-flex tw-flex-row tw-justify-center tw-border-t-[.75rem] tw-border-r-[.75rem] tw-rounded-tr-lg tw-border-b-0 tw-border-l-0 tw-border-solid tw-border-primary-blue tw-bg-white"
+            "tw-h-full tw-flex tw-flex-row tw-justify-center tw-border-t-[.75rem] tw-border-r-[.75rem] tw-rounded-tr-lg tw-border-b-0 tw-border-l-0 tw-border-solid tw-border-primary-blue tw-bg-white tw-p-3"
           }
         >
           <div
-            className={`tw-ml-[2.25rem] tw-flex tw-flex-row tw-text-left`}
-            style={{
-              width: `${width}px`,
-            }}
+            className={`tw-ml-[2.25rem] tw-flex tw-flex-row tw-text-left xs:tw-w-full md:tw-w-1/2`}
           >
             <div
               className={
-                "tw-flex tw-flex-col tw-w-1/2 tw-h-full tw-justify-center"
+                "tw-flex tw-flex-col tw-w-3/4 tw-h-full tw-justify-center"
               }
             >
               <p
                 className={
-                  "tw-title-styling-name tw-text-2xl tw-my-3 tw-font-poppins"
+                  "tw-title-styling-name xs:tw-text-sm md:tw-text-[1.5rem] tw-my-3 tw-font-poppins"
                 }
               >
                 {" "}
                 About Us
               </p>
-              <p className={"tw-font-calibri tw-font-medium tw-text-sm"}>
+              <p
+                className={
+                  "tw-font-calibri tw-font-medium xs:tw-text-sm lg:tw-text-[1.125rem]"
+                }
+              >
                 {" "}
                 Learn more about the team at Accessible Learning Labs and the
                 amazing things we have in the works!
@@ -67,11 +57,13 @@ const AboutUs = () => {
         </div>
       </div>
       <div
-        className={"tw-w-full tw-h-1/2 tw-flex tw-flex-row tw-justify-center"}
+        className={
+          "tw-w-full tw-h-1/2 tw-flex tw-flex-row tw-justify-center xs:tw-collapse md:tw-visible"
+        }
       >
         <div
           ref={yellowBlock}
-          className={"tw-bg-primary-yellow tw-w-1/2 tw-h-3/4"}
+          className={"tw-bg-primary-yellow tw-w-1/2 xs:tw-h-1/2 md:tw-h-3/4"}
         >
           {/*TODO: Placeholder for Stock Image*/}
         </div>
