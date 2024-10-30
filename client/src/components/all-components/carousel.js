4c612c62434d60b1965f8c9795b2574742b445e3
Michael DiBiase <MJD0904@Gmail.com>
2024-10-30 16:09:32 -0400

    Need to work on image sizing, flow to the right, needs flexbox = large images

--- client/src/components/all-components/carousel.js
+++ client/src/components/all-components/carousel.js
@@ -4,7 +4,7 @@ import { images } from "src/constants/school_images.js";
 const Carousel = () => {
   return (
     <>
-      <div className="tw-m-auto tw-relative tw-flex tw-items-center tw-h-80 tw-w-3/4 tw-border-black tw-border-solid tw-overflow-hidden">
+      <div className="tw-m-auto tw-relative tw-items-center tw-h-80 tw-w-3/4 tw-border-black tw-border-solid tw-overflow-hidden">
         <div className="tw-w-full tw-h-full tw-scroll-smooth tw-animate-infinte-scroll tw-whitespace-nowrap">
           {images.map((item, id) => (
             <img
@@ -26,8 +26,17 @@ const Carousel = () => {
         </div>
       </div>
 
-      <div className="tw-m-auto tw-relative tw-flex tw-items-center tw-h-80 tw-w-3/4 tw-border-black tw-border-solid tw-overflow-hidden">
-        <div className="tw-w-full tw-h-full tw-scroll-smooth tw-animate-infinte-scroll-right tw-whitespace-nowrap tw-float-right tw-border-black tw-border-solid tw-justify-start">
+      <div className="tw-flex tw-m-auto tw-relative tw-items-center tw-h-80 tw-w-3/4 tw-border-black tw-border-solid tw-overflow-hidden">
+        <div className="tw-flex tw-justify-end tw-w-full tw-h-full tw-scroll-smooth tw-animate-infinte-scroll-right tw-whitespace-nowrap tw-float-right tw-border-black tw-border-solid">
+          {images.map((item, id) => (
+            <img
+              className="tw-max-w-sm tw-max-h-sm tw-m-5"
+              key={id}
+              src={require("../../assets/images/" + item.img + ".png")}
+              alt={item.alt}
+            />
+          ))}
+
           {images.map((item, id) => (
             <img
               className="tw-max-w-sm tw-max-h-sm tw-m-5"
