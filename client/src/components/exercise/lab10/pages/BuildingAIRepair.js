5367185d8b0f4319455a705f3ca0edee8e9e1fbf
Domenic Mangano <domenicmangano@domenics-mbp.wireless.rit.edu>
2024-11-05 16:33:29 -0500

    Working through lab 10

--- client/src/components/exercise/lab10/pages/BuildingAIRepair.js
+++ client/src/components/exercise/lab10/pages/BuildingAIRepair.js
@@ -52,15 +52,18 @@ class BuildingAIRepair extends Component {
 
     return (
       <div>
+        <h1 className={"tw-title-styling-name tw-text-left"}>
+          Repair AI Movement
+        </h1>
         <Fragment>
           <div className={"center-div"}>
             <div className={"guidance margin-bottom-2"}>
-              <p className={"playthrough__sentence tw-text-center"}>
+              <p className={"tw-body-styling-name tw-my-6"}>
                 Let&lsquo;s create our own AI using a simple exercise where you
                 will move a person left and right across your screen and attempt
                 to avoid different colored falling shapes.
               </p>
-              <p className={"playthrough__sentence tw-text-center"}>
+              <p className={"tw-body-styling-name tw-my-6"}>
                 We need to generate and collect data, but first, we need to
                 write code to make our object move. Click the &lsquo;
                 <span className={"tw-font-bold"}>Repair</span>
