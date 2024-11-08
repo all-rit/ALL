23fb84e8d0f03d3ca49a8f45b090fd994f6d3ce1
Owen Luts <owenlutsdev@gmail.com>
2024-11-08 12:03:55 -0500

    HOME PAGE IS TOTALLY MOBILE COMPATIBLE LETS GO

--- client/src/components/body/landingpage/index.js
+++ client/src/components/body/landingpage/index.js
@@ -50,7 +50,7 @@ const Home = () => {
         <section className="tw-flex tw-bg-primary-blue tw-justify-center tw-items-center tw-py-[75px] sm:tw-py-[150px]">
           <div className="tw-grid tw-w-full tw-h-1/2">
             <div className="tw-bg-primary-yellow tw-w-11/12 tw-h-4/5 tw-justify-self-end tw-self-end tw-rounded-bl-lg tw-relative tw-rounded-lg">
-              <div className="tw-bg-white tw-w-full tw-h-[120%] tw-px-16 tw-py-10 tw-justify-self-end tw-self-center tw-rounded-bl-lg tw-relative tw-bottom-14 tw-left-4 tw-flex tw-items-start tw-flex-col tw-gap-8">
+              <div className="tw-bg-white tw-w-full tw-h-[120%] tw-px-8 tw-py-10 md:tw-px-16 md:tw-py-10 tw-justify-self-end tw-self-center tw-rounded-bl-lg tw-relative tw-bottom-14 tw-left-4 tw-flex tw-items-start tw-flex-col tw-gap-8">
                 <h1 className="tw-title-styling-name">
                   Welcome to Accessible Learning Labs
                 </h1>
