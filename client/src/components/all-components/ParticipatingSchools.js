04af2d18c261d818e26b65b905fb044a2b97d7c1
komeally <kasim.omeally@gmail.com>
2024-11-25 13:50:30 -0500

    Added new images, deleted unnecessary reverse function

--- client/src/components/all-components/ParticipatingSchools.js
+++ client/src/components/all-components/ParticipatingSchools.js
@@ -23,28 +23,6 @@ function display_schools(schools) {
   );
 }
 
-function display_schools_reverse(schools) {
-  return (
-    <>
-      {schools.map((schoolInfo, id) => (
-        <a
-          href={schoolInfo.websiteURL}
-          key={id}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img
-            className="tw-max-w-40 lg:tw-max-w-sm tw-max-h-sm tw-m-5"
-            key={id}
-            src={`/img/participating_schools${schoolInfo.imageURL}`}
-            alt={schoolInfo.schoolName}
-          />
-        </a>
-      ))}
-    </>
-  );
-}
-
 const ParticipatingSchools = () => {
   const [schoolInformation, setSchoolInformation] = useState([]);
 
@@ -68,8 +46,8 @@ const ParticipatingSchools = () => {
 
       <div className="tw-m-auto tw-relative tw-items-center tw-h-1/6 lg:tw-h-1/4 tw-w-full lg:tw-w-3/4 tw-overflow-hidden">
         <div className="tw-w-full tw-h-full tw-flex tw-justify-end tw-scroll-smooth tw-animate-infinite-scroll-right tw-whitespace-nowrap ">
-          {display_schools_reverse(schoolInformation)}
-          {display_schools_reverse(schoolInformation)}
+          {display_schools(schoolInformation)}
+          {display_schools(schoolInformation)}
         </div>
       </div>
     </>
