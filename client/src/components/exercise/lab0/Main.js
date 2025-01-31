158bd61f8cc7891db1487c3f8492b0e0978ca8df
Domenic Mangano <domenicmangano@Domenics-MacBook-Pro.local>
2025-01-31 12:20:06 -0500

    Committing Split between Writing and Developing sections

--- client/src/components/exercise/lab0/Main.js
+++ client/src/components/exercise/lab0/Main.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import { Router } from "@reach/router";
+import SelectExercise from "./SelectExercise";
+import GameStateContext from "./Lab0Context";
+import { EXERCISE_STATES } from "../../../constants/lab0";
 
 const Main = () => {
-  return <div>Lab 0 Exercise</div>;
+  const [exerciseState, setExerciseState] = useState(
+    EXERCISE_STATES.EXERCISE_SELECTION_DEFAULT,
+  );
+
+  return (
+    <>
+      <GameStateContext.Provider value={{ exerciseState, setExerciseState }}>
+        <Router className={"tw-p-3"}>
+          <SelectExercise default path={"/*"} />
+        </Router>
+      </GameStateContext.Provider>
+    </>
+  );
 };
 
 export default Main;
