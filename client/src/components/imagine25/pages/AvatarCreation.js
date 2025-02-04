e2228aee1478634f8840e337e752033594d4535d
ConnerDeFeo <ninjanerozz@gmail.com>
2025-02-04 13:17:29 -0500

    avatarCreation page complete

--- client/src/components/imagine25/pages/AvatarCreation.js
+++ client/src/components/imagine25/pages/AvatarCreation.js
@@ -8,18 +8,21 @@ import {
   DropdownItem,
   FormGroup,
   Label,
+  Form,
+  Col,
+  Button,
 } from "reactstrap";
+import "./avatarStyling.css";
 
 //Function for each respective row in the avatarcreation page to stylize them
-function AvatarStyling(setAvatarState, options) {
+function AvatarStyling(currentSelection, setAvatarState, options) {
   //basic toggling and changing functionality for dropdown
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const toggle = () => setDropdownOpen((prevState) => !prevState);
-  const [currentSelection, setCurrentSelection] = useState("");
 
   return (
-    <Dropdown isOpen={dropdownOpen} toggle={toggle} classname="bg-light">
-      <DropdownToggle color={"light"} className="border boder-dark" caret>
+    <Dropdown isOpen={dropdownOpen} toggle={toggle}>
+      <DropdownToggle color={"light"} className="dropDownToggle" caret>
         {currentSelection}
       </DropdownToggle>
       <DropdownMenu>
@@ -30,8 +33,6 @@ function AvatarStyling(setAvatarState, options) {
             <DropdownItem
               key={key}
               onClick={() => {
-                //Displayed Value
-                setCurrentSelection(value);
                 //Value that was set
                 setAvatarState(key);
               }}
@@ -56,52 +57,81 @@ const AvatarCreation = () => {
   //keep track of current avatar state
   const [hairStyle, setHairStyle] = useState("LongHairStraight");
   const [hairColor, setHairColor] = useState("Black");
-  const [shirtColor, setShirtColor] = useState("Blue");
+  const [shirtColor, setShirtColor] = useState("Gray");
   const [skinColor, setSkinColor] = useState("Light");
 
   return (
     <>
-      <p>Design your avatar!</p>
-      <Avatar
-        clotheType="ShirtCrewNeck"
-        topType={hairStyle}
-        hairColor={hairColor}
-        clotheColor={shirtColor}
-        skinColor={skinColor}
-      />
-      <FormGroup>
-        {/*Each AvatarStyling() function takes in their respective setState, and a map of options.
+      <h3>Design your avatar!</h3>
+      <div className="decorativeLineBlueHorizontal"></div>
+      <div className="d-flex justify-content-center">
+        <div className="decorativeLineYellowVertical"></div>
+        <div>
+          <Avatar
+            clotheType="ShirtCrewNeck"
+            topType={hairStyle}
+            hairColor={hairColor}
+            clotheColor={shirtColor}
+            skinColor={skinColor}
+            className="avatar"
+          />
+          {/*Each AvatarStyling() function takes in their respective setState, and a map of options.
                 The Key will represent the code-side implementation and the value is what the user will see
                 as a selection choice. Labels must be added mannually prior to Avatar styling.*/}
-        <Label>Hair Style</Label>
-        {AvatarStyling(setHairStyle, {
-          ShortHairShortCurly: "Short Curly",
-          LongHairCurly: "Long Curly",
-          ShortHairShortFlat: "Short Straight",
-          LongHairStraight: "Long Straight",
-        })}
-        <Label>Hair Color</Label>
-        {AvatarStyling(setHairColor, {
-          Black: "Black",
-          Blonde: "Blonde",
-          Blue: "Blue",
-          Red: "Red",
-        })}
-        <Label>Shirt Color</Label>
-        {AvatarStyling(setShirtColor, {
-          Gray01: "Gray",
-          Black: "Black",
-          PastelYellow: "Yellow",
-          Pink: "Pink",
-        })}
-        <Label>Skin Color</Label>
-        {AvatarStyling(setSkinColor, {
-          Light: "White",
-          Brown: "Brown",
-          DarkBrown: "Dark Brown",
-          Black: "Black",
-        })}
-      </FormGroup>
+          <Form className="my-3">
+            <FormGroup row>
+              <Col>
+                <Label className="mx-2 fw-bold">Hair Style</Label>
+                {AvatarStyling(hairStyle, setHairStyle, {
+                  ShortHairShortCurly: "Short Curly",
+                  LongHairCurly: "Long Curly",
+                  ShortHairShortFlat: "Short Straight",
+                  LongHairStraight: "Long Straight",
+                })}
+              </Col>
+            </FormGroup>
+            <FormGroup row>
+              <Col>
+                <Label className="mx-2 fw-bold">Hair Color</Label>
+                {AvatarStyling(hairColor, setHairColor, {
+                  Black: "Black",
+                  Blonde: "Blonde",
+                  Blue: "Blue",
+                  Red: "Red",
+                })}
+              </Col>
+            </FormGroup>
+            <FormGroup row>
+              <Col>
+                <Label className="mx-2 fw-bold">Shirt Color</Label>
+                {AvatarStyling(shirtColor, setShirtColor, {
+                  Gray01: "Gray",
+                  Black: "Black",
+                  PastelYellow: "Yellow",
+                  Pink: "Pink",
+                })}
+              </Col>
+            </FormGroup>
+            <FormGroup row>
+              <Col>
+                <Label className="mx-2 fw-bold">Skin Color</Label>
+                {AvatarStyling(skinColor, setSkinColor, {
+                  Light: "White",
+                  Brown: "Brown",
+                  DarkBrown: "Dark Brown",
+                  Black: "Black",
+                })}
+              </Col>
+            </FormGroup>
+          </Form>
+        </div>
+        <div className="decorativeLineYellowVertical"></div>
+      </div>
+      <div className="d-flex">
+        <Button className="avatarButton">Previous</Button>
+        <div className="decorativeLineBlueHorizontal"></div>
+        <Button className="avatarButton">Next</Button>
+      </div>
     </>
   );
 };
