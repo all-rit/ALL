70aab0b0bc42cf021d5dd8af2392c2564c177c6e
ConnerDeFeo <ninjanerozz@gmail.com>
2025-02-20 14:14:14 -0500

    Galaga has easier enemies, removed title screen, added timer

--- client/src/components/imagine25/pages/Game.js
+++ client/src/components/imagine25/pages/Game.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Game = () => {
+  const [seconds, setSeconds] = useState(60);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setSeconds((prevSeconds) => prevSeconds - 1);
+    }, 1000);
+
+    return () => clearInterval(timer);
+  });
+
   const sizeStyling = " tw-w-[800px] tw-h-[600px]";
   return (
-    <div className={"tw-flex tw-justify-left tw-items-center" + sizeStyling}>
+    //onClick keeps tract of which part of frame user has clicked on
+    <div
+      className={
+        "tw-flex tw-justify-left tw-items-center tw-relative" + sizeStyling
+      }
+    >
       <iframe
         src="https://microstudio.io/Nerozz/galaga/3E7A6TBT/"
         className={"tw-rounded-xl" + sizeStyling}
       />
+      <div className="tw-flex tw-justify-center tw-w-[100%] tw-absolute tw-top-5 tw-text-white ">
+        <div>{seconds}</div>
+      </div>
     </div>
   );
 };
