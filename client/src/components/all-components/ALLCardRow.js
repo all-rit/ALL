455b76a7c5ebc88f20dfd650956e86cda0cd27fb
JackyFTW <jackmbarter@gmail.com>
2025-02-24 14:20:09 -0500

    Added JSDocs to ALLCardRow, yayyy

--- client/src/components/all-components/ALLCardRow.js
+++ client/src/components/all-components/ALLCardRow.js
@@ -1,6 +1,22 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+/**
+ * A standard ALL component that displays an image, a title, and a button in a row.
+ * This component also adds a generic indicator level using circles that is shown
+ * above the title. The component is displayed in a row in the following order, left to right:
+ * image -> title and circles -> button.
+ *
+ * @param {any} props.children The content to be shown as secondary information below the row.
+ * @param {string} props.title The title of this row, to be shown in the middle of the row.
+ * @param {string} props.imageURL The relative path to an image to be shown at the start of the row.
+ * @param {string} props.circlesLabel The text to be shown inline before the filled in circles.
+ * @param {number} props.circles The number of total circles to display.
+ * @param {number} props.circlesFilled The number of circles that are filled in. Must be less than {@link props.circles}.
+ * @param {string} props.buttonLabel The text to be displayed within the button.
+ * @param {string} props.buttonStyle Custom styling to apply to the default button look.
+ * @param {function} props.onClick The function that should be called whenever the button is clicked.
+ */
 const ALLCardRow = (props) => {
   const {
     title,
