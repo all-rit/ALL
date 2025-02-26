b0097aa13650b1d6ec668ccf7ef9137ebe0bb4f4
ConnerDeFeo <ninjanerozz@gmail.com>
2025-02-26 12:38:14 -0500

    did pointless modal

--- client/src/components/imagine25/pages/Game.js
+++ client/src/components/imagine25/pages/Game.js
@@ -1,15 +1,5 @@
-import { Modal, ModalBody } from "reactstrap";
 import React, { useEffect, useState } from "react";
-import { Frame } from "../components/Frame";
-import ALLButton from "src/components/all-components/ALLButton";
 import "./Game.css";
-
-//Random score that will be generated for both teams
-const userScore = Math.floor(Math.random() * 1000 + 500);
-
-/*opponent score will always be less than user score, but never less than 475
-This is done so that the game seems realistically close*/
-const opponentScore = Math.floor((userScore - 500) * Math.random() + 475);
 
 const Game = () => {
   const [seconds, setSeconds] = useState(60);
@@ -47,30 +37,6 @@ const Game = () => {
       <div className="tw-flex tw-justify-center tw-w-[100%] tw-absolute tw-top-5 tw-text-white timerFont">
         <div>{seconds}</div>
       </div>
-      <Modal isOpen={modal} className="tw-flex">
-        <ModalBody className="tw-w-[60vw]">
-          {Frame(
-            <>
-              <h3 className="tw-text-center tw-my-[50px]">
-                <strong>You Win!</strong>
-              </h3>
-              <div className="xs:tw-text-md xl:tw-text-xl tw-text-left">
-                <p>Team&apos;s score: {userScore}</p>
-                <p className="tw-mt-[50px]">
-                  Opponent team&apos;s score: {opponentScore}
-                </p>
-              </div>
-              <ALLButton
-                onClick={() => alert("No next page implemented")}
-                label="Analyze Game"
-                className="tw-flex tw-justify-center tw-mt-[75px]"
-              />
-            </>,
-            null,
-            null,
-          )}
-        </ModalBody>
-      </Modal>
     </div>
   );
 };
