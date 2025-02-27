4496a0f0f80c1ababe70d3c22c5e44fa9cf2aa0a
ConnerDeFeo <ninjanerozz@gmail.com>
2025-02-27 13:01:11 -0500

    added dynamically changing capabilities to game.js, now content can be swapped with the galaga game

--- client/src/components/imagine25/pages/Game.js
+++ client/src/components/imagine25/pages/Game.js
@@ -2,8 +2,19 @@ import React, { useEffect, useState } from "react";
 import "./Game.css";
 
 const Game = () => {
+  const contentSizing = "tw-rounded-xl tw-w-[52vw] tw-h-[39vw] xl:tw-h-[600px]";
+
+  const [bgColor, setBgcolor] = useState(" tw-bg-[black]");
+
+  //Content that is embeded on the left side
+  const [content, setContent] = useState(
+    <iframe
+      src="https://microstudio.io/Nerozz/galaga/3E7A6TBT/"
+      className={contentSizing}
+    />,
+  );
+
   const [seconds, setSeconds] = useState(60);
-  const [modal, setModal] = useState(false);
 
   //When page loads timer starts that counts down from 60->0
   useEffect(() => {
@@ -11,28 +22,27 @@ const Game = () => {
       setSeconds((prevSeconds) => {
         if (prevSeconds <= 1) {
           clearInterval(timer);
-          toggle();
+          setContent(<div className={contentSizing}>Hello World</div>);
+          setBgcolor("");
+          return;
         }
+
         return prevSeconds - 1;
       });
     }, 1000);
-
     return () => clearInterval(timer);
   }, []);
 
-  const toggle = () => setModal(!modal);
-
   return (
-    //flex container used to venter game vertically
+    //flex container used to center game vertically, dimensinos are slightly different than content sizing for scaling purposes
     <div
+      //make backgorund black if game is running
       className={
-        "tw-flex tw-justify-left tw-items-center tw-relative tw-w-[52vw] xs:tw-h-[500px] md:tw-h-[525px] lg:tw-h-[550px] xl:tw-h-[600px] tw-bg-[Black] tw-rounded-xl"
+        "tw-flex tw-justify-left tw-items-center tw-relative tw-w-[52vw] xs:tw-h-[500px] md:tw-h-[525px] lg:tw-h-[550px] xl:tw-h-[600px] tw-rounded-xl" +
+        bgColor
       }
     >
-      <iframe
-        src="https://microstudio.io/Nerozz/galaga/3E7A6TBT/"
-        className={"tw-rounded-xl tw-w-[52vw] tw-h-[39vw] xl:tw-h-[600px]"}
-      />
+      {content}
       {/*Not sure if tailwind can support custom styling so "timerFont" is in a css file */}
       <div className="tw-flex tw-justify-center tw-w-[100%] tw-absolute tw-top-5 tw-text-white timerFont">
         <div>{seconds}</div>
