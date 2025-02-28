6e7b3eb6e514e02314ea96cf1bfab188d7166569
mpb8984 <71938683+mpb8984@users.noreply.github.com>
2025-02-28 15:21:26 -0500

    rename dragdropgame file

--- client/src/components/exercise/lab0/DesignLabSection/ScrumActivity/ScrumBoardActivity.js
+++ client/src/components/exercise/lab0/DesignLabSection/ScrumActivity/ScrumBoardActivity.js
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from "react";
-import DragDropGame from "../../../../all-components/DragAndDrop/DragAndDrop";
+import DragDropGame from "../../../../all-components/DragAndDrop/DragDropGame";
 import {
   initialColumns,
   initialBank,
