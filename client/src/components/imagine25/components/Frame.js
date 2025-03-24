cb4742db2d0c36996e992de3ca03478d26d60f8c
ConnerDeFeo <ninjanerozz@gmail.com>
2025-03-24 12:53:30 -0400

    buttons button button

--- client/src/components/imagine25/components/Frame.js
+++ client/src/components/imagine25/components/Frame.js
@@ -1,20 +1,33 @@
 import React from "react";
-import ALLButton from "src/components/all-components/ALLButton";
 
 //put null for any button you do not wish to use
 export const Frame = (content, nextOnClick, prevOnClick) => {
   const prev =
     prevOnClick != null ? (
-      <ALLButton onClick={prevOnClick} label="Previous" inverted={true} />
+      <button
+        className="tw-border-solid tw-border-primary-yellow tw-pb-[0.3rem] tw-pl-[0.5rem] tw-w-[7rem] tw-h-[3rem]
+        tw-border-[0.4rem] tw-border-r-0 tw-border-t-0 tw-rounded-bl-lg yellow-drop-shadow tw-bg-[white] tw-text-xl"
+        onClick={prevOnClick}
+      >
+        Previous
+      </button>
     ) : (
       <div></div>
     );
+
   const next =
     nextOnClick != null ? (
-      <ALLButton onClick={nextOnClick} label="Next" />
+      <button
+        className="tw-border-solid tw-border-primary-blue tw-pt-[0.3rem] tw-pr-[0.5rem] tw-w-[7rem] tw-h-[3rem]
+        tw-border-[0.4rem] tw-border-l-0 tw-border-b-0 tw-rounded-tr-lg blue-drop-shadow tw-bg-[white] tw-text-xl"
+        onClick={nextOnClick}
+      >
+        Next
+      </button>
     ) : (
       <div></div>
     );
+
   return (
     <div className="tw-flex tw-flex-col tw-items-center">
       <div className="d-flex justify-content-center tw-py-6">
