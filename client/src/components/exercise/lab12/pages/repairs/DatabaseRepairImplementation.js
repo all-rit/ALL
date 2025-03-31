5da30caeeca9e4d621595314b611fd18dfcf4005
JackyFTW <jackmbarter@gmail.com>
2025-03-31 11:48:37 -0400

    Refactored DatabaseRepairImplementation

--- client/src/components/exercise/lab12/pages/repairs/DatabaseRepairImplementation.js
+++ client/src/components/exercise/lab12/pages/repairs/DatabaseRepairImplementation.js
@@ -11,15 +11,15 @@ const DatabaseRepairImplementation = (props = {}) => {
   /**
    * DatabaseRepairImplementation is a component that is responsible for displaying the codeblock contents
    * and user inpunts where the participant will make their changes to the codebase and complete the repair section.
-   * @param {props} identityData contains the data used for answer validation and display of it's contents
+   * @param {props} inputs contains the data used for answer validation and display of it's contents
    * @param {props} userInput is a function that takes the user's input in each input and sends the data to the useDataService hook,
    * which then sends that to the useLabService hook
-   * @param {props} isInputValid returns an array based on the number of correct and incorrect inputs of the user in the repair section
+   * @param {props} validInputs returns an array based on the number of correct and incorrect inputs of the user in the repair section
    * @param {props} isFirst returns a boolean value of whether or not this is the first time a user is viewing this repair section
    * @returns
    */
 
-  const { identityData, userInput, isInputValid, isFirst } = props;
+  const { inputs, userInput, validInputs, isFirst } = props;
   return (
     <>
       <CodeLine>
@@ -44,7 +44,7 @@ const DatabaseRepairImplementation = (props = {}) => {
           &apos;they/them&apos;
         </CommentText>
       </CodeLine>
-      {identityData
+      {inputs
         .filter((item) => item.id === 0)
         .map((item, index) => (
           <div key={index}>
@@ -79,7 +79,7 @@ const DatabaseRepairImplementation = (props = {}) => {
               )}
               <SQLText>)</SQLText>
             </CodeLine>
-            {!isInputValid[index] && !isFirst && (
+            {!validInputs[index] && !isFirst && (
               <CodeLine>
                 <MultiTab numberOfTabs={11} />
                 <ErrorText>
@@ -97,7 +97,7 @@ const DatabaseRepairImplementation = (props = {}) => {
         {/* eslint-disable-next-line react/jsx-no-comment-textnodes */}
         <CommentText>// Enter varchar(255)</CommentText>
       </CodeLine>
-      {identityData
+      {inputs
         .filter((item) => item.id === 1)
         .map((item, index) => (
           <div key={index}>
@@ -131,7 +131,7 @@ const DatabaseRepairImplementation = (props = {}) => {
                 />
               )}
             </CodeLine>
-            {!isInputValid[index] && !isFirst && (
+            {!validInputs[index] && !isFirst && (
               <CodeLine>
                 <MultiTab numberOfTabs={11} />
                 <ErrorText>
@@ -150,10 +150,10 @@ const DatabaseRepairImplementation = (props = {}) => {
 };
 
 DatabaseRepairImplementation.propTypes = {
+  inputs: PropTypes.array,
   userInput: PropTypes.func,
-  isInputValid: PropTypes.array,
+  validInputs: PropTypes.array,
   isFirst: PropTypes.bool,
-  identityData: PropTypes.array,
 };
 
 export default DatabaseRepairImplementation;
