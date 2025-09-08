546ff168b6510848ba3d641288f0550244114bb3
domenicmangano <dm9965@rit.edu>
2025-09-08 12:19:55 -0400

    Added first dropdown for lab data repair

--- client/src/components/exercise/lab0/DevelopLabSection/LabData/LabDataRepair.js
+++ client/src/components/exercise/lab0/DevelopLabSection/LabData/LabDataRepair.js
@@ -1,9 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import SQLText from "../../../../all-components/CodeBlock/StyleComponents/SQLText";
 import { labData } from "../../../../../constants/lab0/DevelopALab/LabTableData";
+import {
+  ButtonDropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
+} from "reactstrap";
+import JSONText from "../../../../all-components/CodeBlock/StyleComponents/JSONText";
 // import { SQLText, JSONText } from "../../../../all-components/CodeBlock/StyleComponents"
 
 const DataRepair = () => {
+  const CORRECT_REPAIR = {
+    shortName: "How to Build a Lab",
+    category: "",
+    shortDescription: "",
+    walkthroughVideo: "",
+  };
+
+  const INITIAL_SHORT_NAME_LABEL = "Select a proper short lab name";
+
+  const [shortNameDropdownOpen, setShortNameDropdownOpen] = useState(false);
+  const [shortNameDropdownLabel, setShortNameDropdownLabel] = useState(
+    INITIAL_SHORT_NAME_LABEL,
+  );
+  const [dataRepair, setDataRepair] = useState({
+    shortName: "",
+    category: "",
+    shortDescription: "",
+    walkthroughVideo: "",
+  });
+
+  const repairComplete = dataRepair === CORRECT_REPAIR;
+
+  const toggleShortNameDropdown = () => {
+    setShortNameDropdownOpen(!shortNameDropdownOpen);
+  };
+
+  const updateShortName = (option) => {
+    setDataRepair((prev) => ({
+      ...prev,
+      shortName: option,
+    }));
+    setShortNameDropdownLabel(option);
+  };
+
+  const SHORT_NAME_OPTIONS = [
+    "Building Really Cool Labs with ALL",
+    "How to Build a Lab",
+    "Labs",
+  ];
+
   return (
     <div
       className={"code_editor__code tw-w-full tw-h-full tw-p-5 tw-rounded-lg"}
@@ -25,9 +72,36 @@ const DataRepair = () => {
       <div>
         <SQLText> VALUES (</SQLText>
         <div className={"tw-flex tw-flex-col"}>
-          <SQLText>0</SQLText>
-          <SQLText>0</SQLText>
-          <SQLText>0</SQLText>
+          <JSONText>0</JSONText>
+          <JSONText>
+            &apos;How to Build a Lab with Accessible Learning Labs&apos;,
+          </JSONText>
+          <div className={"tw-flex tw-w-1/2 tw-items-center"}>
+            <ButtonDropdown
+              toggle={toggleShortNameDropdown}
+              isOpen={shortNameDropdownOpen}
+            >
+              <DropdownToggle
+                style={{ fontFamily: "monospace", backgroundColor: "#333" }}
+                caret
+              >
+                {shortNameDropdownLabel}
+              </DropdownToggle>
+              <DropdownMenu>
+                {SHORT_NAME_OPTIONS.map((option) => {
+                  return (
+                    <DropdownItem
+                      key={option}
+                      onClick={() => updateShortName(option)}
+                    >
+                      {option}
+                    </DropdownItem>
+                  );
+                })}
+              </DropdownMenu>
+            </ButtonDropdown>
+            <JSONText>,</JSONText>
+          </div>
           <SQLText>0</SQLText>
           <SQLText>0</SQLText>
           <SQLText>0</SQLText>
@@ -38,6 +112,7 @@ const DataRepair = () => {
           <SQLText>0</SQLText>
         </div>
       </div>
+      {repairComplete && <div>Repair is complete!</div>}
     </div>
   );
 };
