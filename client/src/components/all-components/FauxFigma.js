0aea57bdf7876c0f4541490b11180f1d79794fbf
Kristen Fang <kf4538@rit.edu>
2025-09-29 12:55:19 -0400

    Made Figma caption smaller

--- client/src/components/all-components/FauxFigma.js
+++ client/src/components/all-components/FauxFigma.js
@@ -150,7 +150,7 @@ const FauxFigma = (props) => {
           </div>
         </div>
       </div>
-      <p className={"tw-body-text tw-text-black tw-text-center tw-italic"}>
+      <p className={"tw-text-xs tw-text-black tw-text-center tw-italic"}>
         Amgif&apos;s Interface with all of the sections of a typical ALL lab
         page. The examples shown is of Lab 14 the Accessibility to Focus Order.
       </p>
