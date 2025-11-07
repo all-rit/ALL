e8cec255023822fa005fcd0a42d0d9a9b95bf61d
wlh9824 <wlh9824@rit.edu>
2025-11-07 13:38:50 -0500

    superposition page

---
+++ client/src/components/exercise/lab14/components/Qubit.js
@@ -0,0 +1,59 @@
+import { React } from "react";
+import { useState } from "react";
+
+const Qubit = () => {
+    const [isHovering, setIsHovering] = useState(false);
+    const [collapseHex, setCollapseHex] = useState("");
+    const [fontColor, setFontColor] = useState("white");
+    const [time, setTime] = useState(0);
+    const [text, setText] = useState("");
+
+    const colorChange = () => {
+        setIsHovering(true);
+        // Gives random number either 0 or 1 to collapse to for qubit color
+        const rng = Math.floor(Math.random() * 2);
+
+        if (rng == 0) {
+            setCollapseHex("tw-bg-labBlue");
+            setFontColor("white");
+            setText("0");
+        } else {
+            setCollapseHex("tw-bg-labYellow");
+            setFontColor("black");
+            setText("1");
+        }
+    }
+
+    const handleMouseEnter = () => {
+        setTimeout(colorChange, 1500);
+    };
+  
+    const handleMouseLeave = () => {
+      setIsHovering(false);
+      clearTimeout(time);
+      setTime(0);
+      setFontColor("white");
+    };
+  
+    return (
+      <div
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        className={`${isHovering ? collapseHex : "tw-bg-qubit"}`}
+        style={{
+          color: fontColor,
+          width: '100px',
+          height: '100px',
+          borderRadius: '50%',
+          border: '1px solid black',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        {isHovering ? text : 'Qubit'}
+      </div>
+    );
+  };
+
+  export default Qubit;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -90,6 +90,8 @@ module.exports = {
           "linear-gradient(280.39deg, rgba(72, 0, 194, 0.9) 0%, rgba(72, 0, 194, 1) 100%);",
         "hero-dark":
           "linear-gradient(280.39deg, rgba(72, 0, 194, 0.9) 0%, rgba(72, 0, 194, 1) 100%);",
+        "qubit":
+          "linear-gradient(280.39deg, rgba(13, 40, 188, 1) 0%, rgba(255, 195, 52, 1) 100%);"
       },
       height: {
         128: "31rem",
