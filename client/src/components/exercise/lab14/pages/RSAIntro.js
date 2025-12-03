8df56c8ef02149d9962357b4011d10ca74cd4954
Owen Luts <oluts47@gmail.com>
2025-12-03 11:54:45 -0500

    changed padding sizes

--- client/src/components/exercise/lab14/pages/RSAIntro.js
+++ client/src/components/exercise/lab14/pages/RSAIntro.js
@@ -10,7 +10,7 @@ const RSAIntro = () => {
     <div className="center-div">
       <h1 className={"tw-title tw-text-left"}>The RSA Encryption Algorithm</h1>
       <div className="guidance margin-bottom-2">
-        <p className="tw-body-text tw-text-left tw-py-6">
+        <p className="tw-body-text tw-text-left tw-py-4">
           Rivest-Shamir-Adleman, also known as RSA, is a well-known public-key
           or asymmetric cryptographic algorithm. It protects sensitive data
           through encryption and decryption using a private and public key pair.
@@ -19,14 +19,14 @@ const RSAIntro = () => {
           when sending emails, making online purchases, and even using web
           browsers such as the one you&apos;re viewing this page on!
         </p>
-        <p className="tw-body-text tw-text-left tw-py-6">
+        <p className="tw-body-text tw-text-left tw-py-2">
           RSA works by generating two large prime numbers and multiplying them
           together to create a modulus that then creates a public key and a
           private key. The public key is used for encryption and is known to
           everyone, while the private key is used for decryption and must be
           kept secret by the receiver.
         </p>
-        <p className="tw-body-text tw-text-left">
+        <p className="tw-body-text tw-text-left tw-py-4">
           The reason RSA is so secure is because of it&apos;s reliance on the
           mathematical properties of large prime numbers. While it&apos;s easy
           to create an encryption for RSA by multiplying two large prime
