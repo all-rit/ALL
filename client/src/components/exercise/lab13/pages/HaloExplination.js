4b6de9e1218fb976f3dca1cfa9da872b1a06fadb
Kristen Fang <kf4538@rit.edu>
2026-01-21 15:21:35 -0500

    started modal for questionaire

--- client/src/components/exercise/lab13/pages/HaloExplination.js
+++ client/src/components/exercise/lab13/pages/HaloExplination.js
@@ -1,8 +1,15 @@
-import { React } from "react";
+import { React, useState } from "react";
 import { startExercise } from "src/reducers/lab2/actions";
 import { navigate } from "@reach/router";
+// import ALLModal from "src/components/all-components/ALLModal";
+import ALLModal from "src/components/all-components/ALLModal";
 
 const HaloExplination = () => {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setModalOpen(true);
+  };
   const handleContinue = () => {
     startExercise();
     navigate("/Lab13/Exercise/AIandSearchPanel");
@@ -10,7 +17,21 @@ const HaloExplination = () => {
 
   return (
     <div>
-      Halo Effect Explination Page
+      Halo Effect Explanation Page
+      <button onClick={handleOpenModal}>Open Modal</button>
+      <ALLModal
+        show={modalOpen}
+        setShow={setModalOpen}
+        showHeader={true}
+        customHeader={
+          <div className="tw-text-base tw-font-medium tw-text-textGray tw-m-3 tw-">
+            Rate each factor by it&apos;s influence on your trust.
+          </div>
+        }
+        showFooter={false}
+        customBody={<div>hi</div>}
+        // timeOutTime={2}
+      />
       <button onClick={handleContinue}>Next</button>
     </div>
   );
