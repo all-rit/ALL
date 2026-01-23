a3dbc51b147498789680c9cb438b4109051c2980
Warner Harper <warner17@Warners-Laptop-1235.local>
2026-01-23 13:53:13 -0500

    fixed class naming and defined the original class

--- client/src/components/body/Reading/LinkFooter.jsx
+++ client/src/components/body/Reading/LinkFooter.jsx
@@ -9,7 +9,7 @@ import React from "react";
 const LinkFooter = ({ data }) => {
   return (
     <>
-      <h4 className="tw-body-text tw-leading-snug xs:tw-text-s md:tw-text-[1.125rem] tw-m-0 tw-mb-5">
+      <h4 className="footer-header tw-body-text tw-leading-snug xs:tw-text-s md:tw-text-[1.125rem]">
         For more information, please visit the following websites:
       </h4>
       <div className="link-footer">
