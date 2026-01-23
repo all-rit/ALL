85377a3a7634008fd204ae0f0302c5b9b5abdf6b
ConnerDeFeo <ninjanerozz@gmail.com>
2026-01-22 19:37:59 -0500

    this should be the page complete for now

--- client/src/components/exercise/lab13/pages/IDEIntroduction.js
+++ client/src/components/exercise/lab13/pages/IDEIntroduction.js
@@ -1,6 +1,7 @@
 import { React } from "react";
 import { startExercise } from "src/reducers/lab2/actions";
 import { navigate } from "@reach/router";
+import LabButton from "src/components/all-components/LabButton";
 
 const IDEIntroduction = () => {
   const handleContinue = () => {
@@ -9,9 +10,18 @@ const IDEIntroduction = () => {
   };
 
   return (
-    <div>
-      IDE Introduction Page
-      <button onClick={handleContinue}>Next</button>
+    <div className="tw-space-y-6 mx-auto tw-flex tw-flex-col">
+      <h1 className="tw-text-left tw-font-bold">AI Cognitive Bias Repair</h1>
+      <p className="tw-text-left">
+        Let&apos;s tweak our AI chatbot to reduce the chances of a human
+        experiencing cognitive biases.
+      </p>
+      <div className="tw-space-y-4">
+        <p>
+          Click <strong>&quot;Continue to Repair&quot;</strong> to proceed!
+        </p>
+        <LabButton onClick={handleContinue} label="Continue to Repair" />
+      </div>
     </div>
   );
 };
