3ad4c2c35cb4dd0a3763d72f3a3450721fc53834
Darlyn Gomez <darlyngomez89@gmail.com>
2026-01-28 14:09:47 -0500

    Remove demo of tab component on exercise page

--- client/src/components/exercise/lab13/pages/AIPanel.js
+++ client/src/components/exercise/lab13/pages/AIPanel.js
@@ -1,8 +1,6 @@
 import { React } from "react";
 import { startExercise } from "src/reducers/lab2/actions";
 import { navigate } from "@reach/router";
-import { Tabs } from "../components/Tab/Tabs";
-import { Tab } from "../components/Tab/Tab";
 
 const AIPanel = () => {
   const handleContinue = () => {
@@ -10,70 +8,10 @@ const AIPanel = () => {
     navigate("/Lab13/Exercise/HaloExplination");
   };
 
-  const lorem = `
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-    Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-    `;
-
   return (
     <div>
-      {/* AI Panel Page */}
-      {/* TESTER TABS */}
-      <Tabs>
-        <Tab label="Example 1">
-          <h2>Introduction to AI</h2>
-          <p>{lorem.repeat(5)}</p>
-          <hr />
-          <p>{lorem.repeat(3)}</p>
-          <br />
-          <p>{lorem.repeat(2)}</p>
-        </Tab>
-
-        <Tab label="Example 2">
-          <h2>Example 2</h2>
-          <h3>Subtitle</h3>
-          <hr></hr>
-          <p>{lorem.repeat(4)}</p>
-          <hr />
-          <ul>
-            <li>Self-driving cars</li>
-            <li>Speech recognition</li>
-            <li>Recommendation systems</li>
-          </ul>
-          <br />
-          <p>{lorem.repeat(2)}</p>
-        </Tab>
-
-        <Tab label="Example 3">
-          <h2>Example 3</h2>
-          <p>{lorem.repeat(6)}</p>
-          <hr />
-          <p>{lorem.repeat(3)}</p>
-        </Tab>
-
-        <Tab label="Example 4">
-          <h2>Example 4</h2>
-          <p>{lorem.repeat(5)}</p>
-          <hr />
-          <p>{lorem.repeat(2)}</p>
-          <br />
-          <p>{lorem.repeat(3)}</p>
-        </Tab>
-      </Tabs>
-
-      <div className="tw-flex tw-justify-end tw-mt-3 tw-pr-8">
-        <button
-          onClick={handleContinue}
-          className="
-            tw-group tw-relative tw-flex tw-items-center tw-gap-3 tw-px-8 tw-py-2 tw-bg-primary-blue tw-text-white tw-text-sm tw-font-semibold 
-            tw-rounded-full tw-border-4 tw-border-labBlue tw-shadow-lg hover:tw-bg-lightBlue hover:tw-border-lightBlue tw-transition-all tw-duration-200
-            focus:tw-outline-none focus:tw-ring-2 focus:tw-ring-mediumBlue focus:tw-ring-offset-2
-          "
-        >
-          <span className="tw-leading-none">Next</span>
-        </button>
-      </div>
+      AI Panel Page
+      <button onClick={handleContinue}>Next</button>
     </div>
   );
 };
