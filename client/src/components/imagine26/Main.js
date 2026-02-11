3e1d07098b819adac8376b0ad5f1b7a403595b15
Will Herrick <Willh2004@gmail.com>
2026-02-11 15:03:22 -0500

    Boilerplate files for Deepfake lab, still fixing some issues

--- client/src/components/imagine26/Main.js
+++ client/src/components/imagine26/Main.js
@@ -1,14 +1,121 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { Router } from "@reach/router";
+import UpdateId from "./UpdateId";
+import Done from "./Done";
+import ImagineService from "src/services/ImagineService";
 import useMainStateContext from "src/reducers/MainContext";
+import Survey from "../all-components/imagine-components/SurveyHandlerComp";
+import GalagaInstructions from "./pages/GalagaInstruction";
+import Galaga from "./pages/Game";
+import "./main.css";
 
 const Main = () => {
+  const userID = sessionStorage.getItem("userID");
+
+  const [canContinue, setCanContinue] = useState(true);
+
+  // user has to wait 10 seconds before allowing the next person to play
+  useEffect(() => {
+    if (!canContinue) {
+      const timer = setTimeout(() => {
+        setCanContinue(true);
+      }, 10000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [canContinue]);
+
+  //Removes header
   const { actions } = useMainStateContext();
   const startImagine = () => actions.setIsImagine(true);
   useEffect(() => {
     startImagine();
   }, []);
 
-  return <div>Imagine 26 Component</div>;
+  //After each iteration, clear the use-states
+  const clearInstance = () => {
+    setCanContinue(false);
+  };
+
+  const year = 26;
+
+  // current user number
+  const [userNumber, setUserNumber] = useState(1);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      if (userID != null) {
+        const user = await ImagineService.getUserByID(userID, 26);
+        setUserNumber(user?.id);
+      }
+    };
+
+    fetchUser();
+  }, [userID]);
+
+  return (
+    <>
+      <div className={"tw-flex tw-h-full tw-w-full tw-mt-[10%]"}>
+        <div
+          className={
+            "tw-grid tw-grid-cols-8 tw-grid-rows-9 tw-w-full tw-h-[45rem] tw-gap-y-6 tw-pl-6 tw-absolute tw-top-0"
+          }
+        >
+          <div
+            className={
+              "tw-row-span-3 tw-col-span-8 tw-bg-primary-yellow tw-rounded-bl-lg tw-flex shadow"
+            }
+          />
+
+          <div
+            className={
+              "tw-row-span-5 tw-col-span-8 tw-bg-primary-blue tw-rounded-bl-lg tw-flex shadow"
+            }
+          />
+        </div>
+        <div
+          className={
+            "tw-absolute tw-z-10 tw-top-[-4rem] tw-bg-white tw-left-[12.5%] tw-w-3/4 tw-h-4/5 shadow tw-rounded-xl tw-p-6 tw-min-h-[40rem]"
+          }
+        >
+          <Router
+            className={
+              "tw-flex tw-h-full tw-w-full tw-overflow-y-scroll tw-flex-col tw-justify-center"
+            }
+          >
+            <UpdateId default path={"/"} canContinue={canContinue} />
+            <Survey
+              className="app tw-h-full tw-overflow-x-hidden tw-flex tw-justify-center tw-items-center"
+              path={`/PreSurvey`}
+              type={"pre"}
+              year={year}
+              userID={userID || ""}
+            />
+            <GalagaInstructions path={"/GalagaInstructions"} />
+            <Galaga path={"/Galaga"} />
+            <Survey
+              className="app tw-h-full tw-overflow-x-hidden tw-flex tw-justify-center tw-items-center"
+              path={`/PreSurvey`}
+              type={"pre"}
+              year={year}
+              userID={userID || ""}
+            />
+            <Survey
+              className="app tw-h-full tw-overflow-x-hidden tw-flex tw-justify-center tw-items-center"
+              path={`/PostSurvey`}
+              type={"post"}
+              year={year}
+              userID={userID || ""}
+            />
+            <Done path={"/Done"} resetInstance={clearInstance} />
+          </Router>
+          <div className="tw-body-text tw-text-[2rem] tw-absolute tw-right-[-9rem] tw-top-5">
+            User Id: {userNumber || "None"}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Main;
