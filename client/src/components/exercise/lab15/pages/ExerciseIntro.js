8c192a2f4a441d23cc2876bb05786185385a3134
Kristen Fang <kf4538@rit.edu>
2026-03-03 17:43:50 -0500

    Adding overview text for the start of the exercise

--- client/src/components/exercise/lab15/pages/ExerciseIntro.js
+++ client/src/components/exercise/lab15/pages/ExerciseIntro.js
@@ -5,6 +5,8 @@ import { navigate } from "@reach/router";
 import { EXERCISE_PLAYING } from "src/constants/index";
 import { ExerciseService } from "../../../../services/lab15/ExerciseService";
 
+import LabButton from "../../../all-components/LabButton";
+
 const ExerciseIntro = () => {
   const { actions, state } = useMainStateContext();
 
@@ -22,7 +24,25 @@ const ExerciseIntro = () => {
     startExercise();
     navigate("/Lab15/Exercise/model-hallucination");
   };
-  return <button onClick={handleContinue}>Start Exercise</button>;
+
+  return (
+    <>
+      <div className="center-div">
+        <div className="guidance margin-bottom-2">
+          <h1 className={"tw-title tw-text-left"}>Exercise Start</h1>
+          <p className="tw-body-text tw-my-6">
+            In this exercise, the user will explore how ineffective prompting
+            can lead to inaccurate AI responses and learn how to refine them.
+            Guided by GCSE standards for prompt engineering, the user will craft
+            your their own prompt and receive a real-time grade from the AI
+            agent. To complete the exercise, the user must iterate on their
+            prompt until it meets the passing criteria.
+          </p>
+        </div>
+        <LabButton onClick={handleContinue} key="start" label={"Start"} />
+      </div>
+    </>
+  );
 };
 
 export default ExerciseIntro;
