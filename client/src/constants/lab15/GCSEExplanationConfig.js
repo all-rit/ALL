f32156aee5469c4b347e2ef399a95095118619d5
Kristen Fang <kf4538@rit.edu>
2026-03-16 15:07:24 -0400

    Moved the constants to the constants folder

---
+++ client/src/constants/lab15/GCSEExplanationConfig.js
@@ -0,0 +1,35 @@
+/**
+ * GCSE icon styles
+ */
+export const circleStyles = [
+  "tw-bg-white tw-border-primary-blue tw-text-primary-blue",
+  "tw-bg-white tw-border-primary-yellow tw-text-primary-yellow",
+  "tw-bg-primary-blue tw-text-white tw-border-primary-blue",
+  "tw-bg-primary-yellow tw-text-white tw-border-primary-yellow",
+];
+
+/**
+ * Mapping for each word of GCSE
+ */
+export const gcse_content = [
+  {
+    letter: "G",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam justo nisi, rhoncus id mi tempor, ornare tincidunt felis. Sed vitae felis non augue malesuada accumsan. Morbi dui elit, cursus et justo sed, dictum dictum metus. Morbi eget lectus arcu. Suspendisse lectus libero, fringilla vitae rutrum ac, porta eu metus. Vivamus massa lectus, gravida at lectus vel, tincidunt gravida velit. Integer orci dolor, faucibus in venenatis quis, cursus quis lorem.",
+  },
+  {
+    letter: "C",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam justo nisi, rhoncus id mi tempor, ornare tincidunt felis. Sed vitae felis non augue malesuada accumsan. Morbi dui elit, cursus et justo sed, dictum dictum metus. Morbi eget lectus arcu. Suspendisse lectus libero, fringilla vitae rutrum ac, porta eu metus. Vivamus massa lectus, gravida at lectus vel, tincidunt gravida velit. Integer orci dolor, faucibus in venenatis quis, cursus quis lorem.",
+  },
+  {
+    letter: "S",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam justo nisi, rhoncus id mi tempor, ornare tincidunt felis. Sed vitae felis non augue malesuada accumsan. Morbi dui elit, cursus et justo sed, dictum dictum metus. Morbi eget lectus arcu. Suspendisse lectus libero, fringilla vitae rutrum ac, porta eu metus. Vivamus massa lectus, gravida at lectus vel, tincidunt gravida velit. Integer orci dolor, faucibus in venenatis quis, cursus quis lorem.",
+  },
+  {
+    letter: "E",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam justo nisi, rhoncus id mi tempor, ornare tincidunt felis. Sed vitae felis non augue malesuada accumsan. Morbi dui elit, cursus et justo sed, dictum dictum metus. Morbi eget lectus arcu. Suspendisse lectus libero, fringilla vitae rutrum ac, porta eu metus. Vivamus massa lectus, gravida at lectus vel, tincidunt gravida velit. Integer orci dolor, faucibus in venenatis quis, cursus quis lorem.",
+  },
+];
