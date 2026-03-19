9045c6b82a89b727632f2936f231b12c1a29f8c7
ConnerDeFeo <ninjanerozz@gmail.com>
2026-03-19 15:06:08 -0400

    backmerging

--- server/database/index.js
+++ server/database/index.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const pathname = path.join(__dirname, 'models');
 const withPassword = process.env.DB_PASS ? `:${process.env.DB_PASS}` : '';
-const URI = `postgres://${process.env.DB_USER}${withPassword}@${process.env.DB_HOST}:${process.env.ENVIRONMENT === 'dev'?5433:5432}/${process.env.DB_SCHEMA}`;
+const URI = `postgres://${process.env.DB_USER}${withPassword}@${process.env.DB_HOST}:${process.env.ENVIRONMENT === 'dev' ? 5433 : 5432}/${process.env.DB_SCHEMA}`;
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(URI, {
   dialect: 'postgres',
@@ -14,34 +14,33 @@ const sequelize = new Sequelize(URI, {
   define: {
     timestamps: false,
   },
-  logging: process.env.TESTING === 'test'? false : console.log,
+  logging: process.env.TESTING === 'test' ? false : console.log,
 });
 const db = {};
-
 sequelize
-    .authenticate()
-    .then(() => {
-      console.log('Connection has been established successfully.');
-    })
-    .catch((err) => {
-      console.error('Unable to connect to the database:', err);
-    });
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
 const files = [];
 const sortDir = (maniDir) => {
   const folders = [];
   const CheckFile = (filePath) => (fs.statSync(filePath).isFile());
   const sortPath = (dir) => {
     fs
-        .readdirSync(dir)
-        .filter((file) => (file.indexOf('.') !== 0) && (file !== 'index.js'))
-        .forEach((res) => {
-          const filePath = path.join(dir, res);
-          if (CheckFile(filePath)) {
-            files.push(filePath);
-          } else {
-            folders.push(filePath);
-          }
-        });
+      .readdirSync(dir)
+      .filter((file) => (file.indexOf('.') !== 0) && (file !== 'index.js'))
+      .forEach((res) => {
+        const filePath = path.join(dir, res);
+        if (CheckFile(filePath)) {
+          files.push(filePath);
+        } else {
+          folders.push(filePath);
+        }
+      });
   };
   folders.push(maniDir);
   let i = 0;
@@ -52,10 +51,10 @@ const sortDir = (maniDir) => {
 };
 sortDir(pathname);
 files
-    .forEach((file) => {
-      const model = require(file)(sequelize, Sequelize.DataTypes);
-      db[model.name] = model;
-    });
+  .forEach((file) => {
+    const model = require(file)(sequelize, Sequelize.DataTypes);
+    db[model.name] = model;
+  });
 
 Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
