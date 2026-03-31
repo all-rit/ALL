67bcd32231e6b2bd0c4cf008f21f323fe4884b47
Elaina Trapatsos <ejt7508@rit.edu>
2026-03-31 10:19:36 -0400

    Added "User has disconnected" text with an icon to the black box after the video ends

--- client/src/components/imagine25/components/TeammateVideo.js
+++ client/src/components/imagine25/components/TeammateVideo.js
@@ -64,7 +64,18 @@ const TeammateVideo = (props) => {
           }`}
         />
       ) : (
-        <div className="tw-w-full tw-h-full tw-bg-black tw-rounded-lg tw-shadow-lg tw-mx-auto"></div>
+        <div className="tw-w-full tw-h-full tw-bg-black tw-rounded-lg tw-shadow-lg tw-flex tw-justify-center tw-items-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="tw-fill-white tw-w-8 tw-h-8 tw-mr-3"
+            viewBox="0 0 576 512"
+          >
+            <path d="M41-24.9c-9.4-9.4-24.6-9.4-33.9 0S-2.3-.3 7 9.1l528 528c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-153-153 0-222.2c0-35.3-28.7-64-64-64L129.8 64 41-24.9zM32 128l0 256c0 35.3 28.7 64 64 64l256 0c7.8 0 15.3-1.4 22.2-4L36 105.8c-2.6 6.9-4 14.4-4 22.2zM464 336l73.5 58.8c4.2 3.4 9.4 5.2 14.8 5.2 13.1 0 23.7-10.6 23.7-23.7l0-240.6c0-13.1-10.6-23.7-23.7-23.7-5.4 0-10.6 1.8-14.8 5.2L464 176 464 336z" />
+          </svg>
+          <p className="tw-font-poppins tw-text-white tw-text-md">
+            User has disconnected
+          </p>
+        </div>
       )}
       <p>
         Teammate live from: <b>Buffalo, NY</b>
