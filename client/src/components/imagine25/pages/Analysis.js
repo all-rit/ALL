48f52cf1da4b98ad8e3ea85a50f256aadf31d03b
JackyFTW <jackmbarter@gmail.com>
2026-04-07 11:34:08 -0400

    Fixed typo in analysis message

--- client/src/components/imagine25/pages/Analysis.js
+++ client/src/components/imagine25/pages/Analysis.js
@@ -161,7 +161,7 @@ const Analysis = () => {
           colorMap[user.teammateAvatar.clotheColor].toLowerCase() +
           "\" shirt. Your teammate's points cannot be added due to error. Your team has been disqualified.",
         control:
-          "Congrats on winning! You may collect a prize after completing  the post survery for being so awesome sauce.",
+          "Congrats on winning! You may collect a prize after completing the post survey for being so awesome sauce.",
       };
 
       const error = {
