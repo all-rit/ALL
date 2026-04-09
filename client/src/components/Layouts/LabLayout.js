0ad476f47ac60947d6c351454c760974fc6242a6
Elaina Trapatsos <ejt7508@rit.edu>
2026-04-09 11:01:47 -0400

    Revert "Removed unused components, assets, and constants"
    This reverts commit a9fe683eb541a8058f6a7c58676d0d4613af0e03.

---
+++ client/src/components/Layouts/LabLayout.js
@@ -0,0 +1,8 @@
+/* eslint-disable react/prop-types */
+import React from "react";
+
+const LabLayout = ({ children }) => {
+  return <div className="playthrough">{children}</div>;
+};
+
+export default LabLayout;
