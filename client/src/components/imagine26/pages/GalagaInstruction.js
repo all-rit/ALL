8c26097021f35d897c7123c35e9eb9f3797c4c0e
Vivian Hernandez <hervivi04@gmail.com>
2026-04-21 14:57:40 -0400

    Changed wording based on feedback

--- client/src/components/imagine26/pages/GalagaInstruction.js
+++ client/src/components/imagine26/pages/GalagaInstruction.js
@@ -11,14 +11,14 @@ const GalagaInstructions = () => {
       {Frame(
         <div className="tw-grid tw-w-[20vw]">
           <p className="tw-body-text">
-            You will have a teammate and together will each compete against our
+            You will have a teammate and together will compete against our
             AI-controlled Galaga. The team with the highest combined score wins!
           </p>
           <br />
           {/*&apos; is just a apostrophe --> " ' "*/}
           <p className="tw-body-text">
             You will be playing Galaga! Use arrow keys to move and space to
-            shoot! Avoid enemy ships or lose points!
+            shoot. Avoid enemy ships or lose points!
           </p>
         </div>,
         () => navigate("/Imagine2026/Galaga"),
